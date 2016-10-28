3c392c7945185de1136b6260039475863ae8c77f
Guillaume Perrot <guperrot@microsoft.com>
2016-10-27 19:27:39 -0700

    Fix some warnings
    When opening solution on Visual Studio 2015 + Resharper 9.

--- Contoso.Forms.Puppet/Contoso.Forms.Puppet/App.cs
+++ Contoso.Forms.Puppet/Contoso.Forms.Puppet/App.cs
@@ -3,14 +3,13 @@ using Microsoft.Sonoma.Analytics.Forms;
 using Microsoft.Sonoma.Core;
 using Microsoft.Sonoma.Crashes;
 using System.Collections.Generic;
-using System.Diagnostics;
 using Xamarin.Forms;
 
 namespace Contoso.Forms.Puppet
 {
     public class App : Application
     {
-        private const string LOG_TAG = "SonomaXamarinPuppet";
+        private const string LogTag = "SonomaXamarinPuppet";
 
         public App()
         {
@@ -22,15 +21,15 @@ namespace Contoso.Forms.Puppet
         {
             // Handle when your app starts
             //Sonoma.SetServerUrl("http://in-integration.dev.avalanch.es:8081");
-            SonomaLog.Info(LOG_TAG, "Sonoma.LogLevel=" + Sonoma.LogLevel);
+            SonomaLog.Info(LogTag, "Sonoma.LogLevel=" + Sonoma.LogLevel);
             Sonoma.LogLevel = LogLevel.Verbose;
-            SonomaLog.Info(LOG_TAG, "Sonoma.LogLevel=" + Sonoma.LogLevel);
+            SonomaLog.Info(LogTag, "Sonoma.LogLevel=" + Sonoma.LogLevel);
             SonomaForms.StartTrackingFormPages();
             Sonoma.Start(typeof(Analytics), typeof(Crashes));
             Analytics.TrackEvent("myEvent");
             Analytics.TrackEvent("myEvent2", new Dictionary<string, string> { { "someKey", "someValue" } });
-            SonomaLog.Info(LOG_TAG, "Sonoma.InstallId=" + Sonoma.InstallId);
-            SonomaLog.Info(LOG_TAG, "Crashes.HasCrashedInLastSession=" + Crashes.HasCrashedInLastSession);
+            SonomaLog.Info(LogTag, "Sonoma.InstallId=" + Sonoma.InstallId);
+            SonomaLog.Info(LogTag, "Crashes.HasCrashedInLastSession=" + Crashes.HasCrashedInLastSession);
         }
     }
 }
--- Contoso.iOS.Puppet/Main.cs
+++ Contoso.iOS.Puppet/Main.cs
@@ -2,7 +2,7 @@
 
 namespace Contoso.iOS.Puppet
 {
-	public class Application
+	public static class Application
 	{
 		// This is the main entry point of the application.
 		static void Main(string[] args)
