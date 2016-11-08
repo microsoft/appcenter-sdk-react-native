8cdd50b0a791cf26f2ee69b510ae80d384d3afe2
Guillaume Perrot <guperrot@microsoft.com>
2016-11-07 17:29:51 -0800

    Reflect renaming in scripts folder

--- scripts/start-new-version.sh
+++ scripts/start-new-version.sh
@@ -2,5 +2,5 @@
 set -e
 
 # Replace version in all the assemblies
-find . -name AssemblyInfo.cs | xargs sed -E -i '' "s/(AssemblyInformationalVersion\(\")(.*)\"\)/\1$1-SNAPSHOT\")/g"
-find . -name AssemblyInfo.cs | xargs sed -E -i '' "s/(AssemblyFileVersion\(\")(.*)\"\)/\1$1.0\")/g"
+find SDK -name AssemblyInfo.cs | xargs sed -E -i '' "s/(AssemblyInformationalVersion\(\")(.*)\"\)/\1$1-SNAPSHOT\")/g"
+find SDK -name AssemblyInfo.cs | xargs sed -E -i '' "s/(AssemblyFileVersion\(\")(.*)\"\)/\1$1.0\")/g"
