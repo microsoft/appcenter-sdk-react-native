73304355af11d68898adb63c924ee8f1d1ed2bb5
Bret Johnson <bret@Brets-MacBook-Pro.local>
2016-11-22 02:40:43 -0500

    Added unlink and comments to update.sh script for TestApp

--- TestApp/update.sh
+++ TestApp/update.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
+echo 'Updating our npm packages...'
 rm -rf node_modules/mobilecenter-crashes
 rm -rf node_modules/mobilecenter-analytics
 npm install
+echo 'Unlinking...'
+react-native unlink mobilecenter-crashes
+react-native unlink mobilecenter-analytics
+echo 'Linking...'
 react-native link mobilecenter-crashes
 react-native link mobilecenter-analytics
