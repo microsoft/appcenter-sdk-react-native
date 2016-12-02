e3983f0dbc0f043929593d169e3c4112d52737c8
Bret Johnson <bret.johnson@microsoft.com>
2016-12-02 04:17:26 -0500

    Updated TestApp to work for Android too and incl. analytics (copied from DemoApp)

---
+++ TestApp/index.js
@@ -0,0 +1,121 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  Alert,
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  NativeModules
+} from 'react-native';
+
+import Analytics from 'mobile-center-analytics';
+import Crashes from 'mobile-center-crashes';
+import {FooClass} from './js/FooClass';
+
+var data = {};
+data.x = "x value";
+data.y = 12;
+data.null = null;
+data["undefd"] = undefined;
+data["recursive"] = data;
+data["function"] = function () { console.log(arguments);};
+
+class TestApp extends Component {
+  async componentDidMount() {
+    if (Crashes.hasCrashedInLastSession) {
+      Crashes.process(function (reports, send) {
+        Crashes.addEventListener({
+          willSendCrash: function () {
+            console.log("WILL SEND CRASH");
+            console.log(arguments);
+          },
+          didSendCrash: function () {
+            console.log("DID SEND CRASH");
+            console.log(arguments);
+          },
+          failedSendingCrash: function () {
+            console.log("FAILED SENDING CRASH");
+            console.log(arguments);
+          }
+        });
+        Alert.alert(
+        'Unhandled exception:',
+        reports[0].exceptionReason,
+        [
+          {text: 'Send crash', onPress: () => send(true,{}) },
+          {text: 'Ignore crash', onPress: () => send(false,{}), style: 'cancel'},
+        ]
+      );
+      });
+    }
+  }
+  
+  jsCrash() {
+    var foo = new FooClass();
+    foo.method1();
+  }
+  nativeCrash() {
+    NativeModules.TestCrash.crash();
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Welcome to Mobile Center!
+        </Text>
+
+        <TouchableOpacity onPress={this.jsCrash.bind(this)}>
+          <Text style={styles.button}>
+            Crash JavaScript
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={this.nativeCrash.bind(this)}>
+          <Text style={styles.button}>
+            Crash native code
+          </Text>
+        </TouchableOpacity>
+  
+        <TouchableOpacity onPress={() => Analytics.trackEvent("Button press", { page: "Home page" })}>
+          <Text style={styles.button}>
+            Track Event
+          </Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity onPress={() => Analytics.trackEvent("Button press", data)}>
+          <Text style={styles.button}>
+            Track Event badly (Don't do this, only strings are supported)
+          </Text>
+        </TouchableOpacity>     
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  button: {
+    color: '#4444FF',
+    fontSize: 18,
+    textAlign: 'center',
+    margin: 10,
+  },
+});
+
+AppRegistry.registerComponent('TestApp', () => TestApp);
