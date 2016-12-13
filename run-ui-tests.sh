5e323b35e7b737ecf25364a37a11580fa59d7c35
Alexander Chocron <alchocro@microsoft.com>
2016-12-13 13:52:25 -0800

    Add more functionality to test script

--- run-ui-tests.sh
+++ run-ui-tests.sh
@@ -1,47 +1,50 @@
 #!/bin/bash
 
-# Define constants
-BUILD_SCRIPT=build.sh
-BUILD_TARGET=Tests
+# Define directory and file locations
 SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-TOOLS_DIR=$SCRIPT_DIR/tools
-NPM_INSTALL=$TOOLS_DIR/npm_install.sh
-NPM_URL=https://www.npmjs.org/install.sh
 TEST_APK=$SCRIPT_DIR/Tests/Droid/bin/Release/com.contoso.contoso_forms_test.apk
 TEST_IPA=$SCRIPT_DIR/Tests/iOS/bin/iPhone/Release/Contoso.Forms.Test.iOS.ipa
-UITEST_ASSEMBLY_DIR=$SCRIPT_DIR/Tests/UITests/bin/Release
+UITEST_BUILD_DIR=$SCRIPT_DIR/Tests/UITests/bin/Release
+
+# Define build constants
+BUILD_SCRIPT=build.sh
+BUILD_TARGET=Tests
+
+# Define test parameters
 LOCALE="en-US"
-USERNAME="$1"
-PASSWORD="$2"
-IOS_DEVICES=8551ba4e
-ANDROID_DEVICES=f0b8289c
-ANDROID_APP="$USERNAME/mobilecenter-xamarin-testing-app-android"
-IOS_APP="$USERNAME/mobilecenter-xamarin-testing-app-ios"
+USERNAME="$MOBILE_CENTER_USERNAME" # 'MOBILE_CENTER_USERNAME' environment variable must be set
+PASSWORD="$MOBILE_CENTER_PASSWORD" # 'MOBILE_CENTER_PASSWORD' environment variable must be set
+IOS_DEVICES=8551ba4e # just one device. For a suite of 40, use 118f9d2f
+ANDROID_DEVICES=f0b8289c # just one device. For a suite of 40, use f47808f1
+ANDROID_APP_NAME="mobilecenter-xamarin-testing-app-android"
+IOS_APP_NAME="mobilecenter-xamarin-testing-app-ios"
+ANDROID_APP="$USERNAME/$ANDROID_APP_NAME"
+IOS_APP="$USERNAME/$IOS_APP_NAME"
 TEST_SERIES="master"
 
-# Make sure that the tools directory exists
-if [ ! -d $TOOLS_DIR ]; then
-  mkdir $TOOLS_DIR
-fi
+# Define results constants
+EXIT_CODE=0
+ANDROID_PORTAL_URL="https://mobile.azure.com/users/$USERNAME/apps/$ANDROID_APP_NAME/test/runs/"
+IOS_PORTAL_URL="https://mobile.azure.com/users/$USERNAME/apps/$IOS_APP_NAME/test/runs/"
+ANDROID_RESULTS_FILE="android_results_$(date).txt"
+IOS_RESULTS_FILE="android_results_$(date).txt"
 
-# Download and install NPM if it is not already
+# Define text attributes
+RED=$(tput setaf 1)
+GREEN=$(tput setaf 2)
+BOLD=$(tput bold)
+UNATTRIBUTED=$(tput sgr0)
+
+# Download and install NPM if it is not already installed
 npm -v &>/dev/null
 if [ $? -ne 0 ]; then
-	# Download npm
-	echo "Downloading npm..."
+	# Install npm
+	echo "Installing npm..."
     brew install npm
 	if [ $? -ne 0 ]; then
     	echo "An error occured while downloading npm."
     	exit 1
-	fi
-
-	# Install npm
-	echo "Installing npm..."
-	sh $NPM_INSTALL >/dev/null
-	if [ $? -ne 0 ]; then
-    	echo "An error occured while installing npm."
-    	exit 1
-	fi  
+	fi 
 fi
 
 # Is Mobile Center CLI installed?
@@ -56,15 +59,15 @@ if [ $? -ne 0 ]; then
 	fi
 fi
 
-#2 Log in to Mobile Center
+# Log in to Mobile Center
 echo "Logging in to mobile center as $USERNAME..."
-mobile-center login -u "$1" -p "$PASSWORD"
+mobile-center login -u "$USERNAME" -p "$PASSWORD"
 if [ $? -ne 0 ]; then
     echo "An error occured while logging into Mobile Center."
     exit 1
 fi
 
-#4 Build tests
+# Build tests
 echo "Building applications and UITests..."
 sh $BUILD_SCRIPT -t $BUILD_TARGET
 if [ $? -ne 0 ]; then
@@ -72,14 +75,58 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
-# Upload tests
-echo "Uploading Android tests..."
+# Run Android tests
+echo "[$(date)] Running Android tests..."
 mobile-center test run uitest --app $ANDROID_APP\
  --devices $ANDROID_DEVICES --app-path $TEST_APK\
   --test-series $TEST_SERIES --locale $LOCALE\
-   --build-dir $UITEST_ASSEMBLY_DIR
-echo "Uploading iOS tests..."
+   --build-dir $UITEST_BUILD_DIR > $ANDROID_RESULTS_FILE
+ANDROID_RETURN_CODE=$?
+echo "[$(date)] Android tests completed."
+cat $ANDROID_RESULTS_FILE
+ANDROID_TEST_RUN_ID=$(
+while read -r line
+do
+	if [ $(expr "$line" : "Test run id: ") -ne 0 ]; then
+		echo $(echo $line | cut -d'"' -f 2)
+		break
+	fi
+done < $ANDROID_RESULTS_FILE)
+rm $ANDROID_RESULTS_FILE
+
+# Run iOS tests
+echo "Running iOS tests..."
 mobile-center test run uitest --app $IOS_APP\
    --devices $IOS_DEVICES --app-path $TEST_IPA\
    --test-series $TEST_SERIES --locale $LOCALE\
-   --build-dir $UITEST_ASSEMBLY_DIR
+   --build-dir $UITEST_BUILD_DIR > $IOS_RESULTS_FILE
+IOS_RETURN_CODE=$?
+echo "[$(date)] iOS tests completed."
+cat $IOS_RESULTS_FILE
+IOS_TEST_RUN_ID=$(
+while read -r line
+do
+   	if [ $(expr "$line" : "Test run id: ") -ne 0 ]; then
+		echo $(echo $line | cut -d'"' -f 2)
+		break
+	fi
+done < $IOS_RESULTS_FILE)
+rm $IOS_RESULTS_FILE
+
+# Print results
+if [ $ANDROID_RETURN_CODE -eq 0 ]; then
+	echo "${BOLD}Android test results: ${GREEN}passed! ${UNATTRIBUTED}"
+fi
+if [ $ANDROID_RETURN_CODE -ne 0 ]; then
+	EXIT_CODE=$ANDROID_RETURN_CODE
+	echo "${BOLD}Android test results: ${RED}failed.${UNATTRIBUTED}${BOLD} For more information, visit $ANDROID_PORTAL_URL$ANDROID_TEST_RUN_ID.${UNATTRIBUTED}"
+fi
+if [ $IOS_RETURN_CODE -eq 0 ]; then
+	echo "${BOLD}iOS test results: ${GREEN}passed! ${UNATTRIBUTED}"
+fi
+if [ $IOS_RETURN_CODE -ne 0 ]; then
+	EXIT_CODE=$IOS_RETURN_CODE
+	echo "${BOLD}iOS test results: ${RED}failed.${UNATTRIBUTED}${BOLD} For more information, visit $IOS_PORTAL_URL$IOS_TEST_RUN_ID.${UNATTRIBUTED}"
+fi
+
+exit $EXIT_CODE
