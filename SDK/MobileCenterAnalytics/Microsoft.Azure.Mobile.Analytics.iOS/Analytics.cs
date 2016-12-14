b824b3e3e9082d728f0c2337cfb83d5805e75812
Guillaume Perrot <guperrot@microsoft.com>
2016-12-14 13:51:14 -0800

    Fix ios crash when using "Link All" optimization

--- SDK/MobileCenterAnalytics/Microsoft.Azure.Mobile.Analytics.iOS/Analytics.cs
+++ SDK/MobileCenterAnalytics/Microsoft.Azure.Mobile.Analytics.iOS/Analytics.cs
@@ -5,74 +5,75 @@ using Foundation;
 
 namespace Microsoft.Azure.Mobile.Analytics
 {
-	using System.Linq;
-	using iOSAnalytics = iOS.Bindings.MSAnalytics;
+    using System.Linq;
+    using iOSAnalytics = iOS.Bindings.MSAnalytics;
 
-	/// <summary>
-	/// Analytics feature.
-	/// </summary>
-	public static class Analytics
-	{
-		/// <summary>
-		/// Internal SDK property not intended for public use.
-		/// </summary>
-		/// <value>
-		/// The Android SDK Analytics bindings type.
-		/// </value>
-		public static Type BindingType => typeof(iOSAnalytics);
+    /// <summary>
+    /// Analytics feature.
+    /// </summary>
+    public static class Analytics
+    {
+        /// <summary>
+        /// Internal SDK property not intended for public use.
+        /// </summary>
+        /// <value>
+        /// The Android SDK Analytics bindings type.
+        /// </value>
+        [Preserve]
+        public static Type BindingType => typeof(iOSAnalytics);
 
-		/// <summary>
-		/// Enable or disable Analytics module.
-		/// </summary>
-		public static bool Enabled
-		{
+        /// <summary>
+        /// Enable or disable Analytics module.
+        /// </summary>
+        public static bool Enabled
+        {
             get { return iOSAnalytics.IsEnabled(); }
             set { iOSAnalytics.SetEnabled(value); }
-		}
+        }
 
-		/// <summary>
-		/// Enable or disable automatic page tracking.
-		/// Set this to false to if you plan to call <see cref="TrackPage"/> manually.
-		/// </summary>
-		//public static bool AutoPageTrackingEnabled
-		//{
-		//	get { return iOSAnalytics.IsAutoPageTrackingEnabled(); }
-		//	set { iOSAnalytics.SetAutoPageTrackingEnabled(value); }
-		//}
+        /// <summary>
+        /// Enable or disable automatic page tracking.
+        /// Set this to false to if you plan to call <see cref="TrackPage"/> manually.
+        /// </summary>
+        //public static bool AutoPageTrackingEnabled
+        //{
+        //	get { return iOSAnalytics.IsAutoPageTrackingEnabled(); }
+        //	set { iOSAnalytics.SetAutoPageTrackingEnabled(value); }
+        //}
 
-		/// <summary>
-		/// Track a custom event.
-		/// </summary>
-		/// <param name="name">An event name.</param>
-		/// <param name="properties">Optional properties.</param>
-		public static void TrackEvent(string name, [Optional] IDictionary<string, string> properties)
-		{
-			if (properties != null)
-			{
-				iOSAnalytics.TrackEvent(name, StringDictToNSDict(properties));
-				return;
-			}
-			iOSAnalytics.TrackEvent(name);
-		}
+        /// <summary>
+        /// Track a custom event.
+        /// </summary>
+        /// <param name="name">An event name.</param>
+        /// <param name="properties">Optional properties.</param>
+        public static void TrackEvent(string name, [Optional] IDictionary<string, string> properties)
+        {
+            if (properties != null)
+            {
+                iOSAnalytics.TrackEvent(name, StringDictToNSDict(properties));
+                return;
+            }
+            iOSAnalytics.TrackEvent(name);
+        }
 
-		///// <summary>
-		///// Track a custom page.
-		///// </summary>
-		///// <param name="name">A page name.</param>
-		///// <param name="properties">Optional properties.</param>
-		//public static void TrackPage(string name, [Optional] IDictionary<string, string> properties)
-		//{
-		//	if (properties != null)
-		//	{
-		//		iOSAnalytics.TrackPage(name, StringDictToNSDict(properties));
-		//		return;
-		//	}
-		//	iOSAnalytics.TrackPage(name);
-		//}
+        ///// <summary>
+        ///// Track a custom page.
+        ///// </summary>
+        ///// <param name="name">A page name.</param>
+        ///// <param name="properties">Optional properties.</param>
+        //public static void TrackPage(string name, [Optional] IDictionary<string, string> properties)
+        //{
+        //	if (properties != null)
+        //	{
+        //		iOSAnalytics.TrackPage(name, StringDictToNSDict(properties));
+        //		return;
+        //	}
+        //	iOSAnalytics.TrackPage(name);
+        //}
 
-		private static NSDictionary StringDictToNSDict(IDictionary<string, string> dict)
-		{
-			return NSDictionary.FromObjectsAndKeys(dict.Values.ToArray(), dict.Keys.ToArray());
-		}
-	}
+        private static NSDictionary StringDictToNSDict(IDictionary<string, string> dict)
+        {
+            return NSDictionary.FromObjectsAndKeys(dict.Values.ToArray(), dict.Keys.ToArray());
+        }
+    }
 }
