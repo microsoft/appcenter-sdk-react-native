216403641919f8314ad5135fd79c5185626cc928
Bret Johnson <bret.johnson@microsoft.com>
2017-01-09 21:32:42 -0500

    Changed iOS to include React Native header files, and our own header files, to be included with the namespace to support the React Native 0.40 breaking changes. We also still include headers the old way, by doing a #if __has_include check, in order to support pre RN 0.40 with the same SDK code base.

--- mobile-center-analytics/ios/RNAnalytics/RNAnalytics.m
+++ mobile-center-analytics/ios/RNAnalytics/RNAnalytics.m
@@ -1,11 +1,22 @@
 #import "RNAnalytics.h"
 
+// Support React Native headers both in the React namespace, where they are in RN version 0.40+,
+// and no namespace, for older versions of React Native
+#if __has_include(<React/RCTAssert.h>)
+#import <React/RCTAssert.h>
+#import <React/RCTBridgeModule.h>
+#import <React/RCTConvert.h>
+#import <React/RCTEventDispatcher.h>
+#import <React/RCTRootView.h>
+#import <React/RCTUtils.h>
+#else
 #import "RCTAssert.h"
 #import "RCTBridgeModule.h"
 #import "RCTConvert.h"
 #import "RCTEventDispatcher.h"
 #import "RCTRootView.h"
 #import "RCTUtils.h"
+#endif
 
 @import MobileCenterAnalytics;
 @import RNMobileCenter;
--- mobile-center-crashes/ios/RNCrashes/RNCrashesDelegate.h
+++ mobile-center-crashes/ios/RNCrashes/RNCrashesDelegate.h
@@ -2,7 +2,13 @@
 
 @import MobileCenterCrashes;
 
+// Support React Native headers both in the React namespace, where they are in RN version 0.40+,
+// and no namespace, for older versions of React Native
+#if __has_include(<React/RCTBridge.h>)
+#import <React/RCTBridge.h>
+#else
 #import "RCTBridge.h"
+#endif
 
 @class RNCrashes;
 @class MSErrorReport;
