396cc881cec8fc0959829e5c4fea40c969f724c6
Alexander Chocron <alchocro@microsoft.com>
2017-03-10 16:17:11 -0800

    Modify runsettings and add file for MobileCenterServiceTest

--- SDK/MobileCenter/Microsoft.Azure.Mobile.Windows.Shared/Ingestion/IngestionException.cs
+++ SDK/MobileCenter/Microsoft.Azure.Mobile.Windows.Shared/Ingestion/IngestionException.cs
@@ -9,7 +9,6 @@ namespace Microsoft.Azure.Mobile.Ingestion
         public virtual bool IsRecoverable => false;
 
         public IngestionException(string message) : base(message) { }
-        public IngestionException(string message, Exception innerException) : base(message, innerException) { }
         public IngestionException(Exception innerException) : base(DefaultMessage, innerException) { }
         public IngestionException() : base(DefaultMessage) { }
     }
---
+++ Tests/Microsoft.Azure.Mobile.Test.Windows/MobileCenterServiceTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Moq.Protected;
+
+namespace Microsoft.Azure.Mobile.Test.Windows
+{
+    /*
+        MobileCenterService Protected members for reference:
+            protected IChannelGroup ChannelGroup { get; private set; }
+            protected IChannel Channel { get; private set; }
+            protected abstract string ChannelName { get; }
+            protected abstract string ServiceName { get; }
+            protected virtual string EnabledPreferenceKey => KeyEnabled + PreferenceKeySeparator + ChannelName;
+            protected virtual int TriggerCount => DefaultTriggerCount;
+            protected virtual TimeSpan TriggerInterval => DefaultTriggerInterval;
+            protected virtual int TriggerMaxParallelRequests => DefaultTriggerMaxParallelRequests;
+            protected bool IsInactive;
+    */
+
+    [TestClass]
+    public class MobileCenterServiceTest
+    {
+    }
+}
