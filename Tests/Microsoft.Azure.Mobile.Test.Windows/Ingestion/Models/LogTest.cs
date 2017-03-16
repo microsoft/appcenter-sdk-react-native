bcec3c027fca96adb9a0e433b0b64cd2647d716f
Murat Baysangurov <v-mubays@microsoft.com>
2017-03-16 16:43:19 +0300

    Unit tests added to ingestion models

---
+++ Tests/Microsoft.Azure.Mobile.Test.Windows/Ingestion/Models/LogTest.cs
@@ -0,0 +1,60 @@
+﻿using Moq;
+using Microsoft.Rest;
+using Microsoft.Azure.Mobile.Ingestion.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Microsoft.Azure.Mobile.Test.Windows.Ingestion.Models
+{
+    using Device = Microsoft.Azure.Mobile.Ingestion.Models.Device;
+
+    [TestClass]
+    public class LogTest
+    {
+        private const long toOffset = 0;
+
+        /// <summary>
+        /// Verify that instance is constructed properly.
+        /// </summary>
+        [TestMethod]
+        public void TestInstanceConstruction()
+        {
+            var defaultGuid = default(System.Guid?);
+            var mockDevice = new Mock<Device>();
+            
+            Log emptyLog = new TestLog();
+            Log log = new TestLog(toOffset, mockDevice.Object);
+
+            Assert.IsNotNull(emptyLog);
+            Assert.IsNotNull(log);
+
+            Assert.AreEqual(defaultGuid, log.Sid);
+            Assert.AreEqual<long>(toOffset, log.Toffset);
+            Assert.AreEqual<Device>(mockDevice.Object, log.Device);
+        }
+
+        /// <summary>
+        /// Verify that Validate method throws ValidationException when device == null.
+        /// </summary>
+        [TestMethod]
+        public void TestValidateThrowsExceptionWhenDeviceIsNull()
+        {
+            const Device nullDevice = null;
+            Log log = new TestLog(toOffset, nullDevice);
+            Assert.ThrowsException<ValidationException>(() => log.Validate());
+        }
+
+        /// <summary>
+        /// Verify that Validate calls Device.Validate when device != null.
+        /// </summary>
+        [TestMethod]
+        public void TestValidateCallsDeviceValidateWhenDeviceIsNotNull()
+        {
+            var mockDevice = new Mock<Device>();
+
+            Log log = new TestLog(toOffset, mockDevice.Object);
+            log.Validate();
+
+            mockDevice.Verify(device => device.Validate());
+        }
+    }
+}
---
+++ Tests/Microsoft.Azure.Mobile.Test.Windows/Ingestion/Models/LogWithPropertiesTest.cs
@@ -0,0 +1,43 @@
+﻿using Moq;
+using Microsoft.Rest;
+using System.Collections.Generic;
+using Microsoft.Azure.Mobile.Ingestion.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Microsoft.Azure.Mobile.Test.Windows.Ingestion.Models
+{
+    
+    using Device = Microsoft.Azure.Mobile.Ingestion.Models.Device;
+
+    [TestClass]
+    public class LogWithPopertiesTest
+    {
+        private const long toOffset = 0;
+
+        /// <summary>
+        /// Verify that instance is constructed properly.
+        /// </summary>
+        [TestMethod]
+        public void TestInstanceConstruction()
+        {
+            var defaultDictionary = default(IDictionary<string, string>);
+            var mockDevice = new Mock<Device>();
+
+            LogWithProperties emptyLog = new TestLogWithProperties();
+            LogWithProperties log = new TestLogWithProperties(toOffset, mockDevice.Object);
+
+            Assert.IsNotNull(emptyLog);
+            Assert.IsNotNull(log);
+        
+            Assert.AreEqual(defaultDictionary, log.Properties);
+        }
+    }
+
+
+    class TestLogWithProperties : LogWithProperties
+    {
+        public TestLogWithProperties() { }
+        public TestLogWithProperties(long toffset, Device device, System.Guid? sid = default(System.Guid?), IDictionary<string, string> properties = default(IDictionary<string, string>))
+            : base(toffset, device, sid, properties) { }
+    }
+}
