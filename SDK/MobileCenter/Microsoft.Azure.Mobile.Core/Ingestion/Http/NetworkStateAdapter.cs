b37332798e4cdf1849b9aab2a79ff08231fac9a6
Alexander Chocron <alchocro@microsoft.com>
2017-03-20 10:22:32 -0700

    Add dotnet core project

---
+++ SDK/MobileCenter/Microsoft.Azure.Mobile.Core/Ingestion/Http/NetworkStateAdapter.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Microsoft.Azure.Mobile.Ingestion.Http
+{
+    public class NetworkStateAdapter : INetworkStateAdapter
+    {
+        public bool IsConnected { get; }
+        public event Action NetworkAddressChanged;
+    }
+}
---
+++ SDK/MobileCenter/Microsoft.Azure.Mobile.Core/Utils/ApplicationLifecycleHelper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.Azure.Mobile.Utils
+{
+    public class ApplicationLifecycleHelper : IApplicationLifecycleHelper
+    {
+        public bool Enabled { get; set; }
+        public event Action ApplicationSuspending;
+        public event Action ApplicationResuming;
+    }
+}
