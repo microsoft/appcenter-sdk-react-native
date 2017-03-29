873be1fe4b221efcae67d2bd30c4e34dd3bae0c8
Alexander Chocron <alchocro@microsoft.com>
2017-03-29 10:48:50 -0700

    Create synchronization types to deal with state consistency in channel

---
+++ SDK/MobileCenter/Microsoft.Azure.Mobile.Windows.Shared/Utils/Synchronization/StatefulMutexException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microsoft.Azure.Mobile.Utils.Synchronization
+{
+    public class StatefulMutexException : MobileCenterException
+    {
+        public StatefulMutexException(string message) : base(message)
+        {
+        }
+
+        public StatefulMutexException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
