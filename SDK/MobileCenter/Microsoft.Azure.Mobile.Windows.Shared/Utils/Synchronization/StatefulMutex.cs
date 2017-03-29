7443e4b7d28d2395cbe88062d934b042bad703c7
Alexander Chocron <alchocro@microsoft.com>
2017-03-29 12:42:54 -0700

    Add comments to StatefulMutex class

--- SDK/MobileCenter/Microsoft.Azure.Mobile.Windows.Shared/Utils/Synchronization/StatefulMutex.cs
+++ SDK/MobileCenter/Microsoft.Azure.Mobile.Windows.Shared/Utils/Synchronization/StatefulMutex.cs
@@ -5,21 +5,40 @@ using System.Threading.Tasks;
 
 namespace Microsoft.Azure.Mobile.Utils.Synchronization
 {
+    /// <summary>
+    /// Allows synchronization across tasks, regardless of the thread executing it.
+    /// Doing so involves tracking a state, so that once invalidated, trying to 
+    /// acquire the lock will fail.
+    /// </summary>
     public class StatefulMutex : IDisposable
     {
         private readonly SemaphoreSlim _mutex = new SemaphoreSlim(1, 1);
         private readonly StateKeeper _stateKeeper;
 
+        /// <summary>
+        /// Creates a <see cref="StatefulMutex"/>  with an existing <see cref="StateKeeper"/> 
+        /// </summary>
+        /// <param name="stateKeeper">Tracks the current state for the mutex</param>
         public StatefulMutex(StateKeeper stateKeeper)
         {
             _stateKeeper = stateKeeper;
         }
 
+        /// <summary>
+        /// Locks the mutex and does not verify any state
+        /// </summary>
+        /// <seealso cref="LockAsync"/>
         public void Lock()
         {
             _mutex.Wait();
         }
 
+        /// <summary>
+        /// Locks the mutex if the given <see cref="State"/> is consistent with the mutex's <see cref="StateKeeper"/> state
+        /// </summary>
+        /// <param name="stateSnapshot">The <see cref="State"/> that must be consistent with the mutex's <see cref="StateKeeper"/> state</param>
+        /// <exception cref="StatefulMutexException">The given state is invalid</exception>
+        /// <seealso cref="LockAsync(State)"/> 
         public void Lock(State stateSnapshot)
         {
             _mutex.Wait();
@@ -29,6 +48,12 @@ namespace Microsoft.Azure.Mobile.Utils.Synchronization
             }
         }
 
+        /// <summary>
+        /// Locks the mutex if the given <see cref="State"/> is consistent with the mutex's <see cref="StateKeeper"/> state
+        /// </summary>
+        /// <param name="stateSnapshot">The <see cref="State"/> that must be consistent with the mutex's <see cref="StateKeeper"/> state</param>
+        /// <exception cref="StatefulMutexException">The given state is invalid</exception>
+        /// <seealso cref="Lock(State)"/> 
         public async Task LockAsync(State stateSnapshot)
         {
             await _mutex.WaitAsync();
@@ -39,12 +64,18 @@ namespace Microsoft.Azure.Mobile.Utils.Synchronization
             }
         }
 
+        /// <summary>
+        /// Asynchronously locks the mutex and does not verify any state
+        /// </summary>
+        /// <seealso cref="Lock"/>
         public async Task LockAsync()
         {
             await _mutex.WaitAsync();
         }
 
-        //Double-unlocking does not throw an error
+        /// <summary>
+        /// Unlocks the mutex. Consecutive calls will not throw an exception
+        /// </summary>
         public void Unlock()
         {
             if (_mutex.CurrentCount == 0)
@@ -53,6 +84,9 @@ namespace Microsoft.Azure.Mobile.Utils.Synchronization
             }
         }
 
+        /// <summary>
+        /// Dispose of the <see cref="StatefulMutex"/> 
+        /// </summary>
         public void Dispose()
         {
             _mutex.Dispose();
