3373f0d800eb1d97a640f6859bcf8058891a2c71
max-mironov <v-maximi@microsoft.com>
2017-03-30 12:10:12 +0300

    Bumped version of MobileCenter to 0.6.0. for both iOS and Android and updated other dependencies accordinaly

--- mobile-center-crashes/scripts/postlink.js
+++ mobile-center-crashes/scripts/postlink.js
@@ -16,12 +16,12 @@ return rnpmlink.ios.initMobileCenterConfig().then(function (file) {
 }).then(function (file) {
     console.log('Added code to initialize iOS Crashes SDK in ' + file);
     return rnpmlink.ios.addPodDeps([
-        { pod: 'MobileCenter', version: '0.5.0' },
-        { pod: 'RNMobileCenter', version: '0.3.0' }
+        { pod: 'MobileCenter', version: '0.6.0' },
+        { pod: 'RNMobileCenter', version: '0.4.0' }
     ]).catch(function (e) {
         console.log(`
-            Could not install dependencies using CocoaPods. 
-            Please refer the documentation to install dependencies manually. 
+            Could not install dependencies using CocoaPods.
+            Please refer the documentation to install dependencies manually.
 
             Error Reason - ${e.message}
         `)
