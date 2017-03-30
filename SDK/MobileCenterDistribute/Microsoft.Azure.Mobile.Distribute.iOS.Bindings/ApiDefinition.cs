145a13ef39e052fe5a3aa8631c4e4184247f3249
Benjamin Scholtysik (Reimold) <bereimol@microsoft.com>
2017-03-30 12:17:17 -0700

    Add iOS bindings for MSDistribute

--- SDK/MobileCenterDistribute/Microsoft.Azure.Mobile.Distribute.iOS.Bindings/ApiDefinition.cs
+++ SDK/MobileCenterDistribute/Microsoft.Azure.Mobile.Distribute.iOS.Bindings/ApiDefinition.cs
@@ -6,5 +6,29 @@ namespace Microsoft.Azure.Mobile.Distribute.iOS.Bindings
     [BaseType(typeof(NSObject))]
     interface MSDistribute
     {
+        // +(void)setEnabled:(BOOL)isEnabled;
+        [Static]
+        [Export("setEnabled:")]
+        void SetEnabled(bool isEnabled);
+
+        // +(BOOL)isEnabled;
+        [Static]
+        [Export("isEnabled")]
+        bool IsEnabled();
+
+        // + (void)setApiUrl:(NSString *)apiUrl;
+        [Static]
+        [Export("setApiUrl:")]
+        void SetApiUrl(string apiUrl);
+
+        // + (void)setInstallUrl:(NSString *)installUrl;
+        [Static]
+        [Export("setInstallUrl:")]
+        void SetInstallUrl(string installUrl);
+
+        // + (void)openUrl:(NSURL *)url;
+        [Static]
+        [Export("openUrl:")]
+        void OpenUrl(NSUrl url);
     }
 }
