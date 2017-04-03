4b9ee213eeffb1bdc8065f097cab2f02ae2af2d2
Alexander Chocron <alchocro@microsoft.com>
2017-04-03 16:01:17 -0700

    Add documentation

--- SDK/MobileCenter/Microsoft.Azure.Mobile/MobileCenterService.cs
+++ SDK/MobileCenter/Microsoft.Azure.Mobile/MobileCenterService.cs
@@ -1,5 +1,8 @@
 ﻿namespace Microsoft.Azure.Mobile
 {
+    /// <summary>
+    /// Contains base functionality for classes that implement IMobileCenterService
+    /// </summary>
     public abstract class MobileCenterService : IMobileCenterService
     {
     }
