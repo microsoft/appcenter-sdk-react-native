b6928a49b3b02ab736ae6bf87c754e30d3aaf49c
Alexander Chocron <alchocro@microsoft.com>
2017-04-06 10:45:13 -0700

    Modify configurations

---
+++ SDK/MobileCenter/Microsoft.Azure.Mobile.UWP/Utils/WatsonCrashesStarter.cs
@@ -0,0 +1,23 @@
+﻿using WatsonRegistrationUtility;
+
+namespace Microsoft.Azure.Mobile.Utils
+{
+    /// <summary>
+    /// Used to register application with watson for Crashes.
+    /// </summary>
+    public class WatsonCrashesStarter
+    {
+        /// <exception cref="MobileCenterException"/>
+        public static void RegisterWithWatson(string appSecret)
+        {
+            try
+            {
+                WatsonRegistrationManager.Start(appSecret);
+            }
+            catch
+            {
+                MobileCenterLog.Info(MobileCenterLog.LogTag, "The API to register this application with the Watson service is not supported on this device.");
+            }
+        }
+    }
+}
