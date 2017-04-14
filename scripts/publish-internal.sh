046e826dd9b534506af410a147af382217751ad7
Guillaume Perrot <guperrot@microsoft.com>
2017-04-13 17:34:26 -0700

    Fix increment on new base version
    Also remove old bitrise scripts

--- scripts/publish-internal.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-
-for packageFile in output/*.nupkg
-do
-    curl -X PUT "https://mseng.pkgs.visualstudio.com/_packaging/${NUGET_FEED_ID}/nuget/v2/" -H "Content-Type: multipart/form-data" -H "X-NuGet-ApiKey: $NUGET_PASSWORD" -H "Content-Disposition: form-data; name=package; filename=package" -F "file=@$packageFile" -s --user $NUGET_USER:$NUGET_PASSWORD
-done
