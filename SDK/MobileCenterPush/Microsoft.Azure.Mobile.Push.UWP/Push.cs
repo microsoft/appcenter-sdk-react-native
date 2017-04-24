058d33143ae217bfc89fc72077589250c265a529
Di Hei <dihei@microsoft.com>
2017-04-24 12:05:17 -0700

    initial push commit

---
+++ SDK/MobileCenterPush/Microsoft.Azure.Mobile.Push.UWP/Push.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.Azure.Mobile.Push.Shared.Ingestion.Models;
+using Windows.Networking.PushNotifications;
+
+namespace Microsoft.Azure.Mobile.Push
+{
+    public partial class Push : MobileCenterService
+    {
+        private async void InstanceRegister()
+        {
+            if (Enabled)
+            {
+                string pushToken = (await PushNotificationChannelManager.CreatePushNotificationChannelForApplicationAsync()).Uri;
+
+                if (!string.IsNullOrEmpty(pushToken))
+                {
+                    MobileCenterLog.Debug(MobileCenterLog.LogTag, $"Push token '{pushToken}'");
+
+                    PushInstallationLog pushInstallationLog = new PushInstallationLog(0, null, pushToken, Guid.NewGuid());
+
+                    Channel.Enqueue(pushInstallationLog);
+                }
+                else
+                {
+                    MobileCenterLog.Error(LogTag, "PushToken cannot be null or empty.");
+                }
+            }
+            else
+            {
+                MobileCenterLog.Warn(MobileCenterLog.LogTag, "Push service is not enable.");
+            }
+        }
+    }
+}
