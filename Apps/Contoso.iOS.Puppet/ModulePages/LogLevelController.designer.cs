5b86284eb77f16dd145c5646e413da2b5d269041
Ivan Matkov <v-ivmatk@microsoft.com>
2017-05-02 16:05:16 +0300

    Mobile Center settings page in Puppet.iOS

---
+++ Apps/Contoso.iOS.Puppet/ModulePages/LogLevelController.designer.cs
@@ -0,0 +1,21 @@
+﻿// WARNING
+//
+// This file has been generated automatically by Xamarin Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+using UIKit;
+
+namespace Contoso.iOS.Puppet
+{
+    [Register("LogLevelController")]
+    partial class LogLevelController
+    {
+        void ReleaseDesignerOutlets()
+        {
+        }
+    }
+}
--- Apps/Contoso.iOS.Puppet/ModulePages/MobileCenterController.cs
+++ Apps/Contoso.iOS.Puppet/ModulePages/MobileCenterController.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using Foundation;
 using Microsoft.Azure.Mobile;
 using UIKit;
 
@@ -6,6 +8,22 @@ namespace Contoso.iOS.Puppet
 {
     public partial class MobileCenterController : UITableViewController
     {
+        private static readonly IDictionary<LogLevel, Action<string, string>> LogFunctions = new Dictionary<LogLevel, Action<string, string>> {
+            { LogLevel.Verbose, MobileCenterLog.Verbose },
+            { LogLevel.Debug, MobileCenterLog.Debug },
+            { LogLevel.Info, MobileCenterLog.Info },
+            { LogLevel.Warn, MobileCenterLog.Warn },
+            { LogLevel.Error, MobileCenterLog.Error }
+        };
+        private static readonly IDictionary<LogLevel, string> LogLevelNames = new Dictionary<LogLevel, string> {
+            { LogLevel.Verbose, Constants.Verbose },
+            { LogLevel.Debug, Constants.Debug },
+            { LogLevel.Info, Constants.Info },
+            { LogLevel.Warn, Constants.Warning },
+            { LogLevel.Error, Constants.Error }
+        };
+        private LogLevel mLogWriteLevel = LogLevel.Verbose;
+
         public MobileCenterController(IntPtr handle) : base(handle)
         {
         }
@@ -14,6 +32,34 @@ namespace Contoso.iOS.Puppet
         {
             base.ViewDidLoad();
             MobileCenterEnabledSwitch.On = MobileCenter.Enabled;
+            LogLevelLabel.Text = LogLevelNames[MobileCenter.LogLevel];
+            LogWriteLevelLabel.Text = LogLevelNames[mLogWriteLevel];
+        }
+
+        public override void PrepareForSegue(UIStoryboardSegue segue, NSObject sender)
+        {
+            base.PrepareForSegue(segue, sender);
+
+            var logLevelContoller = segue.DestinationViewController as LogLevelController;
+            if (logLevelContoller != null)
+            {
+                switch (segue.Identifier)
+                {
+                    case "LogLevel":
+                        logLevelContoller.LevelSelected += level =>
+                        {
+                            MobileCenter.LogLevel = level;
+                        };
+                        break;
+                    case "LogWriteLevel":
+                        logLevelContoller.LevelSelected += level =>
+                        {
+                            mLogWriteLevel = level;
+                            LogWriteLevelLabel.Text = LogLevelNames[mLogWriteLevel];
+                        };
+                        break;
+                }
+            }
         }
 
         partial void UpdateEnabled()
@@ -24,6 +70,9 @@ namespace Contoso.iOS.Puppet
 
         partial void WriteLog()
         {
+            string message = LogWriteMessage.Text;
+            string tag = LogWriteTag.Text;
+            LogFunctions[mLogWriteLevel](tag, message);
         }
     }
 }
