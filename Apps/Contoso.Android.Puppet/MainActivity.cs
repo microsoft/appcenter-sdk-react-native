e2fdaa8e40343991a8bb70e31bdf19d00b7477ac
Ivan Matkov <v-ivmatk@microsoft.com>
2017-05-03 17:43:09 +0300

    Add mobile center callbacks and dialogs to Puppet.Android

--- Apps/Contoso.Android.Puppet/MainActivity.cs
+++ Apps/Contoso.Android.Puppet/MainActivity.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Generic;
-using Android.App;
+﻿using Android.App;
 using Android.Content.PM;
 using Android.OS;
 using Android.Support.Design.Widget;
@@ -12,6 +11,8 @@ using Microsoft.Azure.Mobile.Distribute;
 
 namespace Contoso.Android.Puppet
 {
+    using AlertDialog = global::Android.Support.V7.App.AlertDialog;
+
     [Activity(Label = "SXPuppet", Icon = "@drawable/icon", Theme = "@style/PuppetTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : AppCompatActivity
     {
@@ -24,35 +25,163 @@ namespace Contoso.Android.Puppet
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
 
-			// Get the ViewPager and set it's PagerAdapter so that it can display items
+            // Get the ViewPager and set it's PagerAdapter so that it can display items
             var viewPager = FindViewById(Resource.Id.viewpager) as ViewPager;
             viewPager.Adapter = new PagerAdapter(SupportFragmentManager, this);
 
-			// Give the TabLayout the ViewPager
-			var tabLayout = FindViewById(Resource.Id.tablayout) as TabLayout;
-			tabLayout.SetupWithViewPager(viewPager);
+            // Give the TabLayout the ViewPager
+            var tabLayout = FindViewById(Resource.Id.tablayout) as TabLayout;
+            tabLayout.SetupWithViewPager(viewPager);
 
             // Mobile Center integration
             MobileCenterLog.Assert(LogTag, "MobileCenter.LogLevel=" + MobileCenter.LogLevel);
             MobileCenter.LogLevel = LogLevel.Verbose;
             MobileCenterLog.Info(LogTag, "MobileCenter.LogLevel=" + MobileCenter.LogLevel);
+            MobileCenterLog.Info(LogTag, "MobileCenter.Configured=" + MobileCenter.Configured);
+
+            // Set event handlers
+            Crashes.SendingErrorReport += SendingErrorReportHandler;
+            Crashes.SentErrorReport += SentErrorReportHandler;
+            Crashes.FailedToSendErrorReport += FailedToSendErrorReportHandler;
+
+            // Set callbacks
+            Crashes.ShouldProcessErrorReport = ShouldProcess;
+            Crashes.ShouldAwaitUserConfirmation = ConfirmationHandler;
+
+            Distribute.ReleaseAvailable = OnReleaseAvailable;
+
+            MobileCenterLog.Assert(LogTag, "MobileCenter.Configured=" + MobileCenter.Configured);
+            MobileCenterLog.Assert(LogTag, "MobileCenter.InstallId (before configure)=" + MobileCenter.InstallId);
             MobileCenter.SetLogUrl("https://in-integration.dev.avalanch.es");
             Distribute.SetInstallUrl("http://install.asgard-int.trafficmanager.net");
             Distribute.SetApiUrl("https://asgard-int.trafficmanager.net/api/v0.1");
             MobileCenter.Start("bff0949b-7970-439d-9745-92cdc59b10fe", typeof(Analytics), typeof(Crashes), typeof(Distribute));
-            Analytics.TrackEvent("myEvent", new Dictionary<string, string> { { "someKey", "someValue" } });
+
             MobileCenterLog.Info(LogTag, "MobileCenter.InstallId=" + MobileCenter.InstallId);
-            MobileCenterLog.Info(LogTag, "MobileCenter.HasCrashedInLastSession=" + Crashes.HasCrashedInLastSession);
+            MobileCenterLog.Info(LogTag, "Crashes.HasCrashedInLastSession=" + Crashes.HasCrashedInLastSession);
             Crashes.GetLastSessionCrashReportAsync().ContinueWith(report =>
             {
-                MobileCenterLog.Info(LogTag, "MobileCenter.LastThrowable=" + report.Result?.AndroidDetails?.Throwable);
-                MobileCenterLog.Info(LogTag, "MobileCenter.LastException=" + report.Result?.Exception);
+                MobileCenterLog.Info(LogTag, " Crashes.LastSessionCrashReport.Exception=" + report.Result?.Exception);
             });
         }
 
-        protected override void OnDestroy()
+        void SendingErrorReportHandler(object sender, SendingErrorReportEventArgs e)
         {
-            // This will crash with super not called exception, pure java exception that we want to test
+            MobileCenterLog.Info(LogTag, "Sending error report");
+
+            var args = e as SendingErrorReportEventArgs;
+            ErrorReport report = args.Report;
+
+            //test some values
+            if (report.Exception != null)
+            {
+                MobileCenterLog.Info(LogTag, report.Exception.ToString());
+            }
+            else if (report.AndroidDetails != null)
+            {
+                MobileCenterLog.Info(LogTag, report.AndroidDetails.ThreadName);
+            }
+        }
+
+        void SentErrorReportHandler(object sender, SentErrorReportEventArgs e)
+        {
+            MobileCenterLog.Info(LogTag, "Sent error report");
+
+            var args = e as SentErrorReportEventArgs;
+            ErrorReport report = args.Report;
+
+            //test some values
+            if (report.Exception != null)
+            {
+                MobileCenterLog.Info(LogTag, report.Exception.ToString());
+            }
+            else
+            {
+                MobileCenterLog.Info(LogTag, "No system exception was found");
+            }
+
+            if (report.AndroidDetails != null)
+            {
+                MobileCenterLog.Info(LogTag, report.AndroidDetails.ThreadName);
+            }
+        }
+
+        void FailedToSendErrorReportHandler(object sender, FailedToSendErrorReportEventArgs e)
+        {
+            MobileCenterLog.Info(LogTag, "Failed to send error report");
+
+            var args = e as FailedToSendErrorReportEventArgs;
+            ErrorReport report = args.Report;
+
+            //test some values
+            if (report.Exception != null)
+            {
+                MobileCenterLog.Info(LogTag, report.Exception.ToString());
+            }
+            else if (report.AndroidDetails != null)
+            {
+                MobileCenterLog.Info(LogTag, report.AndroidDetails.ThreadName);
+            }
+
+            if (e.Exception != null)
+            {
+                MobileCenterLog.Info(LogTag, "There is an exception associated with the failure");
+            }
+        }
+
+        bool ShouldProcess(ErrorReport report)
+        {
+            MobileCenterLog.Info(LogTag, "Determining whether to process error report");
+            return true;
+        }
+
+        bool ConfirmationHandler()
+        {
+            var builder = new AlertDialog.Builder(this);
+            builder.SetTitle(Resource.String.crash_confirmation_dialog_title);
+            builder.SetMessage(Resource.String.crash_confirmation_dialog_message);
+            builder.SetPositiveButton(Resource.String.crash_confirmation_dialog_title, delegate
+            {
+                Crashes.NotifyUserConfirmation(UserConfirmation.Send);
+            });
+            builder.SetNegativeButton(Resource.String.crash_confirmation_dialog_title, delegate
+            {
+                Crashes.NotifyUserConfirmation(UserConfirmation.DontSend);
+            });
+            builder.SetNeutralButton(Resource.String.crash_confirmation_dialog_title, delegate
+            {
+                Crashes.NotifyUserConfirmation(UserConfirmation.AlwaysSend);
+            });
+            builder.Create().Show();
+            return true;
+        }
+
+        bool OnReleaseAvailable(ReleaseDetails releaseDetails)
+        {
+            MobileCenterLog.Info(LogTag, "OnReleaseAvailable id=" + releaseDetails.Id
+                                            + " version=" + releaseDetails.Version
+                                            + " releaseNotesUrl=" + releaseDetails.ReleaseNotesUrl);
+            var custom = releaseDetails.ReleaseNotes?.ToLowerInvariant().Contains("custom") ?? false;
+            if (custom)
+            {
+                var builder = new AlertDialog.Builder(this);
+                builder.SetTitle(string.Format(GetString(Resource.String.version_x_available), releaseDetails.ShortVersion));
+                builder.SetMessage(releaseDetails.ReleaseNotes);
+                builder.SetPositiveButton(Microsoft.Azure.Mobile.Distribute.Resource.String.mobile_center_distribute_update_dialog_download, delegate
+                {
+                    Distribute.NotifyUpdateAction(UpdateAction.Update);
+                });
+                builder.SetCancelable(false);
+                if (!releaseDetails.MandatoryUpdate)
+                {
+                    builder.SetNegativeButton(Microsoft.Azure.Mobile.Distribute.Resource.String.mobile_center_distribute_update_dialog_postpone, delegate
+                    {
+                        Distribute.NotifyUpdateAction(UpdateAction.Postpone);
+                    });
+                }
+                builder.Create().Show();
+            }
+            return custom;
         }
     }
 }
