631070fd103b55caa74851e7ddd197d4088d657d
Alexander Chocron <alchocro@microsoft.com>
2017-05-04 11:18:09 -0700

    Create empty uwp test projects

---
+++ Tests/Microsoft.Azure.Mobile.Analytics.Test.UWP/UnitTest.cs
@@ -0,0 +1,15 @@
+﻿
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Microsoft.Azure.Mobile.Analytics.Test.UWP
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
