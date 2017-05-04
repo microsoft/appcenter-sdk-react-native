a6053c9efb40ed95db65769d31a9594ec4d7cd95
Guillaume Perrot <guperrot@microsoft.com>
2017-05-04 12:30:21 -0700

    Bump version

--- Tests/Microsoft.Azure.Mobile.Test.Windows/Properties/AssemblyInfo.cs
+++ Tests/Microsoft.Azure.Mobile.Test.Windows/Properties/AssemblyInfo.cs
@@ -16,5 +16,5 @@ using System.Runtime.InteropServices;
 
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("0.0.0.0")]
-[assembly: AssemblyFileVersion("0.9.3.0")]
-[assembly: AssemblyInformationalVersion("0.9.3-SNAPSHOT")]
+[assembly: AssemblyFileVersion("0.10.0.0")]
+[assembly: AssemblyInformationalVersion("0.10.0-SNAPSHOT")]
