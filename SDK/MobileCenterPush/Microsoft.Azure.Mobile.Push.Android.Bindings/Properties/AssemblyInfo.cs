4dcd39adbd6f57793658cff636ea5e3e49368959
Alexander Chocron <alchocro@microsoft.com>
2017-05-05 13:22:32 -0700

    Change ios minimum version and update assembly info files

--- SDK/MobileCenterPush/Microsoft.Azure.Mobile.Push.Android.Bindings/Properties/AssemblyInfo.cs
+++ SDK/MobileCenterPush/Microsoft.Azure.Mobile.Push.Android.Bindings/Properties/AssemblyInfo.cs
@@ -13,17 +13,16 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
-// The form "{Major}.{Minor}.*" will automatically update the build and revision,
-// and "{Major}.{Minor}.{Build}.*" will update just the revision.
-
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("0.0.0.0")]
-[assembly: AssemblyFileVersion("0.9.1.0")]
-[assembly: AssemblyInformationalVersion("0.9.1-SNAPSHOT")]
-
-// The following attributes are used to specify the signing key for the assembly, 
-// if desired. See the Mono documentation for more information about signing.
-
-//[assembly: AssemblyDelaySign(false)]
-//[assembly: AssemblyKeyFile("")]
-
+[assembly: AssemblyFileVersion("0.10.1.0")]
+[assembly: AssemblyInformationalVersion("0.10.1-SNAPSHOT")]
--- SDK/MobileCenterPush/Microsoft.Azure.Mobile.Push.iOS.Bindings/Properties/AssemblyInfo.cs
+++ SDK/MobileCenterPush/Microsoft.Azure.Mobile.Push.iOS.Bindings/Properties/AssemblyInfo.cs
@@ -15,19 +15,22 @@ using Foundation;
 [assembly: AssemblyTitle("Microsoft.Azure.Mobile.Push.iOS.Bindings")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Microsoft Corporation")]
 [assembly: AssemblyProduct("")]
+[assembly: AssemblyCopyright("Microsoft Corp. All rights reserved.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
-// The form "{Major}.{Minor}.*" will automatically update the build and revision,
-// and "{Major}.{Minor}.{Build}.*" will update just the revision.
-
-[assembly: AssemblyVersion("1.0.*")]
-
-// The following attributes are used to specify the signing key for the assembly, 
-// if desired. See the Mono documentation for more information about signing.
-
-//[assembly: AssemblyDelaySign(false)]
-//[assembly: AssemblyKeyFile("")]
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.10.1.0")]
+[assembly: AssemblyInformationalVersion("0.10.1-SNAPSHOT")]
