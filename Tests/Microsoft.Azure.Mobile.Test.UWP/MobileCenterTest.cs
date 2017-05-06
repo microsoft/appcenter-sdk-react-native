5fd6334951d47c87a6f3827502f9ef83f0909f15
Ivan Matkov <v-ivmatk@microsoft.com>
2017-05-06 13:13:16 +0300

    Add check sdk platform id in UWP tests

---
+++ Tests/Microsoft.Azure.Mobile.Test.UWP/MobileCenterTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Microsoft.Azure.Mobile.Test.UWP
+{
+    [TestClass]
+    public class MobileCenterTest
+    {
+        [TestInitialize]
+        public void InitializeMobileCenterTest()
+        {
+            MobileCenter.Instance = null;
+        }
+
+        [TestMethod]
+        public void VerifyPlatformId()
+        {
+            MobileCenter.Configure("uwp=appsecret");
+            Assert.IsTrue(MobileCenter.Configured);
+        }
+    }
+}
---
+++ Tests/Microsoft.Azure.Mobile.Test.UWP/Utils/DeviceInformationHelperTest.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Azure.Mobile.Utils;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Microsoft.Azure.Mobile.Test.UWP.Utils
+{
+    [TestClass]
+    public class DeviceInformationHelperTest
+    {
+        [TestMethod]
+        public void VerifySdkName()
+        {
+            var device = new DeviceInformationHelper().GetDeviceInformation();
+            Assert.AreEqual(device.SdkName, "mobilecenter.uwp");
+        }
+    }
+}
