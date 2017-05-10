de6c5850353f549787305a339657f8cb7cfd209c
Ivan Matkov <v-ivmatk@microsoft.com>
2017-05-10 17:37:45 +0300

    Implement DeviceInformationHelper for WinForms

--- SDK/MobileCenter/Microsoft.Azure.Mobile.WinForms/Properties/AssemblyInfo.cs
+++ SDK/MobileCenter/Microsoft.Azure.Mobile.WinForms/Properties/AssemblyInfo.cs
@@ -14,14 +14,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("e8297044-32bb-4cc9-b580-2535c42cdde4")]
-
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
@@ -34,3 +26,5 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("0.11.0.0")]
+[assembly: AssemblyInformationalVersion("0.11.0-SNAPSHOT")]
+[assembly: ComVisible(false)]
--- SDK/MobileCenter/Microsoft.Azure.Mobile.WinForms/Utils/DeviceInformationHelper.cs
+++ SDK/MobileCenter/Microsoft.Azure.Mobile.WinForms/Utils/DeviceInformationHelper.cs
@@ -1,14 +1,78 @@
 ﻿using System;
+using System.Management;
+using System.Reflection;
+using System.Windows.Forms;
 
 namespace Microsoft.Azure.Mobile.Utils
 {
-    public class DeviceInformationHelper : IDeviceInformationHelper
+    /// <summary>
+    /// Implements the abstract device information helper class
+    /// </summary>
+    public class DeviceInformationHelper : AbstractDeviceInformationHelper
     {
         public static event EventHandler InformationInvalidated;
-
-        public Ingestion.Models.Device GetDeviceInformation()
+        
+        protected override string GetSdkName()
         {
-            throw new NotImplementedException();
+            return "mobilecenter.winforms";
+        }
+
+        protected override string GetDeviceModel()
+        {
+            var managementClass = new ManagementClass("Win32_ComputerSystem");
+            foreach (var managementObject in managementClass.GetInstances())
+            {
+                return (string)managementObject["Model"];
+            }
+            return string.Empty;
+        }
+
+        protected override string GetAppNamespace()
+        {
+             return Assembly.GetEntryAssembly().EntryPoint.DeclaringType.Namespace;
+        }
+
+        protected override string GetDeviceOemName()
+        {
+            var managementClass = new ManagementClass("Win32_ComputerSystem");
+            foreach (var managementObject in managementClass.GetInstances())
+            {
+                return (string)managementObject["Manufacturer"];
+            }
+            return string.Empty;
+        }
+
+        protected override string GetOsName()
+        {
+            return Environment.OSVersion.Platform.ToString();
+        }
+
+        protected override string GetOsBuild()
+        {
+            var version = Environment.OSVersion.Version;
+            return $"{version.Major}.{version.Minor}.{version.Build}.{version.Revision}";
+        }
+
+        protected override string GetOsVersion()
+        {
+            var version = Environment.OSVersion.Version;
+            return $"{version.Major}.{version.Minor}.{version.Build}";
+        }
+
+        protected override string GetAppVersion()
+        {
+            return Application.ProductVersion;
+        }
+
+        protected override string GetAppBuild()
+        {
+            return Application.ProductVersion;
+        }
+
+        protected override string GetScreenSize()
+        {
+            var size = SystemInformation.PrimaryMonitorSize;
+            return $"{size.Width}x{size.Height}";
         }
     }
 }
