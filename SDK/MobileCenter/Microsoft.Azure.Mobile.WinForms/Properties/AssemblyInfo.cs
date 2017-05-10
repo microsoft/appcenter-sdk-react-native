89fb6a05d2e7b4d8d5ba23ea9d11a18a8cfcefd7
Ivan Matkov <v-ivmatk@microsoft.com>
2017-05-10 16:03:52 +0300

    Implement ApplicationSettings for WinForms

--- SDK/MobileCenter/Microsoft.Azure.Mobile.WinForms/Properties/AssemblyInfo.cs
+++ SDK/MobileCenter/Microsoft.Azure.Mobile.WinForms/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Microsoft.Azure.Mobile.WinForms")]
+[assembly: AssemblyTitle("Microsoft.Azure.Mobile")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Microsoft.Azure.Mobile.WinForms")]
+[assembly: AssemblyProduct("Microsoft.Azure.Mobile")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -33,4 +33,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("0.8.0.0")]
+[assembly: AssemblyFileVersion("0.11.0.0")]
--- SDK/MobileCenter/Microsoft.Azure.Mobile.WinForms/Utils/ApplicationSettings.cs
+++ SDK/MobileCenter/Microsoft.Azure.Mobile.WinForms/Utils/ApplicationSettings.cs
@@ -1,30 +1,50 @@
-﻿using System;
+﻿using System.ComponentModel;
+using System.Configuration;
+using System.IO;
+using System.Reflection;
 
 namespace Microsoft.Azure.Mobile.Utils
 {
     public class ApplicationSettings : IApplicationSettings
     {
-        public void Remove(string key)
+        private Configuration config;
+
+        public ApplicationSettings()
         {
-            throw new NotImplementedException();
+            var location = Assembly.GetExecutingAssembly().Location;
+            var path = Path.Combine(Path.GetDirectoryName(location), "MobileCenter.config");
+            var executionFileMap = new ExeConfigurationFileMap { ExeConfigFilename = path };
+            config = ConfigurationManager.OpenMappedExeConfiguration(executionFileMap, ConfigurationUserLevel.None);
         }
 
         public object this[string key]
         {
             get
             {
-                throw new NotImplementedException();
+                return config.AppSettings.Settings[key]?.Value;
             }
-
             set
             {
-                throw new NotImplementedException();
+                var invariant = value != null ? TypeDescriptor.GetConverter(value.GetType()).ConvertToInvariantString(value) : null;
+                config.AppSettings.Settings.Add(new KeyValueConfigurationElement(key, invariant));
+                config.Save();
             }
+        }
+        public void Remove(string key)
+        {
+            config.AppSettings.Settings.Remove(key);
+            config.Save();
         }
 
         public T GetValue<T>(string key, T defaultValue)
         {
-            throw new NotImplementedException();
+            if (config.AppSettings.Settings[key] != null)
+            {
+                var value = config.AppSettings.Settings[key].Value;
+                return (T)TypeDescriptor.GetConverter(typeof(T)).ConvertFromInvariantString(value);
+            }
+            this[key] = defaultValue;
+            return defaultValue;
         }
     }
 }
