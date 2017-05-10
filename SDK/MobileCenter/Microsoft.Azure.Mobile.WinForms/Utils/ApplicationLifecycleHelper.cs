c26dbf94cf8e04db16c5ce32c2c869e79f72acf2
Ivan Matkov <v-ivmatk@microsoft.com>
2017-05-10 18:08:45 +0300

    Add UnhandledException in ApplicationLifecycleHelper for WinForms

--- SDK/MobileCenter/Microsoft.Azure.Mobile.WinForms/Utils/ApplicationLifecycleHelper.cs
+++ SDK/MobileCenter/Microsoft.Azure.Mobile.WinForms/Utils/ApplicationLifecycleHelper.cs
@@ -4,6 +4,15 @@ namespace Microsoft.Azure.Mobile.Utils
 {
     public class ApplicationLifecycleHelper : IApplicationLifecycleHelper
     {
+        public ApplicationLifecycleHelper()
+        {
+            Enabled = true;
+            AppDomain.CurrentDomain.UnhandledException += (sender, eventArgs) =>
+            {
+                UnhandledExceptionOccurred?.Invoke(sender, new UnhandledExceptionOccurredEventArgs((Exception)eventArgs.ExceptionObject));
+            };
+        }
+
         public bool Enabled { get; set; }
 
         public event EventHandler ApplicationSuspended;
