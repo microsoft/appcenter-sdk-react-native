9ffbade4ee9d3d7454141efee687454cc3eb0930
Guillaume Perrot <guperrot@microsoft.com>
2017-05-11 15:09:12 -0700

    Start version 0.11.2

--- SDK/MobileCenterDistribute/Microsoft.Azure.Mobile.Distribute.Android/Properties/AssemblyInfo.cs
+++ SDK/MobileCenterDistribute/Microsoft.Azure.Mobile.Distribute.Android/Properties/AssemblyInfo.cs
@@ -25,5 +25,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("0.0.0.0")]
-[assembly: AssemblyFileVersion("0.11.1.0")]
-[assembly: AssemblyInformationalVersion("0.11.1-SNAPSHOT")]
+[assembly: AssemblyFileVersion("0.11.2.0")]
+[assembly: AssemblyInformationalVersion("0.11.2-SNAPSHOT")]
