e9883152260d5ff0fdb81b39573732a012eece7f
Ivan Matkov <v-ivmatk@microsoft.com>
2017-05-17 12:59:22 +0300

    Add controls to Contoso.WinForms.Puppet

---
+++ Apps/Contoso.WinForms.Puppet/Constants.cs
@@ -0,0 +1,11 @@
+﻿namespace Contoso.WinForms.Puppet
+{
+    public static class Constants
+    {
+        public const string Verbose = "Verbose";
+        public const string Debug = "Debug";
+        public const string Info = "Info";
+        public const string Warning = "Warning";
+        public const string Error = "Error";
+    }
+}
