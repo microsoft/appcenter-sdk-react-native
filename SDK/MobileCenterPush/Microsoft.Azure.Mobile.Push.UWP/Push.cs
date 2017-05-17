cc12b7f8e37d61e09f134436c8258d922637eb7b
Guillaume Perrot <guperrot@microsoft.com>
2017-05-16 17:11:04 -0700

    Fix unusued import

--- SDK/MobileCenterPush/Microsoft.Azure.Mobile.Push.UWP/Push.cs
+++ SDK/MobileCenterPush/Microsoft.Azure.Mobile.Push.UWP/Push.cs
@@ -5,7 +5,6 @@ using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
-using System.Threading;
 using System.Threading.Tasks;
 using Windows.ApplicationModel.Activation;
 using Windows.Data.Xml.Dom;
