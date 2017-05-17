6173cacf2d790b11b187d2decbf3d498c1ef524a
Guillaume Perrot <guperrot@microsoft.com>
2017-05-16 17:21:36 -0700

    Fix push unit test

--- Tests/Microsoft.Azure.Mobile.Push.NET/Push.cs
+++ Tests/Microsoft.Azure.Mobile.Push.NET/Push.cs
@@ -4,8 +4,7 @@ namespace Microsoft.Azure.Mobile.Push
 {
     public partial class Push : MobileCenterService
     {
-
-        private void InstanceRegister()
+        private void ApplyEnabledState()
         {
         }
 
