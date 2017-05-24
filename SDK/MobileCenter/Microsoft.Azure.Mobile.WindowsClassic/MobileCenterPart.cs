211782019d913534fe2bf58bdebdbb7f2007fba9
Alexander Chocron <alchocro@microsoft.com>
2017-05-24 09:05:56 -0700

    Change WinFOrms to WindowsClassic

---
+++ SDK/MobileCenter/Microsoft.Azure.Mobile.WindowsClassic/MobileCenterPart.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.Azure.Mobile.Utils;
+
+namespace Microsoft.Azure.Mobile
+{
+    public partial class MobileCenter
+    {
+        private const string PlatformIdentifier = "windowsclassicdesktop";
+    }
+}
---
+++ SDK/MobileCenter/Microsoft.Azure.Mobile.WindowsClassic/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Microsoft.Azure.Mobile.WindowsClassic")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Microsoft.Azure.Mobile.WindowsClassic")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("1e9ec0aa-3e32-4551-80c4-e4f3e53d7590")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
