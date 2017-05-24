7b946b4ac44e47f832f9f3f099b340dcd883dbda
Alexander Chocron <alchocro@microsoft.com>
2017-05-24 11:48:30 -0700

    Add windowsclassic to build windows sln and correct assembly infos

--- SDK/MobileCenter/Microsoft.Azure.Mobile.UWP/Properties/AssemblyInfo.cs
+++ SDK/MobileCenter/Microsoft.Azure.Mobile.UWP/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Microsoft.Azure.Mobile")]
+[assembly: AssemblyTitle("Microsoft.Azure.Mobile.UWP")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Microsoft.Azure.Mobile")]
+[assembly: AssemblyProduct("Microsoft.Azure.Mobile.UWP")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
