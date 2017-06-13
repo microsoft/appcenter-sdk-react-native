4a271b37962861cf12eb007a613d1fb84cf77ca8
Bret Johnson <bret.johnson@microsoft.com>
2017-06-12 21:51:51 -0400

    Updated to give a much better error message if matching text isn't found in AppDelegate.m, so can't make the source fixups. Also made it so that if whitespace is present at the end of matching lines, it doesn't screw things up--we still match ok.

--- mobile-center-link-scripts/src/ios/AppDelegate.js
+++ mobile-center-link-scripts/src/ios/AppDelegate.js
@@ -9,8 +9,16 @@ var AppDelegate = function (file) {
 
 AppDelegate.prototype.addHeader = function (header) {
     if (this.appDelegateContents.indexOf(header) === -1) {
-        var pattern = '#import "AppDelegate.h"';
-        this.appDelegateContents = this.appDelegateContents.replace(pattern, `${pattern}\n${header}\n`);
+        const match = this.appDelegateContents.match(/#import "AppDelegate.h"[ \t]*\r*\n/);
+        if (match === null) {
+            throw Error(`
+        Could not find line '#import "AppDelegate.h"' in file AppDelegate.m.
+        Update AppDelegate.m so that text is present, as we match on it and insert '` + header + `' after for Mobile Center SDK integration.
+`);
+        }
+
+        const existingLine = match[0];
+        this.appDelegateContents = this.appDelegateContents.replace(existingLine, `${existingLine}${header}\n`);
         debug('Added header', header, 'to file', this.appDelegatePath);
     } else {
         debug(`"${header}"" header already added to AppDelegate.m`);
@@ -19,8 +27,16 @@ AppDelegate.prototype.addHeader = function (header) {
 
 AppDelegate.prototype.addInitCode = function (code) {
     if (this.appDelegateContents.indexOf(code) === -1) {
-        var pattern = this.appDelegateContents.match(/NSURL \*jsCodeLocation;\r*\n/)[0];
-        this.appDelegateContents = this.appDelegateContents.replace(pattern, `${pattern}\n${code}\n`);
+        const match = this.appDelegateContents.match(/NSURL \*jsCodeLocation;[ \t]*\r*\n/);
+        if (match === null) {
+            throw Error(`
+        Could not find line "NSURL \*jsCodeLocation;" in file AppDelegate.m.
+        Update AppDelegate.m so that text is present, as we match on it and insert '` + code + `' after for Mobile Center SDK integration.
+`);
+        }
+
+        const existingLine = match[0];
+        this.appDelegateContents = this.appDelegateContents.replace(existingLine, `${existingLine}\n${code}\n`);
         debug('Added code', code, 'to file', this.appDelegatePath);
     } else {
         debug(`Looks like ${code} is already added to AppDelegate.m`);
