808e5e47a26437156758f72291eb739e2bc4a1c8
Guillaume Perrot <guperrot@microsoft.com>
2017-06-13 18:13:52 -0700

    Avoid duplicate start session when resuming with a log

--- Tests/Microsoft.Azure.Mobile.Analytics.Test.Windows/SessionTrackerTest.cs
+++ Tests/Microsoft.Azure.Mobile.Analytics.Test.Windows/SessionTrackerTest.cs
@@ -55,6 +55,20 @@ namespace Microsoft.Azure.Mobile.Analytics.Test.Windows
             _mockChannel.Verify(channel => channel.EnqueueAsync(It.IsAny<StartSessionLog>()), Times.Exactly(2));
         }
 
+        [TestMethod]
+        public void ResumeAfterTimeoutAndSendEvent()
+        {
+            _sessionTracker.Resume();
+            _sessionTracker.Pause();
+            Task.Delay((int)SessionTracker.SessionTimeout).Wait();
+            _mockChannel.Verify(channel => channel.EnqueueAsync(It.IsAny<StartSessionLog>()), Times.Once());
+
+            _sessionTracker.Resume();
+            _mockChannelGroup.Raise(group => group.EnqueuingLog += null, null, new EnqueuingLogEventArgs(new EventLog()));
+
+            _mockChannel.Verify(channel => channel.EnqueueAsync(It.IsAny<StartSessionLog>()), Times.Exactly(2));
+        }
+
         /// <summary>
         /// Verify that after a pause that is not long enough to be a timeout, the session tracker does not send a start session log
         /// </summary>
