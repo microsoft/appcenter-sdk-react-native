0964cbcfe73bab1556541fa0893eff6ea66a5767
Guillaume Perrot <guperrot@microsoft.com>
2017-06-16 14:22:36 -0700

    Reformat a file

--- SDK/MobileCenter/Microsoft.Azure.Mobile.Android/Consumer.cs
+++ SDK/MobileCenter/Microsoft.Azure.Mobile.Android/Consumer.cs
@@ -4,24 +4,24 @@ using Com.Microsoft.Azure.Mobile.Utils.Async;
 
 namespace Microsoft.Azure.Mobile
 {
-	public class Consumer<T> : Java.Lang.Object, IMobileCenterConsumer
-	{
-		T _result;
+    public class Consumer<T> : Java.Lang.Object, IMobileCenterConsumer
+    {
+        T _result;
 
-		public Task<T> Task { get; }
+        public Task<T> Task { get; }
 
-		public Consumer()
-		{
-			Task = new Task<T>(() => _result);
-		}
+        public Consumer()
+        {
+            Task = new Task<T>(() => _result);
+        }
 
-		public void Accept(Java.Lang.Object result)
-		{
-			if (result != null)
-			{
-				_result = (T)Convert.ChangeType(result, typeof(T));
-			}
-			Task.Start();
-		}
-	}
+        public void Accept(Java.Lang.Object result)
+        {
+            if (result != null)
+            {
+                _result = (T)Convert.ChangeType(result, typeof(T));
+            }
+            Task.Start();
+        }
+    }
 }
