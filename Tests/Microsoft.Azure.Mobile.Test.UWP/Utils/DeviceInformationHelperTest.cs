0280e0b0292ff557e8d9da338f4a780e57137352
Murat Baysangurov <v-mubays@microsoft.com>
2017-06-30 14:40:05 +0300

    DeviceInformationHelper unit test added

--- Tests/Microsoft.Azure.Mobile.Test.UWP/Utils/DeviceInformationHelperTest.cs
+++ Tests/Microsoft.Azure.Mobile.Test.UWP/Utils/DeviceInformationHelperTest.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.Azure.Mobile.Utils;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
 using System.Threading.Tasks;
 
 namespace Microsoft.Azure.Mobile.Test.UWP.Utils
@@ -28,6 +30,31 @@ namespace Microsoft.Azure.Mobile.Test.UWP.Utils
 
             var device = Task.Run(() => new DeviceInformationHelper().GetDeviceInformationAsync()).Result;
             Assert.AreEqual(device.CarrierCountry, CountryCode);
+        }
+
+        /// <summary>
+        /// Verify screen size provider
+        /// </summary>
+        [TestMethod]
+        public void VerifyScreenSizeProvider()
+        {
+            const string testScreenSize = "screen_size";
+            var informationInvalidated = false;
+            var screenSizeProviderMock = new Mock<IScreenSizeProvider>();
+            var screenSizeProviderFactoryMock = new Mock<IScreenSizeProviderFactory>();
+
+            screenSizeProviderMock.Setup(provider => provider.ScreenSize).Returns(testScreenSize);
+            screenSizeProviderFactoryMock.Setup(factory => factory.CreateScreenSizeProvider()).Returns(screenSizeProviderMock.Object);
+            DeviceInformationHelper.SetScreenSizeProviderFactory(screenSizeProviderFactoryMock.Object);
+
+            // Screen size is returned from screen size provider
+            var device = Task.Run(() => new DeviceInformationHelper().GetDeviceInformationAsync()).Result;
+            Assert.AreEqual(device.ScreenSize, testScreenSize);
+
+            // InformationInvalidated is invoked when ScreenSizeChanged event is raised
+            DeviceInformationHelper.InformationInvalidated += (sender, args) => { informationInvalidated = true; };
+            screenSizeProviderMock.Raise(provider => provider.ScreenSizeChanged += null, EventArgs.Empty);
+            Assert.IsTrue(informationInvalidated);
         }
     }
 }
