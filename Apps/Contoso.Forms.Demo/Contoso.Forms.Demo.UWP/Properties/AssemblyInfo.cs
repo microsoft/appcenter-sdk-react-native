1ee790c748d98446a738526a95650bd2eea8c2f2
Guillaume Perrot <guperrot@microsoft.com>
2017-07-11 17:58:49 -0700

    Use nuget 0.14.0 in demo

--- Apps/Contoso.Forms.Demo/Contoso.Forms.Demo.UWP/Properties/AssemblyInfo.cs
+++ Apps/Contoso.Forms.Demo/Contoso.Forms.Demo.UWP/Properties/AssemblyInfo.cs
@@ -24,5 +24,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("0.0.0.0")]
-[assembly: AssemblyFileVersion("0.13.1.0")]
+[assembly: AssemblyFileVersion("0.14.0.0")]
 [assembly: ComVisible(false)]
