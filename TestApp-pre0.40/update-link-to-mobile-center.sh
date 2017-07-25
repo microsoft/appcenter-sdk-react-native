46c6697f04add7ccbaa4d97d495798a2ff82a46d
Guillaume Perrot <guperrot@microsoft.com>
2017-07-25 12:21:10 -0700

    Cleanup unused files

--- TestApp-pre0.40/update-link-to-mobile-center.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-./update-npm-packages.sh
-echo 'Unlinking...'
-react-native unlink mobile-center-crashes
-react-native unlink mobile-center-analytics
-echo 'Linking...'
-react-native link mobile-center-crashes
-react-native link mobile-center-analytics
