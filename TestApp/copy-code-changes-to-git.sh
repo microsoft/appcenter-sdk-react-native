6dd456fb9103cc2af72fad8bfda19170bfafd03c
Guillaume Perrot <guperrot@microsoft.com>
2017-08-11 17:21:02 -0700

    Fix changing iOS answers for linker

--- TestApp/copy-code-changes-to-git.sh
+++ TestApp/copy-code-changes-to-git.sh
@@ -3,7 +3,7 @@ echo 'Copy SDK code changes made inside node_modules to git...'
 cd `dirname $0`
 for i in '' '-analytics' '-crashes' '-push'
 do
-    for j in '*.js' 'android' 'ios'
+    for j in '*.js' 'android' 'ios' 'scripts'
     do
         rsync -r node_modules/mobile-center$i/$j ../mobile-center$i
     done
--- mobile-center-link-scripts/src/ios/AppDelegate.js
+++ mobile-center-link-scripts/src/ios/AppDelegate.js
@@ -25,19 +25,35 @@ AppDelegate.prototype.addHeader = function (header) {
     }
 };
 
-AppDelegate.prototype.addInitCode = function (code) {
+AppDelegate.prototype.addInitCode = function (code, oldCodeRegExp) {
+    const oldCodeMatches = oldCodeRegExp ? this.appDelegateContents.match(oldCodeRegExp) : null;
+    if (oldCodeMatches && oldCodeMatches.length > 1) {
+        for (let i = 1; i < oldCodeMatches.length; i++) {
+            this.appDelegateContents = this.appDelegateContents.replace(oldCodeMatches[i], '');
+            debug(`Removed duplicate ${oldCodeRegExp} lines from AppDelegate.m`);
+        }
+    }
     if (this.appDelegateContents.indexOf(code) === -1) {
-        const match = this.appDelegateContents.match(/NSURL \*jsCodeLocation;[ \t]*\r*\n/);
-        if (match === null) {
-            throw Error(`
+
+        /* If new code not found but old code found, replace. */
+        if (oldCodeMatches) {
+            this.appDelegateContents = this.appDelegateContents.replace(oldCodeMatches[0], code);
+            debug('Replaced code', code, 'to file', this.appDelegatePath);
+        }
+        else {
+
+            const match = this.appDelegateContents.match(/NSURL \*jsCodeLocation;[ \t]*\r*\n/);
+            if (match === null) {
+                throw Error(`
         Could not find line "NSURL \*jsCodeLocation;" in file AppDelegate.m.
         Update AppDelegate.m so that text is present, as we match on it and insert '` + code + `' after for Mobile Center SDK integration.
 `);
-        }
+            }
 
-        const existingLine = match[0];
-        this.appDelegateContents = this.appDelegateContents.replace(existingLine, `${existingLine}\n${code}\n`);
-        debug('Added code', code, 'to file', this.appDelegatePath);
+            const existingLine = match[0];
+            this.appDelegateContents = this.appDelegateContents.replace(existingLine, `${existingLine}\n${code}\n`);
+            debug('Added code', code, 'to file', this.appDelegatePath);
+        }
     } else {
         debug(`Looks like ${code} is already added to AppDelegate.m`);
     }
