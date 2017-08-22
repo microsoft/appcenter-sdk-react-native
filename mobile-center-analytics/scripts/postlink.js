0348c7b4e5da61f5797863fcc2ceda5eff30fb2d
Di Hei <dihei@microsoft.com>
2017-08-22 10:30:54 -0700

    add eslint to mobile-center-analytics

--- mobile-center-analytics/scripts/postlink.js
+++ mobile-center-analytics/scripts/postlink.js
@@ -1,28 +1,28 @@
-var rnpmlink = require('mobile-center-link-scripts');
-var npmPackages = require('./../package.json');
+const rnpmlink = require('mobile-center-link-scripts');
+const npmPackages = require('./../package.json');
 
-return rnpmlink.ios.initMobileCenterConfig().then(function (file) {
-    var prompt = npmPackages.rnpm.params[0];
+return rnpmlink.ios.initMobileCenterConfig().then(() => {
+    const prompt = npmPackages.rnpm.params[0];
     prompt.message = prompt.message.replace(/Android/, 'iOS');
 
     return rnpmlink.inquirer.prompt(prompt);
-}).then(function (answer) {
-    var code = answer.whenToEnableAnalytics === 'ALWAYS_SEND' ?
+}).then((answer) => {
+    const code = answer.whenToEnableAnalytics === 'ALWAYS_SEND' ?
         '  [RNAnalytics registerWithInitiallyEnabled:true];  // Initialize Mobile Center analytics' :
-        '  [RNAnalytics registerWithInitiallyEnabled:false];  // Initialize Mobile Center analytics'
+        '  [RNAnalytics registerWithInitiallyEnabled:false];  // Initialize Mobile Center analytics';
     return rnpmlink.ios.initInAppDelegate('#import <RNAnalytics/RNAnalytics.h>', code, /.*\[RNAnalytics register.*/g);
-}).then(function (file) {
-    console.log('Added code to initialize iOS Analytics SDK in ' + file);
+}).then((file) => {
+    console.log(`Added code to initialize iOS Analytics SDK in ${file}`);
     return rnpmlink.ios.addPodDeps([
         { pod: 'MobileCenter/Analytics', version: '0.11.2' },
         { pod: 'RNMobileCenterShared', version: '0.8.1' } // in case people don't link mobile-center (core)
-    ]).catch(function (e) {
+    ]).catch((e) => {
         console.log(`
             Could not install dependencies using CocoaPods.
             Please refer to the documentation to install dependencies manually.
 
             Error Reason - ${e.message}
-        `)
+        `);
         return Promise.resolve();
-    })
+    });
 });
