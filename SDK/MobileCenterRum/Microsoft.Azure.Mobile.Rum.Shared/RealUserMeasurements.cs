2712072ecd045f3b1c877faff98648e1a390cf8b
Guillaume Perrot <guperrot@microsoft.com>
2017-09-13 16:54:12 -0700

    Reformat a file

--- SDK/MobileCenterRum/Microsoft.Azure.Mobile.Rum.Shared/RealUserMeasurements.cs
+++ SDK/MobileCenterRum/Microsoft.Azure.Mobile.Rum.Shared/RealUserMeasurements.cs
@@ -3,43 +3,43 @@ using System.Threading.Tasks;
 
 namespace Microsoft.Azure.Mobile.Rum
 {
-	/// <summary>
-	/// RealUserMeasurements feature.
-	/// </summary>
-	public partial class RealUserMeasurements
-	{
-		/// <summary>
-		/// Check whether the RealUserMeasurements service is enabled or not.
-		/// </summary>
-		/// <returns>A task with result being true if enabled, false if disabled.</returns>
-		public static Task<bool> IsEnabledAsync()
-		{
-			return PlatformIsEnabledAsync();
-		}
+    /// <summary>
+    /// RealUserMeasurements feature.
+    /// </summary>
+    public partial class RealUserMeasurements
+    {
+        /// <summary>
+        /// Check whether the RealUserMeasurements service is enabled or not.
+        /// </summary>
+        /// <returns>A task with result being true if enabled, false if disabled.</returns>
+        public static Task<bool> IsEnabledAsync()
+        {
+            return PlatformIsEnabledAsync();
+        }
 
-		/// <summary>
-		/// Enable or disable the RealUserMeasurements service.
-		/// </summary>
-		/// <returns>A task to monitor the operation.</returns>
-		public static Task SetEnabledAsync(bool enabled)
-		{
-			return PlatformSetEnabledAsync(enabled);
-		}
+        /// <summary>
+        /// Enable or disable the RealUserMeasurements service.
+        /// </summary>
+        /// <returns>A task to monitor the operation.</returns>
+        public static Task SetEnabledAsync(bool enabled)
+        {
+            return PlatformSetEnabledAsync(enabled);
+        }
 
-		/// <summary>
-		/// Configure key.
-		/// </summary>
-		public static void SetRumKey(string rumKey)
+        /// <summary>
+        /// Configure key.
+        /// </summary>
+        public static void SetRumKey(string rumKey)
         {
             PlatformSetRumKey(rumKey);
         }
 
-		/// <summary>
-		/// Configure url to get JSON configuration.
-		/// </summary>
-		public static void SetConfigurationUrl(string url)
-		{
-			PlatformSetConfigurationUrl(url);
-		}
-	}
+        /// <summary>
+        /// Configure url to get JSON configuration.
+        /// </summary>
+        public static void SetConfigurationUrl(string url)
+        {
+            PlatformSetConfigurationUrl(url);
+        }
+    }
 }
