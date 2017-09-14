1e7e3ddcfbd484858d0490bbedabde69411eeb1f
Alexander Chocron <alchocro@microsoft.com>
2017-09-14 10:24:21 -0700

    Make scripts compatible with new cake version and update bootstrappers

--- scripts/send-push.sh
+++ scripts/send-push.sh
@@ -2,14 +2,13 @@
 ENVIRONMENT=""
 PLATFORM=""
 
-# NOTE: For "--group", use '_' instead of spaces.
 for i in "$@"; do
     case $1 in
-        -e|--environment) ENVIRONMENT="-Environment $2"; shift ;;
-        -p|--platform) PLATFORM="-Platform $2"; shift ;;
+        -e|--environment) ENVIRONMENT="-Environment=$2"; shift ;;
+        -p|--platform) PLATFORM="-Platform=$2"; shift ;;
     *) shift ;;
     esac
     shift
 done
 
-./build.sh -s test-tools.cake -t "SendPushNotification" $PLATFORM $ENVIRONMENT
+./build.sh -s test-tools.cake -t="SendPushNotification" $PLATFORM $ENVIRONMENT
