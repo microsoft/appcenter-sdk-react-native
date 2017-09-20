ef88013e27708e92825d75255d403f7b3a1a4947
Guillaume Perrot <guperrot@microsoft.com>
2017-09-20 16:11:05 -0700

    Rum UWP: fix disposable objects

--- SDK/MobileCenterRum/Microsoft.Azure.Mobile.Rum.Windows.Shared/RealUserMeasurements.cs
+++ SDK/MobileCenterRum/Microsoft.Azure.Mobile.Rum.Windows.Shared/RealUserMeasurements.cs
@@ -173,157 +173,143 @@ namespace Microsoft.Azure.Mobile.Rum
                         {
                             MobileCenterLog.Error(LogTag, "Could not run tests.", e);
                         }
+                        finally
+                        {
+                            lock (_serviceLock)
+                            {
+                                if (cancellationToken == _cancellationTokenSource?.Token)
+                                {
+                                    DisposeCancellationSource();
+                                }
+                            }
+                        }
                     }, cancellationToken);
                 }
                 else
                 {
-                    _cancellationTokenSource?.Cancel();
+                    DisposeCancellationSource();
                 }
             }
+        }
+
+        private void DisposeCancellationSource()
+        {
+            MobileCenterLog.Debug(LogTag, "Cleanup cancellation token.");
+            _cancellationTokenSource?.Cancel();
+            _cancellationTokenSource?.Dispose();
+            _cancellationTokenSource = null;
         }
 
         private async Task RunTestsAsync(string rumKey, CancellationToken cancellationToken)
         {
             // TODO handle network state, requires refactoring to reuse ingestion logic here
-            var httpNetworkAdapter = new HttpNetworkAdapter();
-
-            // Get remote configuration.
-            var jsonConfiguration = await httpNetworkAdapter.SendAsync($"{ConfigurationEndpoint}/{ConfigurationFileName}", HttpMethod.Get, Headers, "", cancellationToken);
-
-            // Parse configuration.
-            var configuration = JsonConvert.DeserializeObject<RumConfiguration>(jsonConfiguration);
-
-            // Prepare random weighted selection.
-            var configurationOk = false;
-            if (configuration.TestEndpoints != null)
+            using (var httpNetworkAdapter = new HttpNetworkAdapter())
             {
-                var totalWeight = 0;
-                var weightedEndpoints = new List<TestEndpoint>(configuration.TestEndpoints.Count);
-                foreach (var endpoint in configuration.TestEndpoints)
-                {
-                    var weight = endpoint.Weight;
-                    if (weight > 0)
-                    {
-                        totalWeight += weight;
-                        endpoint.CumulatedWeight = totalWeight;
-                        weightedEndpoints.Add(endpoint);
-                    }
-                }
+                // Get remote configuration.
+                var jsonConfiguration = await httpNetworkAdapter.SendAsync($"{ConfigurationEndpoint}/{ConfigurationFileName}", HttpMethod.Get, Headers, "", cancellationToken);
 
-                // Select n endpoints randomly with respect to weight.
-                var testUrls = new List<TestUrl>();
-                var random = new Random();
-                var testCount = Math.Min(configuration.TestCount, weightedEndpoints.Count);
-                for (var n = 0; n < testCount; n++)
+                // Parse configuration.
+                var configuration = JsonConvert.DeserializeObject<RumConfiguration>(jsonConfiguration);
+
+                // Prepare random weighted selection.
+                var configurationOk = false;
+                if (configuration.TestEndpoints != null)
                 {
-                    // Select random endpoint
-                    var randomWeight = Math.Floor(random.NextDouble() * totalWeight);
-                    TestEndpoint endpoint = null;
-                    for (var i = 0; i < weightedEndpoints.Count; i++)
+                    var totalWeight = 0;
+                    var weightedEndpoints = new List<TestEndpoint>(configuration.TestEndpoints.Count);
+                    foreach (var endpoint in configuration.TestEndpoints)
                     {
-                        var weightedEndpoint = weightedEndpoints[i];
-                        var cumulatedWeight = weightedEndpoint.CumulatedWeight;
-                        if (endpoint == null)
+                        var weight = endpoint.Weight;
+                        if (weight > 0)
                         {
-                            if (randomWeight <= cumulatedWeight)
+                            totalWeight += weight;
+                            endpoint.CumulatedWeight = totalWeight;
+                            weightedEndpoints.Add(endpoint);
+                        }
+                    }
+
+                    // Select n endpoints randomly with respect to weight.
+                    var testUrls = new List<TestUrl>();
+                    var random = new Random();
+                    var testCount = Math.Min(configuration.TestCount, weightedEndpoints.Count);
+                    for (var n = 0; n < testCount; n++)
+                    {
+                        // Select random endpoint
+                        var randomWeight = Math.Floor(random.NextDouble() * totalWeight);
+                        TestEndpoint endpoint = null;
+                        for (var i = 0; i < weightedEndpoints.Count; i++)
+                        {
+                            var weightedEndpoint = weightedEndpoints[i];
+                            var cumulatedWeight = weightedEndpoint.CumulatedWeight;
+                            if (endpoint == null)
                             {
-                                endpoint = weightedEndpoint;
-                                weightedEndpoints.RemoveAt(i--);
+                                if (randomWeight <= cumulatedWeight)
+                                {
+                                    endpoint = weightedEndpoint;
+                                    weightedEndpoints.RemoveAt(i--);
+                                }
+                            }
+
+                            // Update subsequent endpoints cumulated weights since we removed an element.
+                            else
+                            {
+                                weightedEndpoint.CumulatedWeight -= endpoint.Weight;
                             }
                         }
 
-                        // Update subsequent endpoints cumulated weights since we removed an element.
-                        else
+                        // Update total weight since we removed the picked endpoint.
+                        if (endpoint == null)
                         {
-                            weightedEndpoint.CumulatedWeight -= endpoint.Weight;
+                            continue;
                         }
+                        totalWeight -= endpoint.Weight;
+
+                        // Use endpoint to generate test urls.
+                        var protocolSuffix = "";
+                        var measurementType = endpoint.MeasurementType;
+                        if ((measurementType & FlagHttps) > 0)
+                        {
+                            protocolSuffix = "s";
+                        }
+                        var requestId = endpoint.Url;
+
+                        // Handle backward compatibility with FPv1.
+                        var baseUrl = requestId;
+                        if (!requestId.Contains("."))
+                        {
+                            baseUrl += ".clo.footprintdns.com";
+                        }
+
+                        // Port this Javascript behavior regarding url and requestId.
+                        else if (requestId.StartsWith("*") && requestId.Length > 2)
+                        {
+                            var domain = requestId.Substring(2);
+                            var uuid = ProbeId();
+                            baseUrl = uuid + "." + domain;
+                            requestId = domain.Equals("clo.footprintdns.com", StringComparison.OrdinalIgnoreCase) ? uuid : domain;
+                        }
+
+                        // Generate test urls.
+                        var probeId = ProbeId();
+                        var testUrl = $"http{protocolSuffix}://{baseUrl}/apc/{WarmUpImage}?{probeId}";
+                        testUrls.Add(new TestUrl { Url = testUrl, RequestId = requestId, Object = WarmUpImage, Conn = "cold" });
+                        var testImage = (measurementType & FlagSeventeenk) > 0 ? SeventeenkImage : WarmUpImage;
+                        probeId = ProbeId();
+                        testUrl = $"http{protocolSuffix}://{baseUrl}/apc/{testImage}?{probeId}";
+                        testUrls.Add(new TestUrl { Url = testUrl, RequestId = requestId, Object = testImage, Conn = "warm" });
                     }
 
-                    // Update total weight since we removed the picked endpoint.
-                    if (endpoint == null)
+                    // Run the tests.
+                    var stopWatch = new Stopwatch();
+                    for (var i = 0; i < testUrls.Count; i++)
                     {
-                        continue;
-                    }
-                    totalWeight -= endpoint.Weight;
-
-                    // Use endpoint to generate test urls.
-                    var protocolSuffix = "";
-                    var measurementType = endpoint.MeasurementType;
-                    if ((measurementType & FlagHttps) > 0)
-                    {
-                        protocolSuffix = "s";
-                    }
-                    var requestId = endpoint.Url;
-
-                    // Handle backward compatibility with FPv1.
-                    var baseUrl = requestId;
-                    if (!requestId.Contains("."))
-                    {
-                        baseUrl += ".clo.footprintdns.com";
-                    }
-
-                    // Port this Javascript behavior regarding url and requestId.
-                    else if (requestId.StartsWith("*") && requestId.Length > 2)
-                    {
-                        var domain = requestId.Substring(2);
-                        var uuid = ProbeId();
-                        baseUrl = uuid + "." + domain;
-                        requestId = domain.Equals("clo.footprintdns.com", StringComparison.OrdinalIgnoreCase) ? uuid : domain;
-                    }
-
-                    // Generate test urls.
-                    var probeId = ProbeId();
-                    var testUrl = $"http{protocolSuffix}://{baseUrl}/apc/{WarmUpImage}?{probeId}";
-                    testUrls.Add(new TestUrl { Url = testUrl, RequestId = requestId, Object = WarmUpImage, Conn = "cold" });
-                    var testImage = (measurementType & FlagSeventeenk) > 0 ? SeventeenkImage : WarmUpImage;
-                    probeId = ProbeId();
-                    testUrl = $"http{protocolSuffix}://{baseUrl}/apc/{testImage}?{probeId}";
-                    testUrls.Add(new TestUrl { Url = testUrl, RequestId = requestId, Object = testImage, Conn = "warm" });
-                }
-
-                // Run the tests.
-                var stopWatch = new Stopwatch();
-                for (var i = 0; i < testUrls.Count; i++)
-                {
-                    var testUrl = testUrls[i];
-                    MobileCenterLog.Verbose(LogTag, "Calling " + testUrl.Url);
-                    try
-                    {
-                        stopWatch.Restart();
-                        await httpNetworkAdapter.SendAsync(testUrl.Url, HttpMethod.Get, Headers, "", cancellationToken);
-                        testUrl.Result = stopWatch.ElapsedMilliseconds;
-                    }
-                    catch (OperationCanceledException)
-                    {
-                        throw;
-                    }
-                    catch (Exception e)
-                    {
-                        testUrls.RemoveAt(i--);
-                        MobileCenterLog.Error(LogTag, testUrl.Url + " call failed", e);
-                    }
-                }
-
-                // Generate report.
-                var jsonReport = JsonConvert.SerializeObject(testUrls);
-                MobileCenterLog.Verbose(LogTag, $"Report payload={jsonReport}");
-
-                // Send it.
-                if (configuration.ReportEndpoints != null)
-                {
-                    configurationOk = true;
-                    var reportId = ProbeId();
-                    var reportQueryString = $"?MonitorID=atm&rid={reportId}&w3c=false&prot=https&v=2017061301&tag={rumKey}&DATA={Uri.EscapeDataString(jsonReport)}";
-                    var hadFailure = false;
-                    var hadSuccess = false;
-                    foreach (var reportEndpoint in configuration.ReportEndpoints)
-                    {
-                        var reportUrl = $"http://{reportEndpoint}{reportQueryString}";
-                        MobileCenterLog.Verbose(LogTag, "Calling " + reportUrl);
+                        var testUrl = testUrls[i];
+                        MobileCenterLog.Verbose(LogTag, "Calling " + testUrl.Url);
                         try
                         {
-                            await httpNetworkAdapter.SendAsync(reportUrl, HttpMethod.Get, Headers, "", cancellationToken);
-                            hadSuccess = true;
+                            stopWatch.Restart();
+                            await httpNetworkAdapter.SendAsync(testUrl.Url, HttpMethod.Get, Headers, "", cancellationToken);
+                            testUrl.Result = stopWatch.ElapsedMilliseconds;
                         }
                         catch (OperationCanceledException)
                         {
@@ -331,30 +317,63 @@ namespace Microsoft.Azure.Mobile.Rum
                         }
                         catch (Exception e)
                         {
-                            hadFailure = true;
-                            MobileCenterLog.Error(LogTag, $"Failed to report measurements at {reportEndpoint}", e);
+                            testUrls.RemoveAt(i--);
+                            MobileCenterLog.Error(LogTag, testUrl.Url + " call failed", e);
                         }
                     }
-                    if (hadFailure)
+
+                    // Generate report.
+                    var jsonReport = JsonConvert.SerializeObject(testUrls);
+                    MobileCenterLog.Verbose(LogTag, $"Report payload={jsonReport}");
+
+                    // Send it.
+                    if (configuration.ReportEndpoints != null)
                     {
-                        if (hadSuccess)
+                        configurationOk = true;
+                        var reportId = ProbeId();
+                        var reportQueryString = $"?MonitorID=atm&rid={reportId}&w3c=false&prot=https&v=2017061301&tag={rumKey}&DATA={Uri.EscapeDataString(jsonReport)}";
+                        var hadFailure = false;
+                        var hadSuccess = false;
+                        foreach (var reportEndpoint in configuration.ReportEndpoints)
                         {
-                            MobileCenterLog.Warn(LogTag, "Measurements report failed on some report endpoints.");
+                            var reportUrl = $"http://{reportEndpoint}{reportQueryString}";
+                            MobileCenterLog.Verbose(LogTag, "Calling " + reportUrl);
+                            try
+                            {
+                                await httpNetworkAdapter.SendAsync(reportUrl, HttpMethod.Get, Headers, "", cancellationToken);
+                                hadSuccess = true;
+                            }
+                            catch (OperationCanceledException)
+                            {
+                                throw;
+                            }
+                            catch (Exception e)
+                            {
+                                hadFailure = true;
+                                MobileCenterLog.Error(LogTag, $"Failed to report measurements at {reportEndpoint}", e);
+                            }
+                        }
+                        if (hadFailure)
+                        {
+                            if (hadSuccess)
+                            {
+                                MobileCenterLog.Warn(LogTag, "Measurements report failed on some report endpoints.");
+                            }
+                            else
+                            {
+                                MobileCenterLog.Error(LogTag, "Measurements report failed on all report endpoints.");
+                            }
                         }
                         else
                         {
-                            MobileCenterLog.Error(LogTag, "Measurements report failed on all report endpoints.");
+                            MobileCenterLog.Info(LogTag, "Measurements reported to all report endpoints.");
                         }
                     }
-                    else
-                    {
-                        MobileCenterLog.Info(LogTag, "Measurements reported to all report endpoints.");
-                    }
                 }
-            }
-            if (!configurationOk)
-            {
-                throw new MobileCenterException("Invalid remote configuration for Rum.");
+                if (!configurationOk)
+                {
+                    throw new MobileCenterException("Invalid remote configuration for Rum.");
+                }
             }
         }
 
