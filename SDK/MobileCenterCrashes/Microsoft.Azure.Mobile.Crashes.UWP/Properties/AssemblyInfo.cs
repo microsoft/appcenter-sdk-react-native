44fead88890ae465f55f69cd8c74a20258c2533d
Guillaume Perrot <guperrot@microsoft.com>
2017-10-04 22:37:36 -0700

    Start v0.16.1

--- SDK/MobileCenterCrashes/Microsoft.Azure.Mobile.Crashes.UWP/Properties/AssemblyInfo.cs
+++ SDK/MobileCenterCrashes/Microsoft.Azure.Mobile.Crashes.UWP/Properties/AssemblyInfo.cs
@@ -29,6 +29,6 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("0.0.0.0")]
-[assembly: AssemblyFileVersion("0.16.0.0")]
-[assembly: AssemblyInformationalVersion("0.16.0-SNAPSHOT")]
+[assembly: AssemblyFileVersion("0.16.1.0")]
+[assembly: AssemblyInformationalVersion("0.16.1-SNAPSHOT")]
 [assembly: ComVisible(false)]
