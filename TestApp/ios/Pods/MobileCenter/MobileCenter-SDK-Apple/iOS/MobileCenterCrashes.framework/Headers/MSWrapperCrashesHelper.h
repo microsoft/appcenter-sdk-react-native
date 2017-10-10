74c1a9dd2bef8310ad71efdbfa98353b885e6204
Alexander Chocron <alchocro@microsoft.com>
2017-10-10 10:08:41 -0700

    Remove deleted files from testapp and add headers to framework

---
+++ TestApp/ios/Pods/MobileCenter/MobileCenter-SDK-Apple/iOS/MobileCenterCrashes.framework/Headers/MSWrapperCrashesHelper.h
@@ -0,0 +1,43 @@
+#import <Foundation/Foundation.h>
+#import "MSCrashHandlerSetupDelegate.h"
+
+@class MSErrorReport;
+@class MSErrorAttachmentLog;
+
+/**
+ * This general class allows wrappers to supplement the Crashes SDK with their own
+ * behavior.
+ */
+@interface MSWrapperCrashesHelper : NSObject
+
+/**
+ * Sets the crash handler setup delegate.
+ */
++ (void)setCrashHandlerSetupDelegate:(id<MSCrashHandlerSetupDelegate>)delegate;
+
+/**
+ * Gets the crash handler setup delegate.
+ */
++ (id<MSCrashHandlerSetupDelegate>)getCrashHandlerSetupDelegate;
+
+/**
+ * Disables automatic crash processing. Causes SDK not to send reports, even if ALWAYS_SEND is set.
+ */
++ (void)setAutomaticProcessing:(BOOL)automaticProcessing;
+
+/**
+ * Gets a list of unprocessed crash reports.
+ */
++ (NSArray<MSErrorReport *> *)getUnprocessedCrashReports;
+
+/**
+ * Resumes processing for a list of error reports that is a subset of the unprocessed reports.
+ */
++ (void)sendCrashReportsOrAwaitUserConfirmationForFilteredList:(NSArray<MSErrorReport *> *)filteredList;
+
+/**
+ * Sends error attachments for a particular error report.
+ */
++ (void)sendErrorAttachments:(NSArray<MSErrorAttachmentLog *> *)errorAttachments withIncidentIdentifier:(NSString *)incidentIdentifier;
+
+@end
