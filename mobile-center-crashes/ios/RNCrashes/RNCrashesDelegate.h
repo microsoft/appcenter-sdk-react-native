d9754ecdab6488658f57abb732118006f9cf38fe
Alexander Chocron <alchocro@microsoft.com>
2017-10-13 15:00:24 -0700

    Fix delegate and project file

---
+++ mobile-center-crashes/ios/RNCrashes/RNCrashesDelegate.h
@@ -0,0 +1,17 @@
+#import <Foundation/Foundation.h>
+
+@import MobileCenterCrashes;
+
+// Support React Native headers both in the React namespace, where they are in RN version 0.40+,
+// and no namespace, for older versions of React Native
+#if __has_include(<React/RCTBridge.h>)
+#import <React/RCTBridge.h>
+#else
+#import "RCTBridge.h"
+#endif
+
+
+@interface RNCrashesDelegate : NSObject<MSCrashesDelegate>
+
+@end
+
