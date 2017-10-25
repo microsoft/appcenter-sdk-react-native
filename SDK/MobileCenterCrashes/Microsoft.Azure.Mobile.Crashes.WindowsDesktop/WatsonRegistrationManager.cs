c71227aa396768e5f22fb7d8458caaad21270599
Alexander Chocron <alchocro@microsoft.com>
2017-10-25 13:23:12 -0700

    Add correlation id to windows desktop

--- SDK/MobileCenterCrashes/Microsoft.Azure.Mobile.Crashes.WindowsDesktop/WatsonRegistrationManager.cs
+++ SDK/MobileCenterCrashes/Microsoft.Azure.Mobile.Crashes.WindowsDesktop/WatsonRegistrationManager.cs
@@ -1,10 +1,11 @@
-﻿/*using System.Runtime.InteropServices;
+﻿using System.Runtime.InteropServices;
 
 namespace WatsonRegistrationUtility
 {
     internal class WatsonRegistrationManager
     {
         private const string WatsonAppSecretKey = "VSMCAppSecret";
+        private const string WatsonCorrelationIdKey = "VSMCCorrelationId";
 
         [DllImport("kernel32.dll", CharSet = CharSet.Unicode)]
         public static extern int WerRegisterCustomMetadata([MarshalAs(UnmanagedType.LPWStr)]string key, [MarshalAs(UnmanagedType.LPWStr)]string value);
@@ -13,6 +14,10 @@ namespace WatsonRegistrationUtility
         {
             WerRegisterCustomMetadata(WatsonAppSecretKey, appSecret);
         }
+
+        public static void SetCorrelationId(string correlationId)
+        {
+            WerRegisterCustomMetadata(WatsonCorrelationIdKey, correlationId);
+        }
     }
 }
-*/
