6e8e6559e131bcbe0310ab6db558f30353cd0ac4
Di Hei <dihei@microsoft.com>
2017-11-02 12:12:18 -0700

    Rename shell scripts in TestApp

--- TestApp/update-link-to-mobile-center.sh
+++ TestApp/update-link-to-mobile-center.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
+set -e
+
 ./update-npm-packages.sh
+
 echo 'Unlinking...'
-react-native unlink mobile-center
-react-native unlink mobile-center-crashes
-react-native unlink mobile-center-analytics
-react-native unlink mobile-center-push
+react-native unlink appcenter
+react-native unlink appcenter-crashes
+react-native unlink appcenter-analytics
+react-native unlink appcenter-push
+
 echo 'Updating cocoapods repo'
 pod repo update
-echo 'Removing old Pods...'
+
+echo 'Removing old Pods and Podfile.lock'
 rm -rf ios/Pods
 rm -rf ios/Podfile.lock
-echo 'Copying RNMobileCenter pod manually'
-mkdir -p ios/Pods/RNMobileCenterShared
-cp -R ../RNMobileCenterShared/Products/RNMobileCenterShared ios/Pods/RNMobileCenterShared
+
+echo 'Copying AppCenterReactNativeShared pod manually'
+mkdir -p ios/Pods/AppCenterReactNativeShared
+cp -R ../AppCenterReactNativeShared/Products/AppCenterReactNativeShared ios/Pods/AppCenterReactNativeShared
+
 echo 'Linking...'
 react-native link
--- TestApp/update-npm-packages.sh
+++ TestApp/update-npm-packages.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 echo 'Updating our npm packages...'
-rm -rf node_modules/mobile-center-link-scripts
-rm -rf node_modules/mobile-center
-rm -rf node_modules/mobile-center-crashes
-rm -rf node_modules/mobile-center-analytics
-rm -rf node_modules/mobile-center-push
+rm -rf node_modules/appcenter-link-scripts
+rm -rf node_modules/appcenter
+rm -rf node_modules/appcenter-crashes
+rm -rf node_modules/appcenter-analytics
+rm -rf node_modules/appcenter-push
 npm install
