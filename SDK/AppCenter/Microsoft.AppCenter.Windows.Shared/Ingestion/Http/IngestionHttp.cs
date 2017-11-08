a3b567f26e24e91388fc8f0d4d73d3cad0ceea47
Alexander Chocron <alchocro@microsoft.com>
2017-11-08 11:03:16 -0800

    Fix another endpoint issue

--- SDK/AppCenter/Microsoft.AppCenter.Windows.Shared/Ingestion/Http/IngestionHttp.cs
+++ SDK/AppCenter/Microsoft.AppCenter.Windows.Shared/Ingestion/Http/IngestionHttp.cs
@@ -11,7 +11,7 @@ namespace Microsoft.AppCenter.Ingestion.Http
     public sealed class IngestionHttp : IIngestion
     {
         internal const string DefaultBaseUrl = "https://in.mobile.azure.com";
-        internal const string ApiVersion = "/logs?api_version=1.0.0";
+        internal const string ApiVersion = "/logs?api-version=1.0.0";
         internal const string AppSecret = "App-Secret";
         internal const string InstallId = "Install-ID";
         
