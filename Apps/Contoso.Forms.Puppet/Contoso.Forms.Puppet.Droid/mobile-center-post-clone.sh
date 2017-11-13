464e2b644caff749c734f1a179e6ba26800d37fe
Guillaume Perrot <guperrot@microsoft.com>
2017-11-13 14:46:46 -0800

    Add nuget.config and ios post clone

--- Apps/Contoso.Forms.Puppet/Contoso.Forms.Puppet.Droid/mobile-center-post-clone.sh
+++ Apps/Contoso.Forms.Puppet/Contoso.Forms.Puppet.Droid/mobile-center-post-clone.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 echo "Executing post clone script in `pwd`"
-./build.sh -t=externals-android
-echo $GOOGLE_SERVICES_JSON | base64 -D > Apps/Contoso.Forms.Puppet/Contoso.Forms.Puppet.Droid/google-services.json
+sed -i '' "s/NUGET_PASSWORD/$NUGET_PASSWORD/g" NuGet.config
+./build.sh -t=externals-ios
