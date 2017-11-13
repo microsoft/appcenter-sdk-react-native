3f5959075465b70b94d1f5a211200ba1a78ac366
Guillaume Perrot <guperrot@microsoft.com>
2017-11-13 14:26:43 -0800

    Try to fix post clone on AC

--- Apps/Contoso.Forms.Puppet/Contoso.Forms.Puppet.Droid/mobile-center-post-clone.sh
+++ Apps/Contoso.Forms.Puppet/Contoso.Forms.Puppet.Droid/mobile-center-post-clone.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 echo "Executing post clone script in `pwd`"
-(cd ../../.. && ./build.sh -t=externals-android)
-echo $GOOGLE_SERVICES_JSON | base64 -D > google-services.json
+./build.sh -t=externals-android
+echo $GOOGLE_SERVICES_JSON | base64 -D > Apps/Contoso.Forms.Puppet/Contoso.Forms.Puppet.Droid/google-services.json
