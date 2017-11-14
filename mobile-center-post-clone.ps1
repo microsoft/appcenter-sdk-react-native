5b36edc1c83f3c5efdda72138dd1ecbf021e44a5
Guillaume Perrot <guperrot@microsoft.com>
2017-11-13 18:12:08 -0800

    Fix UWP build

--- mobile-center-post-clone.ps1
+++ mobile-center-post-clone.ps1
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 echo "Executing post clone script in $pwd"
 (Get-Content NuGet.config).replace('NUGET_PASSWORD', $env:NUGET_PASSWORD) | Set-Content NuGet.config
+[System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($env:GOOGLE_SERVICES_JSON)) | Set-Content Apps/Contoso.Forms.Demo/Contoso.Forms.Demo.Droid/google-services.json
