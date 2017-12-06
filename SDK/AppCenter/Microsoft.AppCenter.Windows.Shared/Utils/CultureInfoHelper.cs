8a86e1532bcc52235c2d77b9094b8f057736ed36
Jacob Wallraff <jawal@microsoft.com>
2017-12-06 12:43:35 -0800

    Documenting locale obscurities

--- SDK/AppCenter/Microsoft.AppCenter.Windows.Shared/Utils/CultureInfoHelper.cs
+++ SDK/AppCenter/Microsoft.AppCenter.Windows.Shared/Utils/CultureInfoHelper.cs
@@ -7,9 +7,13 @@ namespace Microsoft.AppCenter.Windows.Shared.Utils
     //https://www.pedrolamas.com/2017/05/03/cultureinfo-changes-in-uwp-part-2/
     class CultureInfoHelper
     {
+        // relative location of system locale name
         private const uint LOCALE_SNAME = 0x0000005c;
+
+        // Constants used by the National Language Support reference
         private const string LOCALE_NAME_USER_DEFAULT = null;
         private const string LOCALE_NAME_SYSTEM_DEFAULT = "!x-sys-default-locale";
+
         private const int BUFFER_SIZE = 530;
         
         [DllImport("api-ms-win-core-localization-l1-2-0.dll", CharSet = CharSet.Unicode)]
@@ -36,7 +40,6 @@ namespace Microsoft.AppCenter.Windows.Shared.Utils
         private static string InvokeGetLocaleInfoEx(string lpLocaleName, uint LCType)
         {
             var buffer = new StringBuilder(BUFFER_SIZE);
-
             var resultCode = GetLocaleInfoEx(lpLocaleName, LCType, buffer, BUFFER_SIZE);
 
             if (resultCode > 0)
