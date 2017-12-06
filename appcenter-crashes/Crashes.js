79a9c53cd0a2e84937da1943d0eaa8eb7582a7a8
Murat Baysangurov <v-mubays@microsoft.com>
2017-12-06 14:40:19 +0300

    Process crash reports only if Crashes is enabled

--- appcenter-crashes/Crashes.js
+++ appcenter-crashes/Crashes.js
@@ -93,26 +93,31 @@ const Crashes = {
             crashesEventEmitter.addListener(EVENT_SENDING_FAILED, listenerMap.onSendingFailed);
         }
         getErrorAttachmentsMethod = listenerMap.getErrorAttachments;
-        AppCenterReactNativeCrashes.getUnprocessedCrashReports()
-            .then((reports) => {
-                if (reports.length > 0) {
-                    const filteredReportIds = [];
-                    reports.forEach((report) => {
-                        if (!listenerMap.shouldProcess ||
-                            listenerMap.shouldProcess(report)) {
-                            filteredReports.push(report);
-                            filteredReportIds.push(report.id);
-                        }
-                    });
-                    AppCenterReactNativeCrashes.sendCrashReportsOrAwaitUserConfirmationForFilteredIds(filteredReportIds).then((alwaysSend) => {
-                        if (alwaysSend) {
-                            Helper.sendErrorAttachments();
-                        } else if (!listenerMap.shouldAwaitUserConfirmation || !listenerMap.shouldAwaitUserConfirmation()) {
-                            Crashes.notifyUserConfirmation(UserConfirmation.SEND);
-                        }
-                    });
-                }
-            });
+        Crashes.isEnabled()
+        .then((isEnabled) => {
+            if (isEnabled) {
+                AppCenterReactNativeCrashes.getUnprocessedCrashReports()
+                .then((reports) => {
+                    if (reports.length > 0) {
+                        const filteredReportIds = [];
+                        reports.forEach((report) => {
+                            if (!listenerMap.shouldProcess ||
+                                listenerMap.shouldProcess(report)) {
+                                filteredReports.push(report);
+                                filteredReportIds.push(report.id);
+                            }
+                        });
+                        AppCenterReactNativeCrashes.sendCrashReportsOrAwaitUserConfirmationForFilteredIds(filteredReportIds).then((alwaysSend) => {
+                            if (alwaysSend) {
+                                Helper.sendErrorAttachments();
+                            } else if (!listenerMap.shouldAwaitUserConfirmation || !listenerMap.shouldAwaitUserConfirmation()) {
+                                Crashes.notifyUserConfirmation(UserConfirmation.SEND);
+                            }
+                        });
+                    }
+                });
+            }
+        });
     }
 };
 
