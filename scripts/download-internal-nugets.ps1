8704d391cb25fdb69f5c3ebac316d6b3f0fbd722
Guillaume Perrot <guperrot@microsoft.com>
2017-12-08 18:22:54 -0800

    Fix download internal nugets on vsts

--- scripts/download-internal-nugets.ps1
+++ scripts/download-internal-nugets.ps1
@@ -1,9 +1,14 @@
+[CmdletBinding()]
+Param(
+    [string]$NugetPassword
+)
+
 $basePackage="Microsoft.AppCenter"
 $version=(Select-String -Path windows/nuspecs/nuget/AppCenter.nuspec -Pattern "version>(.*)<").Matches.Groups[1].Value
 foreach ($packageSuffix in @("", ".Analytics", ".Crashes", ".Distribute", ".Push")) {
   $package = "$basePackage$packageSuffix"
   $url = "https://msmobilecenter.pkgs.visualstudio.com/_apis/packaging/feeds/$env:NUGET_FEED_ID/nuget/packages/$package/versions/$version/content"
-  $password = [System.Text.Encoding]::UTF8.GetBytes("appcenter:$env:NUGET_PASSWORD")
+  $password = [System.Text.Encoding]::UTF8.GetBytes("appcenter:$NugetPassword")
   $password = [System.Convert]::ToBase64String($password)
   $webClient = New-Object System.Net.WebClient
   $webClient.Headers.Add("Authorization","Basic $password")
