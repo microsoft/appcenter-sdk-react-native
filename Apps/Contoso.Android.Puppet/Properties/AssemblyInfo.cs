c85ab0fc2227798d90bb0b85357c92735cdcf154
Guillaume Perrot <guperrot@microsoft.com>
2017-12-08 17:26:12 -0800

    Bump version to 1.1.0

--- Apps/Contoso.Android.Puppet/Properties/AssemblyInfo.cs
+++ Apps/Contoso.Android.Puppet/Properties/AssemblyInfo.cs
@@ -25,5 +25,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("0.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.2.0")]
-[assembly: AssemblyInformationalVersion("1.0.2-SNAPSHOT")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
+[assembly: AssemblyInformationalVersion("1.1.0-SNAPSHOT")]
--- SDK/AppCenterPush/Microsoft.AppCenter.Push.UWP/Properties/AssemblyInfo.cs
+++ SDK/AppCenterPush/Microsoft.AppCenter.Push.UWP/Properties/AssemblyInfo.cs
@@ -28,6 +28,6 @@ using System.Runtime.InteropServices;
 [assembly: ReferenceAssembly]
 #endif
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.2.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
 [assembly: ComVisible(false)]
 [assembly: InternalsVisibleTo("Microsoft.AppCenter.Test.UWP")]
