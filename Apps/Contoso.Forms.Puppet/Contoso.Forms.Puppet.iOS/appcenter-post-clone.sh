fa1cb7263cc5e5fd2f13f17724e97b52e8e96ed0
Guillaume Perrot <guperrot@microsoft.com>
2017-12-08 17:42:28 -0800

    Fix post clone scripts for forms puppet

--- Apps/Contoso.Forms.Puppet/Contoso.Forms.Puppet.iOS/appcenter-post-clone.sh
+++ Apps/Contoso.Forms.Puppet/Contoso.Forms.Puppet.iOS/appcenter-post-clone.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 echo "Executing post clone script in `pwd`"
-sed -i '' "s/NUGET_PASSWORD/$NUGET_PASSWORD/g" NuGet.config
+sed -i '' "s/NUGET_PASSWORD/$NUGET_PASSWORD/g" $APPCENTER_SOURCE_DIRECTORY/NuGet.config
 $APPCENTER_SOURCE_DIRECTORY/build.sh -t=externals-ios
