b1a154dd953d1951785c56f44ea84e876c3acd74
Guillaume Perrot <guperrot@microsoft.com>
2017-12-08 17:38:06 -0800

    Forgot to commit the renamed scripts for post clone

---
+++ Apps/Contoso.Forms.Puppet/Contoso.Forms.Puppet.Droid/appcenter-post-clone.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+echo "Executing post clone script in `pwd`"
+sed -i '' "s/NUGET_PASSWORD/$NUGET_PASSWORD/g" NuGet.config
+$APPCENTER_SOURCE_DIRECTORY/build.sh -t=externals-android
