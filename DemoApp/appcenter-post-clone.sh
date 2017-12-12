61f4b265082474b5313eca06d2aa1f797ac5e137
Di Hei <dihei@microsoft.com>
2017-12-12 10:59:40 -0800

    Fix path issue in appcenter-post-clone script

--- DemoApp/appcenter-post-clone.sh
+++ DemoApp/appcenter-post-clone.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 echo "Executing post clone script in `pwd`"
-echo $NPM_RC | base64 --decode > $APPCENTER_SOURCE_DIRECTORY/.npmrc
+echo $NPM_RC | base64 --decode > $APPCENTER_SOURCE_DIRECTORY/DemoApp/.npmrc
 echo $GOOGLE_SERVICES_JSON | base64 --decode > $APPCENTER_SOURCE_DIRECTORY/DemoApp/android/app/google-services.json
