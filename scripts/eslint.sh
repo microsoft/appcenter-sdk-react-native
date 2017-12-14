4b9e705e4862c776e80faeed6596d01a360dd3bd
Di Hei <dihei@microsoft.com>
2017-12-14 10:17:02 -0800

    Include TestApp to eslint script

--- scripts/eslint.sh
+++ scripts/eslint.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 set -e
-directories=("appcenter" "appcenter-analytics" "appcenter-crashes" "appcenter-link-scripts" "appcenter-push" "DemoApp")
+
+# ESLint should be installed locally in order to work reliably.
+# Please make sure ESLint is only installed locally not globally.
+
+directories=("appcenter" "appcenter-analytics" "appcenter-crashes" "appcenter-link-scripts" "appcenter-push" "DemoApp" "TestApp")
 for directory in "${directories[@]}"
 do
     cd ${directory}
     rm -f package-lock.json
-
-    # npm need to install ESlint locally.
-    # ESlint should be installed locally in order to work reliably. Please make sure ESlint is not installed globally.
-
-    npm install
+    if [ "$directory" = "TestApp" ]; then
+        ./update-npm-packages.sh
+    else
+        npm install
+    fi
     npm run lint
     cd ..
 done
