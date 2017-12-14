9f86934d6f84589f98835434f5ef8ef83de4c2c2
Di Hei <dihei@microsoft.com>
2017-12-13 18:02:33 -0800

    ESlint need to be installed locally in order to work reliably

--- scripts/eslint.sh
+++ scripts/eslint.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 set -e
-directories=("appcenter" "appcenter-analytics" "appcenter-crashes" "appcenter-link-scripts" "appcenter-push" "TestApp" "DemoApp")
+directories=("appcenter" "appcenter-analytics" "appcenter-crashes" "appcenter-link-scripts" "appcenter-push" "DemoApp")
 for directory in "${directories[@]}"
 do
     cd ${directory}
+    rm -f package-lock.json
+
+    # npm need to install ESlint locally.
+    # ESlint should be installed locally in order to work reliably. Please make sure ESlint is not installed globally.
+
+    npm install
     npm run lint
     cd ..
 done
