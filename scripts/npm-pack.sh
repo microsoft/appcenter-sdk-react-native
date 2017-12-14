2cba0a0512e8c3cda6794c123490f79c6448d49c
Di Hei <dihei@microsoft.com>
2017-12-14 13:43:08 -0800

    improve npm-pack script

--- scripts/npm-pack.sh
+++ scripts/npm-pack.sh
@@ -7,3 +7,8 @@ npm pack ./appcenter-analytics
 npm pack ./appcenter-crashes
 npm pack ./appcenter-push
 npm pack ./appcenter-link-scripts
+
+echo "Move appcenter*.tgz into sub-directory ./packages"
+mkdir -p packages
+cp appcenter*.tgz ./packages
+rm appcenter*.tgz
