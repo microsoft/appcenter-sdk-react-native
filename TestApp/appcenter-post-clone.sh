0c95815d26bf3131ebdea46f3cf0278e17248f4a
Di Hei <dihei@microsoft.com>
2017-12-14 17:53:30 -0800

    remove TestApp post clone script

--- TestApp/appcenter-post-clone.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-echo "Executing post clone script in `pwd`"
-echo $GOOGLE_SERVICES_JSON | base64 --decode > $APPCENTER_SOURCE_DIRECTORY/TestApp/android/app/google-services.json
-./update-npm-packages.sh
-# Delete everything except TestApp folder
-rm -rf ../appcenter* ../AppCenterReactNativeShared ../TestApp34 ../BrownfieldTestApp ../DemoApp
