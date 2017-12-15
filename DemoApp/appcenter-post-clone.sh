8ebf0c9cb09ddb470bd8f60a9163f958a0117f7e
Di Hei <dihei@microsoft.com>
2017-12-14 16:22:22 -0800

    fix path issue in post clone script

--- DemoApp/appcenter-post-clone.sh
+++ DemoApp/appcenter-post-clone.sh
@@ -2,3 +2,10 @@
 echo "Executing post clone script in `pwd`"
 echo $NPM_RC | base64 --decode > $APPCENTER_SOURCE_DIRECTORY/DemoApp/.npmrc
 echo $GOOGLE_SERVICES_JSON | base64 --decode > $APPCENTER_SOURCE_DIRECTORY/DemoApp/android/app/google-services.json
+./update-npm-packages.sh
+# App Center React Native Android Build "Copy Files" Task will throw 
+# if there's any broken symlinks inside the repo. So installing
+# npm packages for BrownfieldTestApp is necessary to allow build pass.
+echo "Install npm dependencies for BrownfieldTestApp"
+cd $APPCENTER_SOURCE_DIRECTORY/BrownfieldTestApp
+npm install
