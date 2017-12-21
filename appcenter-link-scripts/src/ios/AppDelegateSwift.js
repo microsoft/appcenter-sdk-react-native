c432f1f566033f9ad3998488d57286c56a4f3857
Murat Baysangurov <v-mubays@microsoft.com>
2017-12-21 17:20:49 +0300

    Fix application didFinishLaunchingWithOptions regex to support braces on new line

--- appcenter-link-scripts/src/ios/AppDelegateSwift.js
+++ appcenter-link-scripts/src/ios/AppDelegateSwift.js
@@ -46,16 +46,13 @@ AppDelegate.prototype.addInitCode = function (code, oldCodeRegExp) {
             this.appDelegateContents = this.appDelegateContents.replace(oldCodeMatches[0], code);
             debug('Replaced code', code, 'to file', this.appDelegatePath);
         } else {
-            const match = this.appDelegateContents.match(/didFinishLaunchingWithOptions{1}.*\{/);
+            const match = this.appDelegateContents.match(/didFinishLaunchingWithOptions{1}.*\s*\{/);
             if (match === null) {
                 throw Error(`
                     Could not find method "application:didFinishLaunchingWithOptions:" in file AppDelegate.swift.
                     Update AppDelegate.swift so that the method is present, as we match on it and insert '${code}' after for AppCenter SDK integration.
                 `);
             }
-
-            console.log('Match found = ', match[0]);
-
             const existingLine = match[0];
             this.appDelegateContents = this.appDelegateContents.replace(existingLine, `${existingLine}\n${code}\n`);
             debug('Added code', code, 'to file', this.appDelegatePath);
