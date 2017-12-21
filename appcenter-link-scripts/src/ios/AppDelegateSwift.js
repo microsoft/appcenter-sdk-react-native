8da499c4b88329256c7d83cdbaff9b54a79f14c0
Murat Baysangurov <v-mubays@microsoft.com>
2017-12-21 16:39:47 +0300

    Adding headers to bridging header file for swift support

--- appcenter-link-scripts/src/ios/AppDelegateSwift.js
+++ appcenter-link-scripts/src/ios/AppDelegateSwift.js
@@ -1,7 +1,9 @@
-const fs = require('fs');
 const debug = require('debug')('appcenter-link:ios:AppDelegateSwift');
-
+const glob = require('glob');
+const fs = require('fs');
 const AppDelegate = require('./AppDelegate');
+
+const bridgingHeaderPaths = glob.sync('**/*-Bridging-Header.h', { ignore: 'node_modules/**' });
 
 const AppDelegateSwift = function (file) {
     AppDelegate.call(this, file);
@@ -11,7 +13,23 @@ AppDelegateSwift.prototype = Object.create(AppDelegate.prototype);
 AppDelegateSwift.prototype.constructor = AppDelegateSwift;
 
 AppDelegateSwift.prototype.addHeader = function (header) {
-    console.log('Adding header to swift');
+    try {
+        fs.accessSync(bridgingHeaderPaths[0], fs.F_OK);
+        let bridgingHeaderContents = fs.readFileSync(bridgingHeaderPaths[0], 'utf-8');
+        if (bridgingHeaderContents.indexOf(header) === -1) {
+            bridgingHeaderContents += '\n';
+            bridgingHeaderContents += header;
+            fs.writeFileSync(bridgingHeaderPaths[0], bridgingHeaderContents);
+            debug('Added header', header, 'to file', bridgingHeaderPaths[0]);
+        } else {
+            debug(`"${header}"" header already added to bridging header`);
+        }
+    } catch (e) {
+        console.log(e);
+        throw Error(`
+            Could not access bridging header. Please check that it exists and that you have rights to read and write into it.
+        `);
+    }
 };
 
 AppDelegate.prototype.addInitCode = function (code, oldCodeRegExp) {
