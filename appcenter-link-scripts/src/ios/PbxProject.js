9679c7c606051e6126681a8e0ede283cc1f77192
Ryan Thomas <RyanThomas@curo.com>
2018-01-16 11:01:46 -0600

    Issue220 - Updated PbxProject.updateFrameworkSearchPaths(..) to replace the ios/Pods search path instead of add an additional path, and to only perform the replace if it is different than the default

--- appcenter-link-scripts/src/ios/PbxProject.js
+++ appcenter-link-scripts/src/ios/PbxProject.js
@@ -6,6 +6,7 @@ const xcode = require('xcode');
 const glob = require('glob');
 const debug = require('debug')('appcenter-link:ios:AppCenterConfigPlist');
 
+const DEFAULT_PODS_FRAMEWORK_SEARCH_PATH = '../../../ios/pods/**';
 
 
 const PbxProject = function (pbxProjectPath) {
@@ -17,11 +18,17 @@ const PbxProject = function (pbxProjectPath) {
 PbxProject.prototype.updateFrameworkSearchPaths = function (podsInstallPath) {
     const sourceRoot = path.resolve(path.dirname(this.pbxProjectPath), '..');
     const absolutePodsInstallPath = path.resolve(podsInstallPath);
-    const relativePodsInstallPath = path.relative(sourceRoot, absolutePodsInstallPath);
+    const relativePodsInstallPath = path.relative(sourceRoot, absolutePodsInstallPath, '**').replace(/\\/g, '/');
+
+    if(relativePodsInstallPath.toLowerCase() === DEFAULT_PODS_FRAMEWORK_SEARCH_PATH) {
+        // pods install path matches the default, nothing to update...
+        return;
+    }
+
     const frameworkSearchPath = path.join('$(SRCROOT)', relativePodsInstallPath);
 
     const FRAMEWORK_SEARCH_PATHS_REPLACEMENT_REGEX = /(FRAMEWORK_SEARCH_PATHS = \()(\r?\n\s*)("\$\(SRCROOT\)\/\.\.\/\.\.\/\.\.\/ios\/Pods\/\*\*",)(\r?\n\s*)("\$\(inherited\)",)(\r?\n\s*)\);/g;
-    const replacementString = `$1$2$3$4$5$4"${frameworkSearchPath}",$6);`;
+    const replacementString = `$1$2"${frameworkSearchPath}",$4$5$6);`;
 
     this.pbxProjectContents = this.pbxProjectContents.replace(FRAMEWORK_SEARCH_PATHS_REPLACEMENT_REGEX, replacementString)
 };
