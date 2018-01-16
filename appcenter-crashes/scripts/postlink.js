c770eddedd6b3453dbdbe521bcc15aa4e11c6de2
Ryan Thomas <RyanThomas@curo.com>
2018-01-16 10:57:10 -0600

    Issue220 , Issue137 - Updated postlink scripts to include logic that will update the framework search paths in the appcenter project.pbxproj files to match the parent ios project path

--- appcenter-crashes/scripts/postlink.js
+++ appcenter-crashes/scripts/postlink.js
@@ -2,42 +2,56 @@ const rnpmlink = require('appcenter-link-scripts');
 const npmPackages = require('./../package.json');
 
 return rnpmlink.ios.checkIfAppDelegateExists()
-    .then(() => rnpmlink.ios.initAppCenterConfig().catch((e) => {
-        console.log(`Could not create or update AppCenter config file (AppCenter-Config.plist). Error Reason - ${e.message}`);
-        return Promise.reject();
-    }))
     .then(() => {
-        const prompt = npmPackages.rnpm.params[0];
-        prompt.message = prompt.message.replace(/Android/, 'iOS');
-        return rnpmlink.inquirer.prompt(prompt)
-            .catch((e) => {
-                console.log(`Could not determine when to send AppCenter crashes. Error Reason - ${e.message}`);
-                return Promise.reject();
-            });
-    })
-    .then((answer) => {
-        const code = answer.whenToSendCrashes === 'ALWAYS_SEND' ?
-            '  [AppCenterReactNativeCrashes registerWithAutomaticProcessing];  // Initialize AppCenter crashes' :
-            '  [AppCenterReactNativeCrashes register];  // Initialize AppCenter crashes';
-        return rnpmlink.ios.initInAppDelegate('#import <AppCenterReactNativeCrashes/AppCenterReactNativeCrashes.h>', code, /.*\[AppCenterReactNativeCrashes register.*/g)
-            .catch((e) => {
-                console.log(`Could not initialize AppCenter crashes in AppDelegate. Error Reason - ${e.message}`);
-                return Promise.reject();
-            });
-    })
-    .then((file) => {
-        console.log(`Added code to initialize iOS Crashes SDK in ${file}`);
-        return rnpmlink.ios.addPodDeps([
-            { pod: 'AppCenter/Crashes', version: '1.1.0' },
-            { pod: 'AppCenterReactNativeShared', version: '1.1.0' } // in case people don't link appcenter (core)
-        ]).catch((e) => {
-            console.log(`
-            Could not install dependencies using CocoaPods.
-            Please refer to the documentation to install dependencies manually.
-
-            Error Reason - ${e.message}
-        `);
+        return Promise.resolve().then(() => rnpmlink.ios.initAppCenterConfig().catch((e) => {
+            console.log(`Could not create or update AppCenter config file (AppCenter-Config.plist). Error Reason - ${e.message}`);
             return Promise.reject();
-        });
+        }))
+        .then(() => {
+            const prompt = npmPackages.rnpm.params[0];
+            prompt.message = prompt.message.replace(/Android/, 'iOS');
+            return rnpmlink.inquirer.prompt(prompt)
+                .catch((e) => {
+                    console.log(`Could not determine when to send AppCenter crashes. Error Reason - ${e.message}`);
+                    return Promise.reject();
+                });
+        })
+        .then((answer) => {
+            const code = answer.whenToSendCrashes === 'ALWAYS_SEND' ?
+                '  [AppCenterReactNativeCrashes registerWithAutomaticProcessing];  // Initialize AppCenter crashes' :
+                '  [AppCenterReactNativeCrashes register];  // Initialize AppCenter crashes';
+            return rnpmlink.ios.initInAppDelegate('#import <AppCenterReactNativeCrashes/AppCenterReactNativeCrashes.h>', code, /.*\[AppCenterReactNativeCrashes register.*/g)
+                .catch((e) => {
+                    console.log(`Could not initialize AppCenter crashes in AppDelegate. Error Reason - ${e.message}`);
+                    return Promise.reject();
+                });
+        })
+        .then((file) => {
+            console.log(`Added code to initialize iOS Crashes SDK in ${file}`);
+            return rnpmlink.ios.addPodDeps([
+                { pod: 'AppCenter/Crashes', version: '1.1.0' },
+                { pod: 'AppCenterReactNativeShared', version: '1.1.0' } // in case people don't link appcenter (core)
+            ]).catch((e) => {
+                console.log(`
+                Could not install dependencies using CocoaPods.
+                Please refer to the documentation to install dependencies manually.
+
+                Error Reason - ${e.message}
+            `);
+                return Promise.reject();
+            });
+        })
+        .then(() => {
+            const appcenterCrashesPbxProjectPath = path.resolve(__dirname, '..', 'ios', 'AppCenterReactNativeCrashes.xcodeproj', 'project.pbxproj');
+            return rnpmlink.ios.updateFrameworkSearchPaths(appcenterCrashesPbxProjectPath)
+                .catch((e) => {
+                    console.log(`Could not update framework search paths in AppCenterReactNativeCrashes ios project.pbxproj file. Error Reason - ${e.message}`);
+                    return Promise.reject();
+                });
+        })
+        .catch(() => Promise.resolve());
     })
-    .catch(() => Promise.resolve());
+    .catch(() => {
+        console.log('Could not locate the ios project directory; skipping AppCenterReactNativeCrashes postlink steps for ios.');
+        return Promise.resolve();
+    });
