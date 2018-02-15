4eaa21f6f2b28e4c39c04d2531c406248ab5e971
Alexander Chocron <alchocro@microsoft.com>
2018-02-15 10:27:24 -0800

    Bump native sdk versions

--- appcenter-crashes/scripts/postlink.js
+++ appcenter-crashes/scripts/postlink.js
@@ -29,7 +29,7 @@ return rnpmlink.ios.checkIfAppDelegateExists()
         console.log(`Added code to initialize iOS Crashes SDK in ${file}`);
         return rnpmlink.ios.addPodDeps(
             [
-                { pod: 'AppCenter/Crashes', version: '1.3.0' },
+                { pod: 'AppCenter/Crashes', version: '1.4.0' },
                 { pod: 'AppCenterReactNativeShared', version: '1.2.0' } // in case people don't link appcenter (core)
             ],
             { platform: 'ios', version: '9.0' }
