7c2745e65f4794aa2c244001fb992dcc9b2e5963
Yury Nevalenny <v-yuneva@microsoft.com>
2018-02-16 00:58:28 +0300

    Update version to 1.4.0

--- SDK/AppCenterCrashes/Microsoft.AppCenter.Crashes.iOS.Bindings/Properties/AssemblyInfo.cs
+++ SDK/AppCenterCrashes/Microsoft.AppCenter.Crashes.iOS.Bindings/Properties/AssemblyInfo.cs
@@ -30,5 +30,5 @@ using System.Reflection;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("0.0.0.0")]
-[assembly: AssemblyFileVersion("1.3.1.0")]
-[assembly: AssemblyInformationalVersion("1.3.1-SNAPSHOT")]
+[assembly: AssemblyFileVersion("1.4.0.0")]
+[assembly: AssemblyInformationalVersion("1.4.0-SNAPSHOT")]
