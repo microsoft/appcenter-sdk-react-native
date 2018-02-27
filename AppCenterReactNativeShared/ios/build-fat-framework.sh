9fe74af233e5d0ca18990ae028d1a71c75d54c3b
Benjamin Scholtysik (Reimold) <bereimol@microsoft.com>
2018-02-27 12:31:58 -0800

    Make sure to att LICENSE.md to Products folder to make sure cocoapod integration for TestApp works. This is now part of the build script that creates the fat framework.

--- AppCenterReactNativeShared/ios/build-fat-framework.sh
+++ AppCenterReactNativeShared/ios/build-fat-framework.sh
@@ -37,6 +37,9 @@ cp -f "${SRCROOT}/${FMK_NAME}/Support/module.modulemap" "${INSTALL_DIR}/Modules/
 # Copies the headers and resources files to the final product folder.
 cp -R "${SRCROOT}/${WRK_DIR}/Release-iphoneos/include/${FMK_NAME}/" "${INSTALL_DIR}/Headers/"
 
+# Copies the license file to the products directory (required for cocoapods)
+cp -f "../LICENSE.md" "${TEMP_DIR}"
+
 # Uses the Lipo Tool to merge both binary files (i386 + armv6/armv7/armv7s/arm64) into one Universal final product.
 lipo -create "${DEVICE_DIR}/lib${FMK_NAME}.a" "${SIMULATOR_DIR}/lib${FMK_NAME}.a" -output "${INSTALL_DIR}/${FMK_NAME}"
 
