f77508bdbcf150f00ed6bbf10862380b4ffe26c3
Guillaume Perrot <guperrot@microsoft.com>
2018-02-28 13:50:28 -0800

    Move android crash prompt to postlink

--- appcenter-crashes/scripts/postlink.js
+++ appcenter-crashes/scripts/postlink.js
@@ -1,5 +1,5 @@
 const rnpmlink = require('appcenter-link-scripts');
-const npmPackages = require('./../package.json');
+const inquirer = require('inquirer');
 
 return rnpmlink.ios.checkIfAppDelegateExists()
     .then(() => rnpmlink.ios.initAppCenterConfig().catch((e) => {
@@ -7,16 +7,47 @@ return rnpmlink.ios.checkIfAppDelegateExists()
         return Promise.reject();
     }))
     .then(() => {
-        const prompt = npmPackages.rnpm.params[0];
-        prompt.message = prompt.message.replace(/Android/, 'iOS');
-        return rnpmlink.inquirer.prompt(prompt)
-            .catch((e) => {
-                console.log(`Could not determine when to send AppCenter crashes. Error Reason - ${e.message}`);
-                return Promise.reject();
-            });
+        return inquirer.prompt([{
+            type: 'list',
+            name: 'whenToSendCrashes',
+            message: 'For the Android app, should crashes be sent automatically or processed in JavaScript before being sent?',
+            choices: [
+                {
+                    name: 'Automatically',
+                    value: 'ALWAYS_SEND'
+                },
+                {
+                    name: 'Processed in JavaScript by user',
+                    value: 'ASK_JAVASCRIPT'
+                }]
+        }]).catch((e) => {
+            console.log(`Could not determine when to enable AppCenter crashes. Error Reason - ${e.message}`);
+            return Promise.reject();
+        });
     })
-    .then((answer) => {
-        const code = answer.whenToSendCrashes === 'ALWAYS_SEND' ?
+    .then((androidAnswer) => {
+        rnpmlink.android.patchStrings('appCenterCrashes_whenToSendCrashes',
+            androidAnswer.whenToSendCrashes);
+        return inquirer.prompt([{
+            type: 'list',
+            name: 'whenToSendCrashes',
+            message: 'For the iOS app, should crashes be sent automatically or processed in JavaScript before being sent?',
+            choices: [
+                {
+                    name: 'Automatically',
+                    value: 'ALWAYS_SEND'
+                },
+                {
+                    name: 'Processed in JavaScript by user',
+                    value: 'ASK_JAVASCRIPT'
+                }]
+        }]).catch((e) => {
+            console.log(`Could not determine when to enable AppCenter crashes. Error Reason - ${e.message}`);
+            return Promise.reject();
+        });
+    })
+    .then((iosAnswer) => {
+        const code = iosAnswer.whenToSendCrashes === 'ALWAYS_SEND' ?
             '  [AppCenterReactNativeCrashes registerWithAutomaticProcessing];  // Initialize AppCenter crashes' :
             '  [AppCenterReactNativeCrashes register];  // Initialize AppCenter crashes';
         return rnpmlink.ios.initInAppDelegate('#import <AppCenterReactNativeCrashes/AppCenterReactNativeCrashes.h>', code, /.*\[AppCenterReactNativeCrashes register.*/g)
