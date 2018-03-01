4ba86f03f0e4f95b0ce09c53b14fd9a1c0c725b4
Jan-Pieter Zoutewelle <jp@intreba.nl>
2018-03-01 09:00:35 +0100

    Adjust typescript Analytics definitions

--- appcenter-analytics/Analytics.d.ts
+++ appcenter-analytics/Analytics.d.ts
@@ -1,6 +1,6 @@
 export function trackEvent(
   eventName: string,
-  properties?: { [name: string]: string | number | boolean }
+  properties?: { [name: string]: string }
 ): Promise<string | null>;
-export function isEnabled(): Promise<boolean | string>;
-export function setEnabled(enabled: boolean): Promise<string | null>;
+export function isEnabled(): Promise<boolean>;
+export function setEnabled(enabled: boolean): Promise<void>;
