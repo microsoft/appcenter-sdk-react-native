6773f684ab65bcd4183f804be6d03943b213ebaa
Jan-Pieter Zoutewelle <jp@intreba.nl>
2018-03-12 11:52:40 +0100

    Add AppCenter types

---
+++ appcenter/AppCenter.d.ts
@@ -0,0 +1,21 @@
+export const enum AppCenterLogLevel {
+    LogLevelVerbose = 2,
+    LogLevelDebug = 3,
+    LogLevelInfo = 4,
+    LogLevelWarning = 5,
+    LogLevelError = 6,
+    LogLevelAssert = 7,
+    LogLevelNone = 99
+}
+
+export interface CustomProperties {
+    set(key: string, value: string);
+}
+
+export function setLogLevel(logLevel: AppCenterLogLevel): Promise<void>;
+export function getLogLevel(): Promise<AppCenterLog>;
+export function getSdkVersion(): string;
+export function isEnabled(): Promise<boolean>;
+export function setEnabled(enabled: boolean): Promise<void>;
+export function getInstallId(): Promise<string>;
+export function setCustomProperties(properties: CustomProperties);
