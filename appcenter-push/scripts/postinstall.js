354c659f845f0d7e34edc53140dc3dc2e77d2cad
Murat Baysangurov <v-mubays@microsoft.com>
2018-05-10 14:31:14 +0300

    Add postinstall script for creating mocks

---
+++ appcenter-push/scripts/postinstall.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+
+// Create mock file for Jest
+const mocksDirectory = `${process.env.INIT_CWD}/__mocks__`;
+const mockFileName = 'appcenter-push.js';
+if(!fs.existsSync(`${mocksDirectory}/${mockFileName}`)) {
+    if (!fs.existsSync(mocksDirectory)){
+        fs.mkdirSync(mocksDirectory);
+    }
+    fs.writeFileSync(`${mocksDirectory}/${mockFileName}`, '');
+}
