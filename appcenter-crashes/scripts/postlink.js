f993307187d958677bdf1fda922b2d6e281d0cc8
Di Hei <dihei@microsoft.com>
2018-05-16 14:30:09 -0700

    bump ios sdk version

--- appcenter-crashes/scripts/postlink.js
+++ appcenter-crashes/scripts/postlink.js
@@ -66,7 +66,7 @@ if (rnpmlink.ios.checkIfAppDelegateExists()) {
             console.log(`Added code to initialize iOS Crashes SDK in ${file}`);
             return rnpmlink.ios.addPodDeps(
                 [
-                    { pod: 'AppCenter/Crashes', version: '1.6.0' },
+                    { pod: 'AppCenter/Crashes', version: '1.6.1' },
                     { pod: 'AppCenterReactNativeShared', version: '1.5.0' } // in case people don't link appcenter (core)
                 ],
                 { platform: 'ios', version: '9.0' }
--- appcenter-push/scripts/postlink.js
+++ appcenter-push/scripts/postlink.js
@@ -33,7 +33,7 @@ if (rnpmlink.ios.checkIfAppDelegateExists()) {
             console.log(`Added code to initialize iOS Push SDK in ${file}`);
             return rnpmlink.ios.addPodDeps(
                 [
-                    { pod: 'AppCenter/Push', version: '1.6.0' },
+                    { pod: 'AppCenter/Push', version: '1.6.1' },
                     { pod: 'AppCenterReactNativeShared', version: '1.5.0' } // in case people don't link appcenter (core)
                 ],
                 { platform: 'ios', version: '9.0' }
