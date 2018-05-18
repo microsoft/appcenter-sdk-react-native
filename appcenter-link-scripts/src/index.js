6222c6e3ce0c0d934a6751c41bf75e64e31cb522
Murat Baysangurov <v-mubays@microsoft.com>
2018-05-18 12:37:22 +0300

    Add js extension for require

--- appcenter-link-scripts/src/index.js
+++ appcenter-link-scripts/src/index.js
@@ -1,6 +1,6 @@
 const android = require('./android');
 const ios = require('./ios');
-const mock = require('./SetupAppCenterMock');
+const mock = require('./SetupAppCenterMock.js');
 const inquirer = require('inquirer');
 
 module.exports = {
