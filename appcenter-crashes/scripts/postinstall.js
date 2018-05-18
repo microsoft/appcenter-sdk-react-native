e38e7dfcdaef155d906982eb555453603eae44da
Murat Baysangurov <v-mubays@microsoft.com>
2018-05-18 12:17:01 +0300

    Extract common code to link-scripts module

--- appcenter-crashes/scripts/postinstall.js
+++ appcenter-crashes/scripts/postinstall.js
@@ -1,29 +1,3 @@
-const fs = require('fs');
-const path = require('path');
+const rnpmlink = require('appcenter-link-scripts');
 
-const projectDirectory = path.resolve(__dirname, '..', '..', '..');
-const setupFileName = `.${path.sep}node_modules${path.sep}appcenter-crashes${path.sep}test${path.sep}appCenterCrashesMock.js`;
-
-// Check if package.json has jest as dependency
-let packageJson = '';
-const packageJsonFile = path.join(`${projectDirectory}`, 'package.json');
-try {
-    packageJson = JSON.parse(fs.readFileSync(packageJsonFile, 'utf8'));
-    if (!Object.prototype.hasOwnProperty.call(packageJson.devDependencies, 'jest') &&
-        !Object.prototype.hasOwnProperty.call(packageJson.dependencies, 'jest')) {
-        return;
-    }
-} catch (e) {
-    console.log('Could not read package.json file');
-    return;
-}
-
-// Add setup file for module
-if (Object.prototype.hasOwnProperty.call(packageJson, 'jest')) {
-    if (packageJson.jest.setupFiles === undefined) {
-        packageJson.jest.setupFiles = [setupFileName];
-    } else if (packageJson.jest.setupFiles.indexOf(setupFileName) === -1) {
-        packageJson.jest.setupFiles.push(setupFileName);
-    }
-    fs.writeFileSync(packageJsonFile, JSON.stringify(packageJson));
-}
+rnpmlink.mock.setupMock('appcenter-crashes', 'AppCenterCrashesMock.js');
--- appcenter-link-scripts/src/index.js
+++ appcenter-link-scripts/src/index.js
@@ -1,9 +1,11 @@
 const android = require('./android');
 const ios = require('./ios');
+const mock = require('./SetupAppCenterMock');
 const inquirer = require('inquirer');
 
 module.exports = {
     android,
     ios,
+    mock,
     inquirer
 };
