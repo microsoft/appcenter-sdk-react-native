27e206ac5d7d2fe89d381f3d6072a1935c4877a0
Clement Polet <clpolet@microsoft.com>
2018-06-19 15:37:34 -0700

    Update demo app with AC v1.7.0

--- Apps/Contoso.WPF.Demo/Properties/AssemblyInfo.cs
+++ Apps/Contoso.WPF.Demo/Properties/AssemblyInfo.cs
@@ -27,5 +27,5 @@ using System.Windows;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("0.0.0.0")]
-[assembly: AssemblyFileVersion("1.6.1.0")]
-[assembly: AssemblyInformationalVersion("1.6.1")]
+[assembly: AssemblyFileVersion("1.7.0.0")]
+[assembly: AssemblyInformationalVersion("1.7.0")]
