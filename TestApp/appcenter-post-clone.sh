6b0a7aea50555ad66fee190a4ea9c71cd8b7d411
Murat Baysangurov <v-mubays@microsoft.com>
2018-07-03 07:48:45 +0300

    Remove post close script from TestApp

--- TestApp/appcenter-post-clone.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-echo "Executing post clone script in `pwd`"
-echo $NPM_RC | base64 --decode > $APPCENTER_SOURCE_DIRECTORY/TestApp/.npmrc
-# Delete everything except TestApp folder
-rm -rf ../appcenter* ../AppCenterReactNativeShared ../TestApp34 ../BrownfieldTestApp ../DemoApp
