a1986bd522cae36d1169580644d990f6952f0f5e
Di Hei <dihei@microsoft.com>
2018-08-22 15:38:59 -0700

    Use same event name in AnalyticsScreen with TransmissionScreen

--- TestApp/app/screens/TransmissionScreen.js
+++ TestApp/app/screens/TransmissionScreen.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Image, View, SectionList, Text, TouchableOpacity } from 'react-native';
+import { Image, View, SectionList, Text, TouchableOpacity, Toast } from 'react-native';
 import ModalSelector from 'react-native-modal-selector';
 
 import AppCenter from 'appcenter';
@@ -13,63 +13,77 @@ import Constants from '../Constants';
 const targetTokens = Constants.targetTokens;
 
 export default class TransmissionScreen extends Component {
-  static navigationOptions = {
-    tabBarIcon: () => <Image style={{ width: 24, height: 24 }} source={TransmissionTabBarIcon} />
-  }
+    static navigationOptions = {
+        tabBarIcon: () => <Image style={{ width: 24, height: 24 }} source={TransmissionTabBarIcon} />
+    }
 
-  state = {
-    targetToken: targetTokens[0]
-  }
+    state = {
+        targetToken: targetTokens[0]
+    }
 
-  async componentWillMount() {
-    await AppCenter.startFromLibrary(Analytics);
-  }
+    async componentWillMount() {
+        await AppCenter.startFromLibrary(Analytics);
+    }
 
-  render() {
-    const pickerRenderItem = ({ item: { title, valueChanged, tokens } }) => (
-      <ModalSelector data={tokens} initValue={title} onChange={valueChanged} style={SharedStyles.modalSelector} />
-    );
-    const actionRenderItem = ({ item: { title, action } }) => (
-      <TouchableOpacity style={SharedStyles.item} onPress={action}>
-        <Text style={SharedStyles.itemButton}>{title}</Text>
-      </TouchableOpacity>
-    );
-    return (
-      <View style={SharedStyles.container}>
-        <SectionList
-          renderItem={({ item }) => <Text style={[SharedStyles.item, SharedStyles.title]}>{item}</Text>}
-          renderSectionHeader={({ section: { title } }) => <Text style={SharedStyles.header}>{title}</Text>}
-          keyExtractor={(item, index) => item + index}
-          sections={[
-            {
-              title: 'Select transmission target',
-              data: [
-                {
-                  title: 'Select target token',
-                  valueChanged: option => this.setState({ targetToken: option }),
-                  tokens: targetTokens
-                },
-              ],
-              renderItem: pickerRenderItem
-            },
-            {
-              title: 'Actions',
-              data: [
-                {
-                  title: 'Track Event with properties',
-                  action: async () => {
-                    const transmissionTarget = await Analytics.getTransmissionTarget(this.state.targetToken.key);
-                    if (transmissionTarget) {
-                      transmissionTarget.trackEvent('event_for_transmission_target', { page: 'Transmission screen' });
-                    }
-                  }
-                }
-              ],
-              renderItem: actionRenderItem
-            },
-          ]}
-        />
-      </View>
-    );
-  }
+    render() {
+        const pickerRenderItem = ({ item: { title, valueChanged, tokens } }) => (
+          <ModalSelector data={tokens} initValue={title} onChange={valueChanged} style={SharedStyles.modalSelector} />
+        );
+        const actionRenderItem = ({ item: { title, action } }) => (
+          <TouchableOpacity style={SharedStyles.item} onPress={action}>
+            <Text style={SharedStyles.itemButton}>{title}</Text>
+          </TouchableOpacity>
+        );
+        const showEventToast = eventName => Toast.show(`Scheduled event ${eventName}. Please check verbose logs.`);
+        return (
+          <View style={SharedStyles.container}>
+            <SectionList
+              renderItem={({ item }) => <Text style={[SharedStyles.item, SharedStyles.title]}>{item}</Text>}
+              renderSectionHeader={({ section: { title } }) => <Text style={SharedStyles.header}>{title}</Text>}
+              keyExtractor={(item, index) => item + index}
+              sections={[
+                        {
+                            title: 'Select transmission target',
+                            data: [
+                                {
+                                    title: 'Select target token',
+                                    valueChanged: option => this.setState({ targetToken: option }),
+                                    tokens: targetTokens
+                                },
+                            ],
+                            renderItem: pickerRenderItem
+                        },
+                        {
+                            title: 'Actions',
+                            data: [
+                                {
+                                    title: 'Track event without properties',
+                                    action: async () => {
+                                        const transmissionTarget = await Analytics.getTransmissionTarget(this.state.targetToken.key);
+                                        if (transmissionTarget) {
+                                            const eventName = 'EventWithoutPropertiesFromTarget';
+                                            transmissionTarget.trackEvent(eventName);
+                                            showEventToast(eventName);
+                                        }
+                                    }
+                                },
+                                {
+                                    title: 'Track event with properties',
+                                    action: async () => {
+                                        const transmissionTarget = await Analytics.getTransmissionTarget(this.state.targetToken.key);
+                                        if (transmissionTarget) {
+                                            const eventName = 'EventWithPropertiesFromTarget';
+                                            transmissionTarget.trackEvent(eventName, { property1: '100', property2: '200' });
+                                            showEventToast(eventName);
+                                        }
+                                    }
+                                }
+                            ],
+                            renderItem: actionRenderItem
+                        },
+                    ]}
+            />
+          </View>
+        );
+    }
 }
