2ed41e0549ff489f8d79532669b85d9115534067
Jacob Wallraff <jawal@microsoft.com>
2018-08-22 11:05:14 -0700

    Addressing feedback

--- AppCenterReactNativeShared/android/src/main/java/com/microsoft/appcenter/reactnative/shared/AppCenterReactNativeShared.java
+++ AppCenterReactNativeShared/android/src/main/java/com/microsoft/appcenter/reactnative/shared/AppCenterReactNativeShared.java
@@ -15,6 +15,8 @@ import static com.microsoft.appcenter.utils.AppCenterLog.LOG_TAG;
 
 public class AppCenterReactNativeShared {
 
+    private static final String APP_SECRET_KEY = "app_secret";
+
     private static String sAppSecret;
 
     private static boolean sStartedAutomatically;
@@ -25,7 +27,7 @@ public class AppCenterReactNativeShared {
 
     @SuppressWarnings("unused")
     public static void configureAppCenter(Application application) {
-        if (AppCenter.isConfigured()) {
+        if (sApplication != null) {
             return;
         }
         sApplication = application;
@@ -80,7 +82,7 @@ public class AppCenterReactNativeShared {
                 configStream.close();
                 String jsonContents = new String(buffer, "UTF-8");
                 JSONObject json = new JSONObject(jsonContents);
-                sAppSecret = json.getString("app_secret");
+                sAppSecret = json.getString(APP_SECRET_KEY);
                 sStartedAutomatically = json.optBoolean("start_automatically", true);
             } catch (Exception e) {
                 AppCenterLog.error(LOG_TAG, "Failed to parse appcenter-config.json", e);
