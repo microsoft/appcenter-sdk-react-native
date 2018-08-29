a978c5b375caa5f2b20199af7cc47ad4cc56518f
Di Hei <dihei@microsoft.com>
2018-08-29 12:01:54 -0700

    Rename target tokens to align with aria portal

--- DemoApp/app/Constants.ios.js
+++ DemoApp/app/Constants.ios.js
@@ -1,14 +1,14 @@
 const Constants = {
   targetTokens: [{
-    label: 'Target Token 1',
+    label: 'Parent Target Token',
     key: '73cb994e3ad44e56acf55fd873b7f06e-4a3684af-3d9f-41b2-a492-e7adf6e5ed66-7343'
   },
   {
-    label: 'Target Token 2 (Child of 1)',
+    label: 'Child Target Token',
     key: 'f8615f6d3ad242d5abd817b50b43d485-23e91290-319c-43c7-9a40-3a1e663ba695-6909'
   },
   {
-    label: 'Target Token 3 (Child of 2)',
+    label: 'Grandchild Target Token',
     key: '0db07a59bd744afea64287beee33291f-f4181552-9df9-4a4d-b017-57c457b2ec3c-6976'
   },
   {
