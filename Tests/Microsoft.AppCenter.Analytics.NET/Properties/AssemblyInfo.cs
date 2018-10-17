752b877ac6af90f37e5c53459458dac6a47cd006
Ivan Matkov <matkov.ivan@gmail.com>
2018-10-17 22:49:12 +0300

    Bump sdk version to 1.10.0

--- Tests/Microsoft.AppCenter.Analytics.NET/Properties/AssemblyInfo.cs
+++ Tests/Microsoft.AppCenter.Analytics.NET/Properties/AssemblyInfo.cs
@@ -33,7 +33,7 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("0.0.0.0")]
-[assembly: AssemblyFileVersion("1.9.1.0")]
-[assembly: AssemblyInformationalVersion("1.9.1-SNAPSHOT")]
+[assembly: AssemblyFileVersion("1.10.0.0")]
+[assembly: AssemblyInformationalVersion("1.10.0-SNAPSHOT")]
 
 [assembly: InternalsVisibleTo("Microsoft.AppCenter.Analytics.Test.Windows")]
