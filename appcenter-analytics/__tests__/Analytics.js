bd37c1d3b8288ff79d05c7c226d971ad3532961c
Murat Baysangurov <v-mubays@microsoft.com>
2018-11-15 20:13:52 +0300

    Fix eslint errors

--- appcenter-analytics/__tests__/Analytics.js
+++ appcenter-analytics/__tests__/Analytics.js
@@ -2,7 +2,6 @@ import { NativeModules } from 'react-native';
 import Analytics from '../Analytics';
 
 describe('Native module methods are called', () => {
-
   test('isEnabled is called', async () => {
     const spy = jest.spyOn(NativeModules.AppCenterReactNativeAnalytics, 'isEnabled');
     await Analytics.isEnabled();
@@ -10,26 +9,25 @@ describe('Native module methods are called', () => {
   });
 
   test('trackEvent is called', async () => {
-    let eventName = "Test event";
-    let eventProperties = {"string": "value", "number": 1, "boolean": true};
-    let expectedProperties = {"string": "value", "number": "1", "boolean": "true"};
+    const eventName = 'Test event';
+    const eventProperties = { string: 'value', number: 1, boolean: true };
+    const expectedProperties = { string: 'value', number: '1', boolean: 'true' };
     const spy = jest.spyOn(NativeModules.AppCenterReactNativeAnalytics, 'trackEvent');
     await Analytics.trackEvent(eventName, eventProperties);
     expect(spy).toHaveBeenCalledWith(eventName, expectedProperties);
   });
 
   test('setEnabled is called', async () => {
-    let enabled = true;
+    const enabled = true;
     const spy = jest.spyOn(NativeModules.AppCenterReactNativeAnalytics, 'setEnabled');
     await Analytics.setEnabled(enabled);
     expect(spy).toHaveBeenCalledWith(enabled);
   });
 
   test('getTransmissionTarget is called', async () => {
-    let token = "Test token";
+    const token = 'Test token';
     const spy = jest.spyOn(NativeModules.AppCenterReactNativeAnalytics, 'getTransmissionTarget');
     await Analytics.getTransmissionTarget(token);
     expect(spy).toHaveBeenCalledWith(token);
   });
-
 });
--- appcenter-analytics/jest/setup.js
+++ appcenter-analytics/jest/setup.js
@@ -1,10 +1,8 @@
-jest.mock('NativeModules', () => {
-  return {
-    AppCenterReactNativeAnalytics:{
-      isEnabled: jest.fn(),
-      trackEvent: jest.fn(),
-      setEnabled: jest.fn(),
-      getTransmissionTarget: jest.fn(() => new Promise((resolve) => { resolve()}))
-    }
-  };
-});
+jest.mock('NativeModules', () => ({
+  AppCenterReactNativeAnalytics: {
+    isEnabled: jest.fn(),
+    trackEvent: jest.fn(),
+    setEnabled: jest.fn(),
+    getTransmissionTarget: jest.fn(() => new Promise(resolve => resolve()))
+  }
+}));
