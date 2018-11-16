4f686160ce0f61624702d636a0477a4ce9036ac9
Murat Baysangurov <v-mubays@microsoft.com>
2018-11-16 15:34:08 +0300

    Updated unit tests

--- appcenter-analytics/jest/setup.js
+++ appcenter-analytics/jest/setup.js
@@ -3,6 +3,16 @@ jest.mock('NativeModules', () => ({
     isEnabled: jest.fn(),
     trackEvent: jest.fn(),
     setEnabled: jest.fn(),
-    getTransmissionTarget: jest.fn(() => new Promise(resolve => resolve()))
+    getTransmissionTarget: jest.fn(() => new Promise(resolve => resolve())),
+    setTransmissionTargetAppName: jest.fn(),
+    setTransmissionTargetAppVersion: jest.fn(),
+    setTransmissionTargetAppLocale: jest.fn(),
+    setTransmissionTargetEventProperty: jest.fn(),
+    removeTransmissionTargetEventProperty: jest.fn(),
+    collectTransmissionTargetDeviceId: jest.fn(),
+    isTransmissionTargetEnabled: jest.fn(),
+    setTransmissionTargetEnabled: jest.fn(),
+    trackTransmissionTargetEvent: jest.fn(),
+    getChildTransmissionTarget: jest.fn(() => new Promise(resolve => resolve()))
   }
 }));
