61febe8be81b099eb859681cf1f9b1c614b50042
Bryan Smith <bryanj.smith91@gmail.com>
2019-01-14 11:37:02 -0800

    Consume new SetUserId in Contoso.iOS.Puppet project.

--- Apps/Contoso.iOS.Puppet/ModulePages/AppCenterController.designer.cs
+++ Apps/Contoso.iOS.Puppet/ModulePages/AppCenterController.designer.cs
@@ -1,64 +1,84 @@
 // WARNING
 //
-// This file has been generated automatically by Xamarin Studio to store outlets and
-// actions made in the UI designer. If it is removed, they will be lost.
-// Manual changes to this file may not be handled correctly.
+// This file has been generated automatically by Visual Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
 //
 using Foundation;
+using System;
 using System.CodeDom.Compiler;
 
 namespace Contoso.iOS.Puppet
 {
-	[Register ("AppCenterController")]
-	partial class AppCenterController
-	{
-		[Outlet]
-		UIKit.UILabel LogLevelLabel { get; set; }
+    [Register ("AppCenterController")]
+    partial class AppCenterController
+    {
+        [Outlet]
+        UIKit.UILabel LogLevelLabel { get; set; }
 
-		[Outlet]
-		UIKit.UILabel LogWriteLevelLabel { get; set; }
 
-		[Outlet]
-		UIKit.UITextField LogWriteMessage { get; set; }
+        [Outlet]
+        UIKit.UILabel LogWriteLevelLabel { get; set; }
 
-		[Outlet]
-		UIKit.UITextField LogWriteTag { get; set; }
 
-		[Outlet]
-		UIKit.UISwitch AppCenterEnabledSwitch { get; set; }
+        [Outlet]
+        UIKit.UITextField LogWriteMessage { get; set; }
 
-		[Action ("UpdateEnabled")]
-		partial void UpdateEnabled ();
 
-		[Action ("WriteLog")]
-		partial void WriteLog ();
-		
-		void ReleaseDesignerOutlets ()
-		{
-			if (AppCenterEnabledSwitch != null) {
-				AppCenterEnabledSwitch.Dispose ();
-				AppCenterEnabledSwitch = null;
-			}
+        [Outlet]
+        UIKit.UITextField LogWriteTag { get; set; }
 
-			if (LogLevelLabel != null) {
-				LogLevelLabel.Dispose ();
-				LogLevelLabel = null;
-			}
 
-			if (LogWriteMessage != null) {
-				LogWriteMessage.Dispose ();
-				LogWriteMessage = null;
-			}
+        [Outlet]
+        UIKit.UISwitch AppCenterEnabledSwitch { get; set; }
 
-			if (LogWriteTag != null) {
-				LogWriteTag.Dispose ();
-				LogWriteTag = null;
-			}
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextField UserIdTextField { get; set; }
 
-			if (LogWriteLevelLabel != null) {
-				LogWriteLevelLabel.Dispose ();
-				LogWriteLevelLabel = null;
-			}
-		}
-	}
+
+        [Action ("UpdateEnabled")]
+        partial void UpdateEnabled ();
+
+
+        [Action ("WriteLog")]
+        partial void WriteLog ();
+
+        [Action ("UpdateUserId:")]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        partial void UpdateUserId (UIKit.UITextField sender);
+
+        void ReleaseDesignerOutlets ()
+        {
+            if (AppCenterEnabledSwitch != null) {
+                AppCenterEnabledSwitch.Dispose ();
+                AppCenterEnabledSwitch = null;
+            }
+
+            if (LogLevelLabel != null) {
+                LogLevelLabel.Dispose ();
+                LogLevelLabel = null;
+            }
+
+            if (LogWriteLevelLabel != null) {
+                LogWriteLevelLabel.Dispose ();
+                LogWriteLevelLabel = null;
+            }
+
+            if (LogWriteMessage != null) {
+                LogWriteMessage.Dispose ();
+                LogWriteMessage = null;
+            }
+
+            if (LogWriteTag != null) {
+                LogWriteTag.Dispose ();
+                LogWriteTag = null;
+            }
+
+            if (UserIdTextField != null) {
+                UserIdTextField.Dispose ();
+                UserIdTextField = null;
+            }
+        }
+    }
 }
