5729e294f2a983c62396be1f67549544d3046b9c
Stephanie Dover <stdover@microsoft.com>
2019-01-14 12:52:10 -0800

    Updated Android TestApp UI to include User ID

--- TestApp/app/screens/CrashesScreen.js
+++ TestApp/app/screens/CrashesScreen.js
@@ -26,7 +26,8 @@ export default class CrashesScreen extends Component {
     crashesEnabled: false,
     lastSessionStatus: '',
     textAttachment: '',
-    binaryAttachment: ''
+    binaryAttachment: '',
+    userId: ''
   }
 
   async componentWillMount() {
