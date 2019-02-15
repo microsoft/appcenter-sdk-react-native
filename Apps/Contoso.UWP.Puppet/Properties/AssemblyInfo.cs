7d2f1a7524fec74649c423e06d9b2dbe31f2025f
Subhashvysyaraju <31912978+Subhashvysyaraju@users.noreply.github.com>
2019-02-15 11:28:28 -0800

    Start new version cycle 1.13.2

--- Apps/Contoso.UWP.Puppet/Properties/AssemblyInfo.cs
+++ Apps/Contoso.UWP.Puppet/Properties/AssemblyInfo.cs
@@ -24,5 +24,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("0.0.0.0")]
-[assembly: AssemblyFileVersion("1.13.1.0")]
+[assembly: AssemblyFileVersion("1.13.2.0")]
 [assembly: ComVisible(false)]
