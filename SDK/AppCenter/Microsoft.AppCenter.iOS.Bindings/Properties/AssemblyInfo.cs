b259f38cd3236fd3fe58705bd7dcef61458ba63b
Guillaume Perrot <guperrot@microsoft.com>
2019-03-19 14:39:26 -0700

    Add copyright to AssemblyInfo.cs files

--- SDK/AppCenter/Microsoft.AppCenter.iOS.Bindings/Properties/AssemblyInfo.cs
+++ SDK/AppCenter/Microsoft.AppCenter.iOS.Bindings/Properties/AssemblyInfo.cs
@@ -1,4 +1,7 @@
 using System.Resources;
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
 using System.Reflection;
 using Foundation;
 
--- SDK/AppCenterCrashes/Microsoft.AppCenter.Crashes.iOS.Bindings/Properties/AssemblyInfo.cs
+++ SDK/AppCenterCrashes/Microsoft.AppCenter.Crashes.iOS.Bindings/Properties/AssemblyInfo.cs
@@ -1,4 +1,7 @@
 using Foundation;
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
 using System.Reflection;
 
 // This attribute allows you to mark your assemblies as “safe to link”. 
