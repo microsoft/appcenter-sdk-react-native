bba9125ad8edac8f96780dd5b1f34b405c380bd5
Alan Kenyon <alan@shipt.com>
2019-03-19 17:56:08 -0700

    initial changes

--- appcenter/scripts/postlink.js
+++ appcenter/scripts/postlink.js
@@ -29,7 +29,7 @@ if (rnpmlink.ios.checkIfAppDelegateExists()) {
         .then((file) => {
             console.log(`Added code to initialize iOS AppCenter SDK in ${file}`);
             return rnpmlink.ios.addPodDeps(
-                [{ pod: 'AppCenterReactNativeShared', version: '1.13.0' }],
+                [{ pod: 'AppCenterReactNative', path: '../node_modules/appcenter/ios' }],
                 { platform: 'ios', version: '9.0' }
             );
         })
