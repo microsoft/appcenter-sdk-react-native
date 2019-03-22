1f2825b3e3deeb26aa29ec132cb8e3c81cb6222e
Dmitry Trabo <v-dmtrab@microsoft.com>
2019-03-22 11:36:58 +0300

    Addressed feedback

--- DemoApp/ios/DemoApp/AppDelegate.h
+++ DemoApp/ios/DemoApp/AppDelegate.h
@@ -10,7 +10,6 @@
 #import <React/RCTBridgeDelegate.h>
 #import <UIKit/UIKit.h>
 
-@interface AppDelegate : UIResponder <UIApplicationDelegate>
 @interface AppDelegate : UIResponder <UIApplicationDelegate, RCTBridgeDelegate>
 
 @property (nonatomic, strong) UIWindow *window;
