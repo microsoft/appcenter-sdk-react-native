1e31ea4d393ea9c9a7a85560aac6018db6226f3a
Ivan Matkov <v-ivmatk@microsoft.com>
2019-04-19 15:06:22 +0300

    Address feedback

--- SDK/AppCenterAuth/Microsoft.AppCenter.Auth.Android/Properties/AssemblyInfo.cs
+++ SDK/AppCenterAuth/Microsoft.AppCenter.Auth.Android/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using Android.App;
@@ -27,4 +27,4 @@ using Android.App;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
