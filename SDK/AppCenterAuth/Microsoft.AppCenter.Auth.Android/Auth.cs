284315622ceada9273775baeb702cc94f448acca
Guillaume Perrot <guperrot@microsoft.com>
2019-04-18 20:31:09 -0700

    Work in progress to bind Android identity

---
+++ SDK/AppCenterAuth/Microsoft.AppCenter.Auth.Android/Auth.cs
@@ -0,0 +1,21 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Threading.Tasks;
+using Com.Microsoft.Appcenter.Identity;
+
+namespace Microsoft.AppCenter.Auth
+{
+    public partial class Auth
+    {
+        private static Task<SignInResult> PlatformSignInAsync()
+        {
+            Identity.signIn();
+            return Task.FromResult(new SignInResult
+            {
+                Exception = new NotImplementedException()
+            });
+        }
+    }
+}
