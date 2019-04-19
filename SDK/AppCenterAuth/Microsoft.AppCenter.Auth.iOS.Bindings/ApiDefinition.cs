c2621097ca3f828db1eab5ad5463ef10665536cc
Ivan Matkov <v-ivmatk@microsoft.com>
2019-04-19 17:52:21 +0300

    Add initial auth ios binding

---
+++ SDK/AppCenterAuth/Microsoft.AppCenter.Auth.iOS.Bindings/ApiDefinition.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+using ObjCRuntime;
+using Foundation;
+using UIKit;
+
+namespace NativeLibrary
+{
+    // @interface MSIdentity : MSService
+    [BaseType(typeof(NSObject))]
+    interface MSIdentity
+    {
+        // +(void)setEnabled:(BOOL)isEnabled;
+        [Static]
+        [Export("setEnabled:")]
+        void SetEnabled(bool isEnabled);
+
+        // +(BOOL)isEnabled;
+        [Static]
+        [Export("isEnabled")]
+        bool IsEnabled();
+
+        // + (void) signInWithCompletionHandler:(MSSignInCompletionHandler _Nullable) completionHandler;
+        [Static]
+        [Export("signInWithCompletionHandler:")]
+        void SignIn(MSSignInCompletionHandler completionHandler);
+
+        // + (void) signOut;
+        [Static]
+        [Export("signOut")]
+        void SignOut();
+    }
+
+    // typedef void (^MSSignInCompletionHandler)(MSUserInformation* _Nullable userInformation, NSError * _Nullable error);
+    delegate void MSSignInCompletionHandler([NullAllowed] MSUserInformation userInformation, [NullAllowed] NSError error);
+
+    [BaseType(typeof(NSObject))]
+    interface MSUserInformation
+    {
+        // @property(nonatomic, copy) NSString *accountId;
+        [Export("accountId")]
+        string AccountId { get; set; }
+    }
+}
---
+++ SDK/AppCenterAuth/Microsoft.AppCenter.Auth.iOS/Auth.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.Threading.Tasks;
+using Microsoft.AppCenter.Auth.iOS.Bindings;
+
+namespace Microsoft.AppCenter.Auth
+{
+    public partial class Auth : AppCenterService
+    {
+        private static Task<SignInResult> PlatformSignInAsync()
+        {
+            MSAuth.SignIn(null);
+            return null;
+        }
+    }
+}
