e8b370ef141ca4c9ad0ac017c698549545365563
Ivan Matkov <v-ivmatk@microsoft.com>
2019-04-23 15:33:37 +0300

    Add blank android binding projects for Data module

---
+++ SDK/AppCenterData/Microsoft.AppCenter.Data.Android/Data.cs
@@ -0,0 +1,65 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Threading.Tasks;
+
+namespace Microsoft.AppCenter.Data
+{
+    public partial class Data
+    {
+        private static void PlatformSetApiUrl(string apiUrl)
+        {
+        }
+
+        private static Task<bool> PlatformIsEnabledAsync()
+        {
+            return Task.FromResult(false);
+        }
+
+        private static Task PlatformSetEnabledAsync(bool enabled)
+        {
+            return Task.FromResult(default(object));
+        }
+
+        private static Task<Document<T>> PlatformRead<T>(string partition, string documentId)
+        {
+            return Task.FromResult<Document<T>>(null);
+        }
+
+        private static Task<Document<T>> PlatformRead<T>(string partition, string documentId, ReadOptions readOptions)
+        {
+            return Task.FromResult<Document<T>>(null);
+        }
+
+        private static Task<PaginatedDocuments<T>> PlatformList<T>(string partition)
+        {
+            return Task.FromResult<PaginatedDocuments<T>>(null);
+        }
+
+        private static Task<Document<T>> PlatformCreate<T>(string partition, string documentId, T document)
+        {
+            return Task.FromResult<Document<T>>(null);
+        }
+
+        private static Task<Document<T>> PlatformCreate<T>(string partition, string documentId, T document, WriteOptions writeOptions)
+        {
+            return Task.FromResult<Document<T>>(null);
+        }
+
+        private static Task<Document<object>> PlatformDelete(string partition, string documentId)
+        {
+            return Task.FromResult<Document<object>>(null);
+        }
+
+        private static Task<Document<T>> PlatformReplace<T>(string partition, string documentId, T document)
+        {
+            return Task.FromResult<Document<T>>(null);
+        }
+
+        private static Task<Document<T>> PlatformReplace<T>(string partition, string documentId, T document, WriteOptions writeOptions)
+        {
+            return Task.FromResult<Document<T>>(null);
+        }
+    }
+}
