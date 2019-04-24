3137b73e51681b2c5b1aecb17747efc865f66391
Anna Kocheshkova <v-annkoc@microsoft.com>
2019-04-24 18:52:45 +0300

    Fix imports

--- SDK/AppCenterData/Microsoft.AppCenter.Data.iOS/PaginatedDocuments.cs
+++ SDK/AppCenterData/Microsoft.AppCenter.Data.iOS/PaginatedDocuments.cs
@@ -1,9 +1,8 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-using System;
+using Foundation;
 using System.Threading.Tasks;
-using Newtonsoft.Json;
 using Microsoft.AppCenter.Data.iOS.Bindings;
 
 namespace Microsoft.AppCenter.Data
