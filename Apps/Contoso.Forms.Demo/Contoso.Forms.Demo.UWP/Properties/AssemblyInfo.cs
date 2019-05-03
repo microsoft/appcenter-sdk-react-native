825b9d612c6f20df4759207660211769363a085a
Guillaume Perrot <guperrot@microsoft.com>
2019-05-03 15:42:36 -0700

    Update demo with 2.0.0 and MBaaS

--- Apps/Contoso.Forms.Demo/Contoso.Forms.Demo.UWP/Properties/AssemblyInfo.cs
+++ Apps/Contoso.Forms.Demo/Contoso.Forms.Demo.UWP/Properties/AssemblyInfo.cs
@@ -27,5 +27,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("0.0.0.0")]
-[assembly: AssemblyFileVersion("1.14.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
 [assembly: ComVisible(false)]
--- Apps/Contoso.WPF.Demo/Properties/AssemblyInfo.cs
+++ Apps/Contoso.WPF.Demo/Properties/AssemblyInfo.cs
@@ -30,5 +30,5 @@ using System.Windows;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("0.0.0.0")]
-[assembly: AssemblyFileVersion("1.14.0.0")]
-[assembly: AssemblyInformationalVersion("1.14.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
+[assembly: AssemblyInformationalVersion("2.0.0")]
