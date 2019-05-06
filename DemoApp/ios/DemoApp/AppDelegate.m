34f55f0c0f04c3fcfa0dd7b2c9d7540baeabae71
Brynjulf Risbakken <brynjulfz@hotmail.com>
2019-05-06 16:16:15 +0200

    corrected moduleName reference
    The app registry name is DemoApp and not RnDiffApp

--- DemoApp/ios/DemoApp/AppDelegate.m
+++ DemoApp/ios/DemoApp/AppDelegate.m
@@ -48,7 +48,7 @@
 
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
   RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
-                                                   moduleName:@"RnDiffApp"
+                                                   moduleName:@"DemoApp"
                                             initialProperties:nil];
   rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
 
