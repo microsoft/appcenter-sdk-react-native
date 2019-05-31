2a021960df7ea33567ad0145bd7c7fd427d7c142
Jacky Lui <t-jalu@microsoft.com>
2019-05-31 12:14:30 -0700

    Added an import & formating

--- appcenter-data/android/src/main/java/com/microsoft/appcenter/reactnative/data/AppCenterReactNativeDataPackage.java
+++ appcenter-data/android/src/main/java/com/microsoft/appcenter/reactnative/data/AppCenterReactNativeDataPackage.java
@@ -8,6 +8,7 @@ package com.microsoft.appcenter.reactnative.data;
 import android.app.Application;
 
 import com.facebook.react.ReactPackage;
+import com.facebook.react.bridge.JavaScriptModule;
 import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
