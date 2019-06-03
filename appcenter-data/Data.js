029882d1171a0a63da08ce819a59c1d7cc6dbf65
Jacky Lui <t-jalu@microsoft.com>
2019-06-03 16:09:39 -0700

    Changed quotes to single

--- appcenter-data/Data.js
+++ appcenter-data/Data.js
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-const ReactNative = require("react-native");
+const ReactNative = require('react-native');
 
 const { AppCenterReactNativeData } = ReactNative.NativeModules;
 
