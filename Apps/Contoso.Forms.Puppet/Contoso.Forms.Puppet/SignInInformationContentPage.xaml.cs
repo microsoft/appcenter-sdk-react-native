eb070aed898d50c5710dd2b0f78c986efaa29553
Dmitrii Chichuk <v-dmchic@microsoft.com>
2019-06-04 15:40:36 +0300

    SignInInformationContentPage implementation

--- Apps/Contoso.Forms.Puppet/Contoso.Forms.Puppet/SignInInformationContentPage.xaml.cs
+++ Apps/Contoso.Forms.Puppet/Contoso.Forms.Puppet/SignInInformationContentPage.xaml.cs
@@ -1,15 +1,27 @@
 ﻿using System;
-using System.Collections.Generic;
-
 using Xamarin.Forms;
 
 namespace Contoso.Forms.Puppet
 {
     public partial class SignInInformationContentPage : ContentPage
     {
+
         public SignInInformationContentPage()
         {
             InitializeComponent();
+        }
+
+        public SignInInformationContentPage(string accountIdText, string accessTokenText, string idTokenText)
+        {
+            InitializeComponent();
+            AccountIdEntry.Text = accountIdText;
+            AccessTokenEntry.Text = accessTokenText;
+            IdTokenEntry.Text = idTokenText;
+        }
+
+        async void OK(object sender, EventArgs e)
+        {
+            await Navigation.PopModalAsync();
         }
     }
 }
