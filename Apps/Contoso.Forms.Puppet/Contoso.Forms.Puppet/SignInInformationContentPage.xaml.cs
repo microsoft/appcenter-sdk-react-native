38a9b94876164fbb09fcedc1f25ecf365895f025
Dmitrii Chichuk <v-dmchic@microsoft.com>
2019-06-04 15:38:41 +0300

    Add SignInInformationContentPage xaml

---
+++ Apps/Contoso.Forms.Puppet/Contoso.Forms.Puppet/SignInInformationContentPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Contoso.Forms.Puppet
+{
+    public partial class SignInInformationContentPage : ContentPage
+    {
+        public SignInInformationContentPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
