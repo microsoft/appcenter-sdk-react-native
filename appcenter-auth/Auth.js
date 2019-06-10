5b4e05315c278e058621aa5c7ef22a5f45dfe6a8
Jacky Lui <t-jalu@microsoft.com>
2019-06-10 14:18:05 -0700

    Removed unused imports to make eslint happy

--- appcenter-auth/Auth.js
+++ appcenter-auth/Auth.js
@@ -1,12 +1,6 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-const ReactNative = require('react-native');
-
-const { AppCenterReactNativeAuth } = ReactNative.NativeModules;
-
-const Auth = {
-    
-};
+const Auth = {};
 
 module.exports = Auth;
