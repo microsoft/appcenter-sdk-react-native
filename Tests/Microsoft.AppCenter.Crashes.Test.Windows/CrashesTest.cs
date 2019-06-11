605a11f67fc41e37caf8bbd89b1aac9f292acde1
Michael Smith <mismith@microsoft.com>
2019-06-11 11:40:08 -0700

    added enable/disable functionality

---
+++ Tests/Microsoft.AppCenter.Crashes.Test.Windows/CrashesTest.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Microsoft.AppCenter.Crashes.Test.Windows
+{
+    [TestClass]
+    public class CrashesTest
+    {
+        [TestInitialize]
+        public void InitializeCrashTest()
+        {
+            Crashes.Instance = new Crashes();
+        }
+
+        [TestMethod]
+        public void InstanceIsNotNull()
+        {
+            Crashes.Instance = null;
+            Assert.IsNotNull(Crashes.Instance);
+        }
+
+        [TestMethod]
+        public void GetEnabled()
+        {
+            Crashes.SetEnabledAsync(false).Wait();
+            Assert.IsFalse(Crashes.IsEnabledAsync().Result);
+
+            Crashes.SetEnabledAsync(true).Wait();
+            Assert.IsTrue(Crashes.IsEnabledAsync().Result);
+        }
+    }
+}
---
+++ Tests/Microsoft.AppCenter.Crashes.Test.Windows/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Microsoft.AppCenter.Crashes.Test.Windows")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Microsoft.AppCenter.Crashes.Test.Windows")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1f1dbe94-aaee-4a43-8586-ef8743672587")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
