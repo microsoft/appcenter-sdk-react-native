9703689f72aca7b002e9b665f33401caae3293a0
Guillaume Perrot <guperrot@microsoft.com>
2019-06-12 15:06:31 -0700

    Fix sending HTTP logs from windows desktop apps

---
+++ SDK/AppCenter/Microsoft.AppCenter.WindowsDesktop/Ingestion/Http/HttpNetworkAdapter.cs
@@ -0,0 +1,22 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.Net;
+
+namespace Microsoft.AppCenter.Ingestion.Http
+{
+    /// <inheritdoc />
+    public sealed partial class HttpNetworkAdapter
+    {
+        // Static initializer specific to windows desktop platforms.
+        static HttpNetworkAdapter()
+        {
+            // ReSharper disable once InvertIf
+            if ((ServicePointManager.SecurityProtocol & SecurityProtocolType.Tls12) != SecurityProtocolType.Tls12)
+            {
+                ServicePointManager.SecurityProtocol |= SecurityProtocolType.Tls12;
+                AppCenterLog.Debug(AppCenterLog.LogTag, "Enabled TLS 1.2 explicitly as it was disabled.");
+            }
+        }
+    }
+}
