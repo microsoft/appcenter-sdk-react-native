7b731ac0ce9bcb96a961b8bd3c33a1cf1e90cd1c
Guillaume Perrot <guperrot@microsoft.com>
2019-06-13 15:48:49 -0700

    Report puppet changes to demo

--- Apps/Contoso.WPF.Demo/App.xaml.cs
+++ Apps/Contoso.WPF.Demo/App.xaml.cs
@@ -11,7 +11,7 @@ namespace Contoso.WPF.Demo
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
-    public partial class App : Application
+    public partial class App
     {
         protected override void OnStartup(StartupEventArgs e)
         {
