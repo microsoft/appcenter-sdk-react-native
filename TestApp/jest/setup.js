0d4d5c1331d3d97f6031172df494551b690df8fe
Dmitry Trabo <v-dmtrab@microsoft.com>
2019-06-17 18:37:22 +0300

    Fixed tests

--- TestApp/jest/setup.js
+++ TestApp/jest/setup.js
@@ -3,3 +3,6 @@
 
 // https://github.com/expo/expo/issues/1705#issuecomment-385338032
 require('stacktrace-parser');
+import mockAsyncStorage from '@react-native-community/async-storage/jest/async-storage-mock';
+
+jest.mock('@react-native-community/async-storage', () => mockAsyncStorage);
