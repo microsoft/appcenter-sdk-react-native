8ea24fd7142ac1a85bc65980d0d9ab03b86427f7
Michael Smith <mismith@microsoft.com>
2019-06-17 15:39:37 -0700

    testing progress

--- Tests/Microsoft.AppCenter.Crashes.Test.Windows/CrashesTest.cs
+++ Tests/Microsoft.AppCenter.Crashes.Test.Windows/CrashesTest.cs
@@ -7,31 +7,30 @@ using Microsoft.AppCenter.Channel;
 using Microsoft.AppCenter.Utils;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
+using Microsoft.AppCenter.Crashes.Ingestion.Models;
 
 namespace Microsoft.AppCenter.Crashes.Test.Windows
 {
     [TestClass]
     public class CrashesTest
     {
-
-        private ApplicationLifecycleHelper _applicationLifecycleHelper;
         private Mock<IChannelGroup> _mockChannelGroup;
         private Mock<IChannelUnit> _mockChannel;
+        private Mock<ErrorLogHelper> _mockErrorLogHelper;
+        private Mock<IApplicationLifecycleHelper> _mockApplicationLifecycleHelper;
 
         [TestInitialize]
         public void InitializeCrashTest()
         {
             Crashes.Instance = new Crashes();
-            ErrorLogHelper.ProcessInformation = Mock.Of<IProcessInformation>();
-            ErrorLogHelper.DeviceInformationHelper = Mock.Of<IDeviceInformationHelper>();
-
-            _applicationLifecycleHelper = new ApplicationLifecycleHelper();
             _mockChannelGroup = new Mock<IChannelGroup>();
             _mockChannel = new Mock<IChannelUnit>();
+            _mockErrorLogHelper = new Mock<ErrorLogHelper>();
+            _mockApplicationLifecycleHelper = new Mock<IApplicationLifecycleHelper>();
             _mockChannelGroup.Setup(
                     group => group.AddChannel(It.IsAny<string>(), It.IsAny<int>(), It.IsAny<TimeSpan>(), It.IsAny<int>()))
                 .Returns(_mockChannel.Object);
-            ApplicationLifecycleHelper.Instance = _applicationLifecycleHelper;
+            //ApplicationLifecycleHelper.Instance = _applicationLifecycleHelper;
         }
 
         [TestMethod]
@@ -58,8 +57,13 @@ namespace Microsoft.AppCenter.Crashes.Test.Windows
             Crashes.SetEnabledAsync(true).Wait();
             Crashes.Instance.OnChannelGroupReady(_mockChannelGroup.Object, string.Empty);
 
-            // listener is listening
+            // Raise an arbitrary event for UnhandledExceptionOccurred handler
+            _mockApplicationLifecycleHelper.Raise(eventExpression => eventExpression.UnhandledExceptionOccurred += null,
+                new UnhandledExceptionOccurredEventArgs(new System.Exception("test")));
 
+            // Channel is enabled and listener is listening
+            _mockChannel.Verify(channel => channel.SetEnabled(true), Times.Once());
+            _mockErrorLogHelper.Verify(errorLogHelper => ErrorLogHelper.SaveErrorLogFile(It.IsAny<ManagedErrorLog>()), Times.Once());
         }
 
         [TestMethod]
@@ -69,8 +73,26 @@ namespace Microsoft.AppCenter.Crashes.Test.Windows
             Crashes.SetEnabledAsync(false).Wait();
             Crashes.Instance.OnChannelGroupReady(_mockChannelGroup.Object, string.Empty);
 
+            _mockChannel.Verify(channel => channel.SetEnabled(false), Times.Once());
             // listener stops listening
+            //_applicationLifecycleHelper.InvokeUnhandledException();
+
+            // listener is listening
+            _mockErrorLogHelper.Verify(asdasd => ErrorLogHelper.SaveErrorLogFile(It.IsAny<ManagedErrorLog>()), Times.Never());
+            
             // error log files are deleted
+            _mockErrorLogHelper.Verify(asdas => ErrorLogHelper.RemoveStoredErrorLogFile(It.IsAny<Guid>()), Times.Once());
+        }
+
+        [TestMethod]
+        public void ApplyEnabledStateDeleteFails()
+        {
+            // disabled state is applied
+            Crashes.SetEnabledAsync(false).Wait();
+            Crashes.Instance.OnChannelGroupReady(_mockChannelGroup.Object, string.Empty);
+
+            // file deletion throws
+            
         }
     }
 }
