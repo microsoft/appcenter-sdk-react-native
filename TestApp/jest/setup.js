654a9817127605f24dc3f98525e50cb74e70e8c7
Ruslan Urmeev <v-ruurme@microsoft.com>
2019-06-18 14:15:37 +0400

    removed targetSdkVersion from AndroidManifest; moved comment in test file;

--- TestApp/jest/setup.js
+++ TestApp/jest/setup.js
@@ -1,9 +1,9 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-// https://github.com/expo/expo/issues/1705#issuecomment-385338032
 import mockAsyncStorage from '@react-native-community/async-storage/jest/async-storage-mock';
 
+// https://github.com/expo/expo/issues/1705#issuecomment-385338032
 require('stacktrace-parser');
 
 jest.mock('@react-native-community/async-storage', () => mockAsyncStorage);
