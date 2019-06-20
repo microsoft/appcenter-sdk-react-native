be5a9db46e11be3c005a8851efcc8067d28a60d8
Alexander Chocron <alchocro@microsoft.com>
2019-06-20 15:19:12 -0700

    Change location of database in WPF/winforms

--- SDK/AppCenter/Microsoft.AppCenter.UWP/Utils/Constants.cs
+++ SDK/AppCenter/Microsoft.AppCenter.UWP/Utils/Constants.cs
@@ -12,5 +12,8 @@ namespace Microsoft.AppCenter.Utils
     {
         // File Storage.
         public static readonly string AppCenterFilesDirectoryPath = Path.Combine(global::Windows.Storage.ApplicationData.Current.LocalFolder.Path, "Microsoft", "AppCenter");
+
+        // The database file does not go in the main App Center files folder because it cannot be changed without migrating.
+        public static readonly string AppCenterDatabasePath = "Microsoft.AppCenter.Storage";
     }
 }
