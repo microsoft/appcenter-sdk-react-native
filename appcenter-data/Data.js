7e64166dc84c707cf3a783df7a068ee766a0bc12
Jacky Lui <t-jalu@microsoft.com>
2019-06-26 14:55:19 -0700

    Added JSdocs

--- appcenter-data/Data.js
+++ appcenter-data/Data.js
@@ -19,19 +19,42 @@ const DefaultPartitions = {
 const Data = {
     TimeToLive,
     DefaultPartitions,
-    read(documentId, partition, readOptions) {
-        if (readOptions === undefined) {
-            readOptions = new Data.ReadOptions(TimeToLive.DEFAULT);
-        }
-        return AppCenterReactNativeData.read(documentId, partition, readOptions).then(convertTimestampToDate);
-    },
-    create(documentId, document, partition, writeOptions) {
-        if (writeOptions === undefined) {
-            writeOptions = new Data.WriteOptions(TimeToLive.DEFAULT);
-        }
-        return AppCenterReactNativeData.create(documentId, document, partition, writeOptions).then(convertTimestampToDate);
-    }
+    read,
+    create
 };
+
+/**
+ * Read a document.
+ *
+ * @param {string} documentId - The CosmosDB document id.
+ * @param {string} partition - The CosmosDB partition key.
+ * @param {object} readOptions - Cache read options when the operation is done offline.
+ * @return {Promise} Future asynchronous operation with result being the document with metadata.
+ * If the operation fails, the error can be checked by reading the error object.
+ */
+function read(documentId, partition, readOptions) {
+    if (readOptions === undefined) {
+        readOptions = new Data.ReadOptions(TimeToLive.DEFAULT);
+    }
+    return AppCenterReactNativeData.read(documentId, partition, readOptions).then(convertTimestampToDate);
+}
+
+/**
+ * Create a document
+ *
+ * @param {string} documentId - The CosmosDB document id.
+ * @param {object} document - The document.
+ * @param {string} partition - The CosmosDB partition key.
+ * @param {object} writeOptions - Cache write options when the operation is done offline.
+ * @return {Promise} Future asynchronous operation with result being the document with metadata.
+ * If the operation fails, the error can be checked by reading the error object.
+ */
+function create(documentId, document, partition, writeOptions) {
+    if (writeOptions === undefined) {
+        writeOptions = new Data.WriteOptions(TimeToLive.DEFAULT);
+    }
+    return AppCenterReactNativeData.create(documentId, document, partition, writeOptions).then(convertTimestampToDate);
+}
 
 Data.ReadOptions = class {
     constructor(timeToLive) {
@@ -46,9 +69,11 @@ Data.WriteOptions = class {
 };
 
 function convertTimestampToDate(result) {
-    // Create a new `Date` object from timestamp as milliseconds
+    // Create a new `Date` object from timestamp as milliseconds.
     result.lastUpdatedDate = new Date(result.lastUpdatedDate);
     return result;
 }
+
+this.read();
 
 module.exports = Data;
