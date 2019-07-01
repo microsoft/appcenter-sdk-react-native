15607fd5a9aa0ea226146243f299de6976299dc6
Timothy Jyung <tijyung@microsoft.com>
2019-07-01 12:58:43 -0700

    addressed feedback

--- SDK/AppCenterData/Microsoft.AppCenter.Data.Shared/SharedConversionExtensions.cs
+++ SDK/AppCenterData/Microsoft.AppCenter.Data.Shared/SharedConversionExtensions.cs
@@ -1,17 +1,14 @@
-﻿using System;
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
 
 namespace Microsoft.AppCenter.Data
 {
     public static class SharedConversionExtensions
     {
-        public static DocumentWrapper<T> ToDocumentWrapper<T>(
-            string partition,
-            string eTag,
-            string id,
-            bool isFromDeviceCache,
-            string jsonValue,
-            Func<string, T> deserealizer,
-            DateTimeOffset date)
+        public static DocumentWrapper<T> ToDocumentWrapper<T>(string partition, string eTag, string id, bool isFromDeviceCache,
+            string jsonValue, Func<string, T> deserealizer, DateTimeOffset date)
         {
             DocumentWrapper<T> parsedDocument = new DocumentWrapper<T>
             {
