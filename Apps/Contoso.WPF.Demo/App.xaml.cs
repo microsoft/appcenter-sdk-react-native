ca3768ee2e13fa6c0f80dfd236599f52f076b245
Anastasia Kubova <anastasia.kubova@akvelon.com>
2019-07-03 14:32:21 +0300

    added callback

--- Apps/Contoso.WPF.Demo/App.xaml.cs
+++ Apps/Contoso.WPF.Demo/App.xaml.cs
@@ -1,6 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+using Contoso.WPF.Demo.Properties;
 using Microsoft.AppCenter;
 using Microsoft.AppCenter.Analytics;
 using Microsoft.AppCenter.Crashes;
@@ -16,6 +17,13 @@ namespace Contoso.WPF.Demo
         protected override void OnStartup(StartupEventArgs e)
         {
             AppCenter.LogLevel = LogLevel.Verbose;
+            Crashes.GetErrorAttachments = (ErrorReport report) =>
+            {
+                return new ErrorAttachmentLog[]
+                {
+                    ErrorAttachmentLog.AttachmentWithText(Settings.Default.TextErrorAttachments, Settings.Default.FileErrorAttachments),
+                };
+            };
             AppCenter.Start("f4e2a83d-3052-4884-8176-8b2c50277d16", typeof(Analytics), typeof(Crashes));
         }
     }
