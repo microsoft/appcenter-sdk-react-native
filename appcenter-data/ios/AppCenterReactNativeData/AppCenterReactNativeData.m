67c4b3ad74daa122427474514a9ad912aad1d8f8
Jacky Lui <t-jalu@microsoft.com>
2019-07-05 16:28:36 -0700

    Implemented read, create, replace, and remove on iOS

--- appcenter-data/ios/AppCenterReactNativeData/AppCenterReactNativeData.m
+++ appcenter-data/ios/AppCenterReactNativeData/AppCenterReactNativeData.m
@@ -23,8 +23,11 @@
 
 #import <AppCenter/MSAppCenter.h>
 #import <AppCenterData/MSData.h>
+#import <AppCenterData/MSDataError.h>
 #import <AppCenterData/MSDictionaryDocument.h>
 #import <AppCenterData/MSDocumentWrapper.h>
+#import <AppCenterData/MSReadOptions.h>
+#import <AppCenterData/MSWriteOptions.h>
 #import <AppCenterReactNativeShared/AppCenterReactNativeShared.h>
 
 @interface AppCenterReactNativeData () <RCTBridgeModule>
@@ -33,13 +36,21 @@
 
 @implementation AppCenterReactNativeData
 
-static NSString *const kMSDeserializedValue = @"deserializedValue";
-static NSString *const kMSjsonValue = @"jsonValue";
-static NSString *const kMSPartition = @"partition";
-static NSString *const kMSId = @"id";
-static NSString *const kMSETag = @"eTag";
-static NSString *const kMSLastUpdatedDate = @"lastUpdatedDate";
-static NSString *const kMSIsFromDeviceCache = @"isFromDeviceCache";
+static NSString *const kMSDeserializedValueKey = @"deserializedValue";
+
+static NSString *const kMSjsonValueKey = @"jsonValue";
+
+static NSString *const kMSPartitionKey = @"partition";
+
+static NSString *const kMSIDKey = @"id";
+
+static NSString *const kMSETagKey = @"eTag";
+
+static NSString *const kMSLastUpdatedDateKey = @"lastUpdatedDate";
+
+static NSString *const kMSIsFromDeviceCacheKey = @"isFromDeviceCache";
+
+static NSString *const kMSTimeToLiveKey = @"timeToLive";
 
 RCT_EXPORT_MODULE();
 
@@ -50,24 +61,102 @@ RCT_EXPORT_MODULE();
     }
 }
 
-RCT_EXPORT_METHOD(read:(NSString *)documentId
-                  partition:(NSString *) partition
+RCT_EXPORT_METHOD(read:(NSString *)documentID
+                  partition:(NSString *)partition
+                  readOptions:(NSDictionary *)readOptionsMap
+                  resolver:(RCTPromiseResolveBlock)resolve
+                  rejecter:(RCTPromiseRejectBlock)reject) {
+    MSReadOptions * readOptions = [self getReadOptions:readOptionsMap];
+    [MSData readDocumentWithID:documentID
+                  documentType:[MSDictionaryDocument class]
+                     partition:partition
+                   readOptions:readOptions
+             completionHandler:[self dataCompletionHandler:@"Failed read" reject:reject resolve:resolve]];
+}
+
+RCT_EXPORT_METHOD(create:(NSString *)documentID
+                  partition:(NSString *)partition
+                  document: (NSDictionary *)documentMap
+                  writeOptions:(NSDictionary *)writeOptionsMap
                   resolver:(RCTPromiseResolveBlock)resolve
                   rejecter:(RCTPromiseRejectBlock)reject){
-    [MSData readDocumentWithID: documentId
-                  documentType:[MSDictionaryDocument class]
-                     partition: partition
-             completionHandler:^(MSDocumentWrapper *_Nonnull document) {
-                 NSMutableDictionary *dict = [[NSMutableDictionary alloc] init];
-                 dict[kMSDeserializedValue] = [document.deserializedValue serializeToDictionary];
-                 dict[kMSjsonValue] = document.jsonValue;
-                 dict[kMSPartition] = document.partition;
-                 dict[kMSId] = document.documentId;
-                 dict[kMSETag] = document.eTag;
-                 dict[kMSLastUpdatedDate] = @([document.lastUpdatedDate timeIntervalSince1970] * 1000);
-                 dict[kMSIsFromDeviceCache] = [NSNumber numberWithBool:document.fromDeviceCache];
-                 return resolve(dict);
-             }];
+    MSWriteOptions * writeOptions = [self getWriteOptions:writeOptionsMap];
+    MSDictionaryDocument *document = [[MSDictionaryDocument alloc] initFromDictionary:documentMap];
+    [MSData createDocumentWithID:documentID
+                        document:document
+                       partition:partition
+                    writeOptions:writeOptions
+               completionHandler:[self dataCompletionHandler:@"Failed create" reject:reject resolve:resolve]];
+}
+
+RCT_EXPORT_METHOD(replace:(NSString *)documentID
+                  partition:(NSString *)partition
+                  document:(NSDictionary *)documentMap
+                  writeOptions:(NSDictionary *)writeOptionsMap
+                  resolver:(RCTPromiseResolveBlock)resolve
+                  rejecter:(RCTPromiseRejectBlock)reject){
+    MSWriteOptions * writeOptions = [self getWriteOptions:writeOptionsMap];
+    MSDictionaryDocument *document = [[MSDictionaryDocument alloc] initFromDictionary:documentMap];
+    [MSData replaceDocumentWithID:documentID
+                         document:document
+                        partition:partition
+                     writeOptions:writeOptions
+                completionHandler:[self dataCompletionHandler:@"Failed replace" reject:reject resolve:resolve]];
+}
+
+RCT_EXPORT_METHOD(remove:(NSString *)documentID
+                  partition:(NSString *)partition
+                  writeOptions:(NSDictionary* )writeOptionsMap
+                  resolver:(RCTPromiseResolveBlock)resolve
+                  rejecter:(RCTPromiseRejectBlock)reject) {
+    MSWriteOptions * writeOptions = [self getWriteOptions:writeOptionsMap];
+    [MSData deleteDocumentWithID:documentID
+                       partition:partition
+                    writeOptions:writeOptions
+               completionHandler:[self dataCompletionHandler:@"Failed remove" reject:reject resolve:resolve]];
+}
+
+- (void (^)(MSDocumentWrapper * _Nonnull))dataCompletionHandler:(NSString*)errorCode
+                                                         reject:(RCTPromiseRejectBlock)reject
+                                                        resolve:(RCTPromiseResolveBlock)resolve {
+    return ^(MSDocumentWrapper * _Nonnull documentWrapper) {
+        NSMutableDictionary *jsDocumentWrapper = [[NSMutableDictionary alloc] init];
+        jsDocumentWrapper[kMSjsonValueKey] = documentWrapper.jsonValue;
+        jsDocumentWrapper[kMSPartitionKey] = documentWrapper.partition;
+        jsDocumentWrapper[kMSIDKey] = documentWrapper.documentId;
+        jsDocumentWrapper[kMSETagKey] = documentWrapper.eTag;
+        jsDocumentWrapper[kMSLastUpdatedDateKey] = @([documentWrapper.lastUpdatedDate timeIntervalSince1970] * 1000);
+        jsDocumentWrapper[kMSIsFromDeviceCacheKey] = [NSNumber numberWithBool:documentWrapper.fromDeviceCache];
+        if (documentWrapper.error) {
+            MSDataError *dataError = documentWrapper.error;
+            [jsDocumentWrapper addEntriesFromDictionary:dataError.userInfo];
+            NSError *error = [[NSError alloc] initWithDomain:dataError.domain code:dataError.code userInfo:jsDocumentWrapper];
+            reject(@"nope", error.description, error);
+            return;
+        }
+        jsDocumentWrapper[kMSDeserializedValueKey] = [documentWrapper.deserializedValue serializeToDictionary];
+        return resolve(jsDocumentWrapper);
+    };
+}
+
+- (MSReadOptions *)getReadOptions:(NSDictionary *)readOptionsMap {
+    MSReadOptions *readOptions;
+    if (!readOptionsMap && [readOptionsMap valueForKey:kMSTimeToLiveKey]) {
+        readOptions = [[MSReadOptions alloc] initWithDeviceTimeToLive:[[readOptionsMap valueForKey:kMSTimeToLiveKey] integerValue]];
+    } else {
+        readOptions = [[MSReadOptions alloc] init];
+    }
+    return readOptions;
+}
+
+- (MSWriteOptions *)getWriteOptions:(NSDictionary *)writeOptionsMap {
+    MSWriteOptions *writeOptions;
+    if (!writeOptionsMap && [writeOptionsMap valueForKey:kMSTimeToLiveKey]) {
+        writeOptions = [[MSWriteOptions alloc] initWithDeviceTimeToLive:[[writeOptionsMap valueForKey:kMSTimeToLiveKey] integerValue]];
+    } else {
+        writeOptions = [[MSWriteOptions alloc] init];
+    }
+    return writeOptions;
 }
 
 @end
