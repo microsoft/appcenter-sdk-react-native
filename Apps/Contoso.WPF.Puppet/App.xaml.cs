5c438c0bad3546da0173f76da1137c99a49e213b
Anastasia Kubova <anastasia.kubova@akvelon.com>
2019-07-08 14:14:17 +0300

    Add  crashes callbacks functionality to WPF Puppet

--- Apps/Contoso.WPF.Puppet/App.xaml.cs
+++ Apps/Contoso.WPF.Puppet/App.xaml.cs
@@ -14,14 +14,91 @@ namespace Contoso.WPF.Puppet
     /// </summary>
     public partial class App
     {
+        private const string LogTag = "AppCenterSasquatch";
+
         protected override void OnStartup(StartupEventArgs e)
         {
             AppCenter.LogLevel = LogLevel.Verbose;
             AppCenter.SetLogUrl("https://in-integration.dev.avalanch.es");
-            Crashes.SendingErrorReport += (sender, args) => Console.WriteLine($@"[App] Sending report Id={args.Report.Id} Exception={args.Report.Exception}");
-            Crashes.SentErrorReport += (sender, args) => Console.WriteLine($@"[App] Sent report Id={args.Report.Id}");
-            Crashes.FailedToSendErrorReport += (sender, args) => Console.WriteLine($@"[App] FailedToSend report Id={args.Report.Id} Error={args.Exception}");
+            Crashes.ShouldAwaitUserConfirmation = ConfirmationHandler;
+            Crashes.ShouldProcessErrorReport = ShouldProcess;
+
+            // Event handlers
+            Crashes.SendingErrorReport += SendingErrorReportHandler;
+            Crashes.SentErrorReport += SentErrorReportHandler;
+            Crashes.FailedToSendErrorReport += FailedToSendErrorReportHandler;
             AppCenter.Start("42f4a839-c54c-44da-8072-a2f2a61751b2", typeof(Analytics), typeof(Crashes));
+            Crashes.HasCrashedInLastSessionAsync().ContinueWith(hasCrashed =>
+            {
+                AppCenterLog.Info(LogTag, "Crashes.HasCrashedInLastSession=" + hasCrashed.Result);
+            });
+            Crashes.GetLastSessionCrashReportAsync().ContinueWith(task =>
+            {
+                AppCenterLog.Info(LogTag, "Crashes.LastSessionCrashReport.Exception=" + task.Result?.Exception);
+            });
+        }
+
+        bool ShouldProcess(ErrorReport report)
+        {
+            AppCenterLog.Info(LogTag, "Determining whether to process error report");
+            return true;
+        }
+
+        bool ConfirmationHandler()
+        {
+            Current.Dispatcher.Invoke(new Action(() =>
+            {
+                var dialog = new UserConfirmationDialog();
+                if (dialog.ShowDialog() == true)
+                {
+                    Crashes.NotifyUserConfirmation(dialog.ClickResult);
+                }
+            }));
+            return true;
+        }
+
+        static void SendingErrorReportHandler(object sender, SendingErrorReportEventArgs e)
+        {
+            AppCenterLog.Info(LogTag, "Sending error report");
+            var report = e.Report;
+
+            // Test some values
+            if (report.Exception != null)
+            {
+                AppCenterLog.Info(LogTag, report.Exception.ToString());
+            }
+        }
+
+        static void SentErrorReportHandler(object sender, SentErrorReportEventArgs e)
+        {
+            AppCenterLog.Info(LogTag, "Sent error report");
+            var report = e.Report;
+
+            // Test some values
+            if (report.Exception != null)
+            {
+                AppCenterLog.Info(LogTag, report.Exception.ToString());
+            }
+            else
+            {
+                AppCenterLog.Info(LogTag, "No system exception was found");
+            }
+        }
+
+        static void FailedToSendErrorReportHandler(object sender, FailedToSendErrorReportEventArgs e)
+        {
+            AppCenterLog.Info(LogTag, "Failed to send error report");
+            var report = e.Report;
+
+            // Test some values
+            if (report.Exception != null)
+            {
+                AppCenterLog.Info(LogTag, report.Exception.ToString());
+            }
+            if (e.Exception != null)
+            {
+                AppCenterLog.Info(LogTag, "There is an exception associated with the failure");
+            }
         }
     }
 }
