cd3404614ec4b5cdf7165055b198569ad620f19f
Alexander Chocron <alchocro@microsoft.com>
2019-07-10 16:32:16 -0700

    Add functional test for trackerror

--- Tests/Microsoft.AppCenter.Crashes.Test.Windows/CrashesTest.cs
+++ Tests/Microsoft.AppCenter.Crashes.Test.Windows/CrashesTest.cs
@@ -4,13 +4,20 @@
 using Microsoft.AppCenter.Channel;
 using Microsoft.AppCenter.Crashes.Ingestion.Models;
 using Microsoft.AppCenter.Crashes.Utils;
+using Microsoft.AppCenter.Ingestion.Http;
 using Microsoft.AppCenter.Ingestion.Models;
+using Microsoft.AppCenter.Ingestion.Models.Serialization;
+using Microsoft.AppCenter.Storage;
 using Microsoft.AppCenter.Utils;
 using Microsoft.AppCenter.Utils.Files;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
+using Newtonsoft.Json;
 using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
 
 namespace Microsoft.AppCenter.Crashes.Test.Windows
 {
@@ -808,6 +815,39 @@ namespace Microsoft.AppCenter.Crashes.Test.Windows
             // The alwaysSend value should never set because we are disabled
             var alwaysSendValue = AppCenter.Instance.ApplicationSettings.GetValue(Crashes.PrefKeyAlwaysSend, false);
             Assert.IsFalse(alwaysSendValue);
+        }
+
+        [TestMethod]
+        public void TestTrackErrorValidParameters()
+        {
+            var mockHttpNetworkAdapter = Mock.Of<IHttpNetworkAdapter>();
+            var storage = new Storage.Storage(new StorageAdapter("test.db"));
+            var ingestion = new IngestionHttp(mockHttpNetworkAdapter);
+            var channelGroup = new ChannelGroup(ingestion, storage, "app secret");
+            Crashes.Instance.OnChannelGroupReady(channelGroup, "app secret");
+            var semaphore = new SemaphoreSlim(0);
+            HandledErrorLog actualLog = null;
+            Mock.Get(mockHttpNetworkAdapter).Setup(adapter => adapter.SendAsync(
+                It.IsAny<string>(),
+                It.IsAny<string>(),
+                It.IsAny<IDictionary<string, string>>(),
+                It.IsAny<string>(),
+                It.IsAny<CancellationToken>()))
+                .Callback((string uri, string method, IDictionary<string, string> headers, string content, CancellationToken cancellation) =>
+                {
+                    actualLog = JsonConvert.DeserializeObject<LogContainer>(content, LogSerializer.SerializationSettings).Logs.Single() as HandledErrorLog;
+                    semaphore.Release();
+                });
+
+            var exception = new System.Exception("Something went wrong.");
+            var properties = new Dictionary<string, string> { { "k1", "v1" }, { "p2", "v2"} };
+            Crashes.TrackError(exception, properties);
+
+            // Wait until the http layer sends the log.
+            semaphore.Wait();
+            Assert.IsNotNull(actualLog);
+            Assert.AreEqual(exception.Message, actualLog.Exception.Message);
+            CollectionAssert.AreEquivalent(properties, actualLog.Properties as Dictionary<string, string>);
         }
 
         /// <summary>
