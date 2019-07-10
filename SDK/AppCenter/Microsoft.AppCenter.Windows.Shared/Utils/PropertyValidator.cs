ce5f097c44ff866d5ae46100b6657ae09c83c922
Guillaume Perrot <guperrot@microsoft.com>
2019-07-09 17:02:05 -0700

    Implement handled errors for windows

---
+++ SDK/AppCenter/Microsoft.AppCenter.Windows.Shared/Utils/PropertyValidator.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.Generic;
+
+namespace Microsoft.AppCenter.Windows.Shared.Utils
+{
+    public static class PropertyValidator
+    {
+        private const int MaxEventProperties = 20;
+        private const int MaxEventPropertyKeyLength = 125;
+        private const int MaxEventPropertyValueLength = 125;
+
+        /// <summary>
+        /// Validates properties.
+        /// </summary>
+        /// <param name="properties">Properties collection to validate.</param>
+        /// <param name="logName">Log name.</param>
+        /// <returns>Valid properties collection with maximum size of 5</returns>
+        public static IDictionary<string, string> ValidateProperties(IDictionary<string, string> properties, string logName)
+        {
+            if (properties == null)
+            {
+                return null;
+            }
+            var result = new Dictionary<string, string>();
+            foreach (var property in properties)
+            {
+                if (result.Count >= MaxEventProperties)
+                {
+                    AppCenterLog.Warn(AppCenterLog.LogTag,
+                        $"{logName} : properties cannot contain more than {MaxEventProperties} items. Skipping other properties.");
+                    break;
+                }
+
+                // Skip empty property.
+                var key = property.Key;
+                var value = property.Value;
+                if (string.IsNullOrEmpty(key))
+                {
+                    AppCenterLog.Warn(AppCenterLog.LogTag,
+                        $"{logName} : a property key cannot be null or empty. Property will be skipped.");
+                    break;
+                }
+                if (value == null)
+                {
+                    AppCenterLog.Warn(AppCenterLog.LogTag,
+                        $"{logName} : property '{key}' : property value cannot be null. Property will be skipped.");
+                    break;
+                }
+
+                // Truncate exceeded property.
+                if (key.Length > MaxEventPropertyKeyLength)
+                {
+                    AppCenterLog.Warn(AppCenterLog.LogTag,
+                        $"{logName} : property '{key}' : property key length cannot be longer than {MaxEventPropertyKeyLength} characters. Property key will be truncated.");
+                    key = key.Substring(0, MaxEventPropertyKeyLength);
+                }
+                if (value.Length > MaxEventPropertyValueLength)
+                {
+                    AppCenterLog.Warn(AppCenterLog.LogTag,
+                        $"{logName} : property '{key}' : property value length cannot be longer than {MaxEventPropertyValueLength} characters. Property value will be truncated.");
+                    value = value.Substring(0, MaxEventPropertyValueLength);
+                }
+                result.Add(key, value);
+            }
+            return result;
+        }
+    }
+}
--- SDK/AppCenterCrashes/Microsoft.AppCenter.Crashes.Windows.Shared/Utils/ErrorLogHelper.cs
+++ SDK/AppCenterCrashes/Microsoft.AppCenter.Crashes.Windows.Shared/Utils/ErrorLogHelper.cs
@@ -320,7 +320,7 @@ namespace Microsoft.AppCenter.Crashes.Utils
             AppCenterLog.Debug(Crashes.LogTag, "Deleted crashes local files.");
         }
 
-        private static ModelException CreateModelException(System.Exception exception)
+        internal static ModelException CreateModelException(System.Exception exception)
         {
             var modelException = new ModelException
             {
