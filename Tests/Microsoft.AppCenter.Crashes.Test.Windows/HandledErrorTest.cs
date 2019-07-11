d8084e454e5b2d851a45389a49a0a48f018ec8bf
Guillaume Perrot <guperrot@microsoft.com>
2019-07-10 19:11:27 -0700

    Add tests around disabling and handled errors

--- Tests/Microsoft.AppCenter.Crashes.Test.Windows/HandledErrorTest.cs
+++ Tests/Microsoft.AppCenter.Crashes.Test.Windows/HandledErrorTest.cs
@@ -187,5 +187,51 @@ namespace Microsoft.AppCenter.Crashes.Test.Windows
             Assert.AreEqual("testKeyTooLong", actualLog.Properties[new string('k', 125)]);
             Assert.AreEqual(new string('v', 125), actualLog.Properties["testValueTooLong"]);
         }
+
+        [TestMethod]
+        public void TrackErrorWhenDisabled()
+        {
+            var logCount = 0;
+            var mockHttpNetworkAdapter = StartCrashes();
+            var semaphore = new SemaphoreSlim(0);
+            HandledErrorLog actualLog = null;
+            Mock.Get(mockHttpNetworkAdapter).Setup(adapter => adapter.SendAsync(
+                    It.IsAny<string>(),
+                    It.IsAny<string>(),
+                    It.IsAny<IDictionary<string, string>>(),
+                    It.IsAny<string>(),
+                    It.IsAny<CancellationToken>()))
+                .Callback((string uri, string method, IDictionary<string, string> headers, string content, CancellationToken cancellation) =>
+                {
+                    actualLog = JsonConvert.DeserializeObject<LogContainer>(content, LogSerializer.SerializationSettings).Logs.Single() as HandledErrorLog;
+                    ++logCount;
+                    semaphore.Release();
+                });
+
+            // If we disable the SDK.
+            Crashes.SetEnabledAsync(false).Wait();
+
+            // When we track an error.
+            var exception = new System.Exception("Something went wrong.");
+            var properties = new Dictionary<string, string> { { "k1", "v1" }, { "p2", "v2" } };
+            Crashes.TrackError(exception, properties);
+
+            // Then it's not sent.
+            semaphore.Wait(1000);
+            Assert.IsNull(actualLog);
+
+            // If we re-enable.
+            Crashes.SetEnabledAsync(true).Wait();
+
+            // When we track an error.
+            Crashes.TrackError(exception, properties);
+
+            // Then it's sent.
+            semaphore.Wait(1000);
+            Assert.IsNotNull(actualLog);
+            Assert.AreEqual(1, logCount);
+            Assert.AreEqual(exception.Message, actualLog.Exception.Message);
+            CollectionAssert.AreEquivalent(properties, actualLog.Properties as Dictionary<string, string>);
+        }
     }
 }
