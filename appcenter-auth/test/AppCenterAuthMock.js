419fbbb5d58b10ced90f70f290cb4e7b0c507ef4
Jacky Lui <t-jalu@microsoft.com>
2019-07-12 10:47:15 -0700

    formatting

--- appcenter-auth/test/AppCenterAuthMock.js
+++ appcenter-auth/test/AppCenterAuthMock.js
@@ -2,8 +2,8 @@
 // Licensed under the MIT License.
 
 jest.mock('appcenter-auth', () => ({
-    isEnabled: jest.fn(),
-    setEnabled: jest.fn(),
-    signIn: jest.fn(),
-    signOut: jest.fn()
+  isEnabled: jest.fn(),
+  setEnabled: jest.fn(),
+  signIn: jest.fn(),
+  signOut: jest.fn()
 }));
