14566f9e66a47db683cc2f47b91fc426ac16d453
Di Hei <dihei@microsoft.com>
2019-07-25 16:29:54 -0700

    Delete BrownfieldTestApp
    We created BrownfieldTestApp a while ago as an experiment to verify App Center SDK integration of "brownfield" apps using CocoaPods. We never use it again. As Cocoapods integration is natively supported in RN v0.60, there is no need to keep it around.

--- BrownfieldTestApp/ios/Pods/Headers/Public/AppCenter/AppCenterCrashes/MSErrorAttachmentLog.h
+++
@@ -1,4 +0,0 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
-
-../../../../AppCenter/AppCenter-SDK-Apple/iOS/AppCenterCrashes.framework/Headers/MSErrorAttachmentLog.h
