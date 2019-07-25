d5a216c89faaeec63bcff3437a43c4121267bf80
Jae Lim <jaelim@microsoft.com>
2019-07-25 13:17:20 -0700

    Add AssemblyInfo.cs back to the place for classes to be visible for testing

---
+++ SDK/AppCenter/Microsoft.AppCenter.WindowsDesktop/Properties/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("Microsoft.AppCenter.Test.WindowsDesktop")]
+[assembly: InternalsVisibleTo("Microsoft.AppCenter.Crashes.Test.Windows")]
