6c0db517a02081181218805b2013d4b7892bd5c3
Guillaume Perrot <guperrot@microsoft.com>
2019-07-26 20:09:03 -0700

    Fix UWP test

--- Tests/Microsoft.AppCenter.Test.UWP/Utils/ApplicationSettingsTest.cs
+++ Tests/Microsoft.AppCenter.Test.UWP/Utils/ApplicationSettingsTest.cs
@@ -41,9 +41,8 @@ namespace Microsoft.AppCenter.Test.UWP.Utils
             const string key = "test";
             Assert.IsFalse(settings.ContainsKey(key));
             Assert.AreEqual(42, settings.GetValue(key, 42));
-            Assert.IsTrue(settings.ContainsKey(key));
-            Assert.AreEqual(42, settings.GetValue<int>(key));
-            Assert.AreEqual(42, settings.GetValue(key, 0));
+            Assert.AreEqual(0, settings.GetValue<int>(key));
+            Assert.AreEqual(0, settings.GetValue(key, 0));
         }
 
         /// <summary>
