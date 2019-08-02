d89614e9aa283226b037fe87dd3f2abdd9336de5
Jacky Lui <t-jalu@microsoft.com>
2019-08-02 13:47:12 -0700

    Added jsdocs to Auth

--- appcenter-auth/Auth.js
+++ appcenter-auth/Auth.js
@@ -6,18 +6,45 @@ const ReactNative = require('react-native');
 const { AppCenterReactNativeAuth } = ReactNative.NativeModules;
 
 const Auth = {
-    isEnabled() {
-        return AppCenterReactNativeAuth.isEnabled();
-    },
-    setEnabled(enabled) {
-        return AppCenterReactNativeAuth.setEnabled(enabled);
-    },
-    signIn() {
-        return AppCenterReactNativeAuth.signIn();
-    },
-    signOut() {
-        AppCenterReactNativeAuth.signOut();
-    }
+    isEnabled,
+    setEnabled,
+    signIn,
+    signOut
 };
+
+/**
+ * Check whether Auth service is enabled or not.
+ *
+ * @return Future asynchronous operation with result being true if enabled, false otherwise.
+ */
+function isEnabled() {
+    return AppCenterReactNativeAuth.isEnabled();
+}
+
+/**
+ * Enable or disable Auth service.
+ *
+ * @param {boolean} enabled - true to enable, false to disable.
+ * @return Future asynchronous operation with null result to monitor when the operation completes.
+ */
+function setEnabled(enabled) {
+    return AppCenterReactNativeAuth.setEnabled(enabled);
+}
+
+/**
+ * Sign in to get user information.
+ *
+ * @return Future asynchronous operation with the result of the sign-in operation.
+ */
+function signIn() {
+    return AppCenterReactNativeAuth.signIn();
+}
+
+/**
+ * Sign out user and invalidate a user's token.
+ */
+function signOut() {
+    AppCenterReactNativeAuth.signOut();
+}
 
 module.exports = Auth;
