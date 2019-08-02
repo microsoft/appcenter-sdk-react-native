b01ae8d22470195934cbdf0e3f7e93aea28b1bc0
Michael Smith <mismith@microsoft.com>
2019-08-02 15:15:17 -0700

    add redirect to build repository

--- DemoApp/appcenter-post-clone.sh
+++ DemoApp/appcenter-post-clone.sh
@@ -8,4 +8,5 @@ cd ~/.cocoapods/repos
 git clone https://$USER_ACCOUNT:$ACCESS_TOKEN@msmobilecenter.visualstudio.com/SDK/_git/$REPO_NAME
 # There is a bug when trying to do a pod repo add from build agent
 # pod repo add $REPO_NAME https://$USER_ACCOUNT:$ACCESS_TOKEN@msmobilecenter.visualstudio.com/SDK/_git/$REPO_NAME --verbose
+cd $BUILD_REPOSITORY_LOCALPATH
 (cd ios && pod install)
