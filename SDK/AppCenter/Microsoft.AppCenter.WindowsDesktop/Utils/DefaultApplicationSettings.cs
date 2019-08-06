06f2727af9b8991c4e0bfaa4443aa14801cdb257
Guillaume Perrot <guperrot@microsoft.com>
2019-08-05 17:28:34 -0700

    Use user specific directory for SDK AppCenter.config file

--- SDK/AppCenter/Microsoft.AppCenter.WindowsDesktop/Utils/DefaultApplicationSettings.cs
+++ SDK/AppCenter/Microsoft.AppCenter.WindowsDesktop/Utils/DefaultApplicationSettings.cs
@@ -8,11 +8,13 @@ using System.Configuration;
 using System.IO;
 using System.Linq;
 using System.Reflection;
+using System.Text.RegularExpressions;
 
 namespace Microsoft.AppCenter.Utils
 {
     public class DefaultApplicationSettings : IApplicationSettings
     {
+        private const string FileName = "AppCenter.config";
         private static readonly object configLock = new object();
         private static IDictionary<string, string> current;
 
@@ -90,8 +92,45 @@ namespace Microsoft.AppCenter.Utils
 
         private static Configuration OpenConfiguration()
         {
-            var location = Assembly.GetExecutingAssembly().Location;
-            FilePath = Path.Combine(Path.GetDirectoryName(location), "AppCenter.config");
+            // Get new config path.
+            var userConfig = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.PerUserRoamingAndLocal);
+            var userConfigPath = Path.GetDirectoryName(userConfig.FilePath);
+
+            // Don't have AppCenter.config be reset on each app assembly version, use parent directory.
+            var parentDirectory = Path.GetDirectoryName(userConfigPath);
+            if (Directory.Exists(parentDirectory))
+            {
+                userConfigPath = parentDirectory;
+            }
+            FilePath = Path.Combine(userConfigPath, FileName);
+
+            // If old path exists, migrate.
+            try
+            {
+                // Get old config path.
+                var oldLocation = Assembly.GetExecutingAssembly().Location;
+                var oldPath = Path.Combine(Path.GetDirectoryName(oldLocation), FileName);
+                if (File.Exists(oldPath))
+                {
+                    // Delete old file if a new one already exists.
+                    if (File.Exists(FilePath))
+                    {
+                        File.Delete(oldPath);
+                    }
+
+                    // Or migrate by moving if no new file yet.
+                    else
+                    {
+                        File.Move(oldPath, FilePath);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                AppCenterLog.Warn(AppCenterLog.LogTag, "Could not check/migrate old config file", e);
+            }
+
+            // Open the configuration (with the new file path).
             var executionFileMap = new ExeConfigurationFileMap { ExeConfigFilename = FilePath };
             return ConfigurationManager.OpenMappedExeConfiguration(executionFileMap, ConfigurationUserLevel.None);
         }
