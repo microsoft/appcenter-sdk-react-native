d7e5a53ed1257e519d186defb7406f2388c0dbed
Guillaume Perrot <guperrot@microsoft.com>
2019-08-07 19:09:59 -0700

    Optimize lifecycle events for .NET Framework

--- SDK/AppCenter/Microsoft.AppCenter.WindowsDesktop/Utils/ApplicationLifecycleHelper.cs
+++ SDK/AppCenter/Microsoft.AppCenter.WindowsDesktop/Utils/ApplicationLifecycleHelper.cs
@@ -31,7 +31,8 @@ namespace Microsoft.AppCenter.Utils
         [DllImport("user32.dll")]
         private static extern bool UnhookWinEvent(IntPtr hWinEventHook);
 
-        private const uint EVENT_OBJECT_LOCATIONCHANGE = 0x800B;
+        private const uint EVENT_SYSTEM_MINIMIZESTART = 0x0016;
+        private const uint EVENT_SYSTEM_MINIMIZEEND = 0x0017;
         private const uint WINEVENT_OUTOFCONTEXT = 0;
 
         // Need to ensure delegate is not collected while we're using it,
@@ -93,7 +94,7 @@ namespace Microsoft.AppCenter.Utils
             // We need Windows thread ID, not managed
             var threadId = AppDomain.GetCurrentThreadId();
 #pragma warning restore CS0618 // Type or member is obsolete
-            var hook = SetWinEventHook(EVENT_OBJECT_LOCATIONCHANGE, EVENT_OBJECT_LOCATIONCHANGE, IntPtr.Zero, hookDelegate, 0, (uint)threadId, WINEVENT_OUTOFCONTEXT);
+            var hook = SetWinEventHook(EVENT_SYSTEM_MINIMIZESTART, EVENT_SYSTEM_MINIMIZEEND, IntPtr.Zero, hookDelegate, 0, (uint)threadId, WINEVENT_OUTOFCONTEXT);
             Application.ApplicationExit += delegate { UnhookWinEvent(hook); };
         }
 
