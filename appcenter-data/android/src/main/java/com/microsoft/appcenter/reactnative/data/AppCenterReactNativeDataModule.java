55834336bbf8ca0f76cc3f08838ab97954e56cb0
minyi <minyi@microsoft.com>
2019-08-26 16:19:06 -0700

    remove empty line

--- appcenter-data/android/src/main/java/com/microsoft/appcenter/reactnative/data/AppCenterReactNativeDataModule.java
+++ appcenter-data/android/src/main/java/com/microsoft/appcenter/reactnative/data/AppCenterReactNativeDataModule.java
@@ -132,7 +132,6 @@ public class AppCenterReactNativeDataModule extends BaseJavaModule {
             }
         };
         Data.setRemoteOperationListener(remoteOperationlistener);
-
     }
 
     @ReactMethod
