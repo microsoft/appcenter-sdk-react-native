7258bb7d1e9d3a127dc150e084b3d6eae1d15bf7
minyi <minyi@microsoft.com>
2019-08-28 17:26:22 -0700

    add implementation of document list

---
+++ TestApp/app/components/DataDocumentListView.js
@@ -0,0 +1,62 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+import React, { Component } from 'react';
+import { View, Text, Image, FlatList, TouchableOpacity, StyleSheet } from 'react-native';
+
+import RemoveIcon from '../assets/remove.png';
+
+export class DataDocumentListView extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <FlatList
+          style={{ marginTop: 10 }}
+          data={this.props.items}
+          renderItem={({ item }) => (
+            <View style={styles.documentsContainer}>
+              <TouchableOpacity style={styles.documentText}>
+                <Text>{item.id}</Text>
+              </TouchableOpacity>
+              <Divider />
+              <TouchableOpacity onPress={() => this.props.onDocumentRemoved(item.id)}>
+                <Image style={styles.removeIcon} source={RemoveIcon} />
+              </TouchableOpacity>
+            </View>)
+          }
+          keyExtractor={(item, index) => index.toString()}
+        />
+      </View>
+    );
+  }
+}
+
+const Divider = () => <View style={{ width: 4 }} />;
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'white',
+    padding: 10,
+    flexDirection: 'column'
+  },
+  propertiesList: {
+    marginTop: 10
+  },
+  documentsContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
+  documentText: {
+    flex: 1,
+    padding: 5,
+    height: 35,
+    fontSize: 17,
+    borderColor: 'black',
+    borderWidth: 0.3
+  },
+  removeIcon: {
+    height: 26,
+    width: 26
+  }
+});
--- TestApp/app/screens/DataScreen.js
+++ TestApp/app/screens/DataScreen.js
@@ -4,6 +4,7 @@
 import React, { Component } from 'react';
 import { Image, View, Text, Switch, SectionList } from 'react-native';
 import ModalSelector from 'react-native-modal-selector';
+import { DataDocumentListView } from '../components/DataDocumentListView';
 
 import Data from 'appcenter-data';
 
@@ -38,7 +39,8 @@ export default class DataScreen extends Component {
   // Screen's state.
   state = {
     dataEnabled: false,
-    partition: Data.DefaultPartitions.APP_DOCUMENTS
+    partition: Data.DefaultPartitions.APP_DOCUMENTS,
+    documents: []
   }
 
   async componentDidMount() {
@@ -49,11 +51,33 @@ export default class DataScreen extends Component {
     this.props.navigation.setParams({
       refreshScreen: this.refreshToggle.bind(this)
     });
+    this.setState({documents: await this.listDocuments(this.state.partition)})
   }
 
   async refreshToggle() {
     const dataEnabled = await Data.isEnabled();
     this.setState({ dataEnabled });
+  }
+
+  async listDocuments(partition) {
+    let documents = [];
+    hasNextPage = false;
+    try {
+      let page = await Data.list(partition);
+      page.currentPage.items.forEach(item => {
+        documents.push(item);
+      });
+      while (await page.hasNextPage()) {
+        page = await page.getNextPage();
+        page.currentPage.items.forEach(item => {
+          documents.push(item);
+        });
+      }
+      return documents;
+    } catch (err) {
+      console.log(err)
+    }
+    return documents;
   }
 
   render() {
@@ -73,6 +97,12 @@ export default class DataScreen extends Component {
       />
     );
 
+    const documentsViewer = ({ item: { onDocumentRemoved } }) => (
+      <DataDocumentListView 
+        items={this.state.documents} 
+        onDocumentRemoved = {onDocumentRemoved}
+      />
+    );
     return (
       <View style={SharedStyles.container}>
         <SectionList
@@ -101,11 +131,31 @@ export default class DataScreen extends Component {
               data: [
                 {
                   onChange: async (option) => {
-                    this.setState({ partition: option.key });
+                    let documents = await this.listDocuments(option.key)
+                    this.setState({ partition: option.key, documents: documents });
                   }
                 }
               ],
               renderItem: partitionPicker
+            },
+            {
+              title: 'Documents',
+              data: [
+                {
+                  onDocumentRemoved: async (documentId) => {
+                    console.log(documentId);
+                    await Data.remove(documentId, this.state.partition);
+                    let newDocuments = [];
+                    this.state.documents.forEach(document=>{
+                      if (document.id !== documentId) {
+                        newDocuments.push(document);
+                      }
+                    });
+                    this.setState({ documents: newDocuments});
+                  }
+                }
+              ],
+              renderItem: documentsViewer
             }
           ]}
         />
