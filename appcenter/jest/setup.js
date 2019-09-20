1f4fa48a4d403cd3a8a60b7da4f41ace547089ec
Di Hei <dihei@microsoft.com>
2019-09-19 18:16:33 -0700

    Add jest mock

---
+++ appcenter/jest/setup.js
@@ -0,0 +1,16 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+jest.mock('NativeModules', () => ({
+  AppCenterReactNative: {
+    getLogLevel: jest.fn(),
+    setLogLevel: jest.fn(),
+    getInstallId: jest.fn(),
+    setUserId: jest.fn(),
+    isEnabled: jest.fn(),
+    setEnabled: jest.fn(),
+    setCustomProperties: jest.fn(),
+    getSdkVersion: jest.fn(),
+    setAuthTokenListener: jest.fn()
+  }
+}));
--- appcenter/test/AppCenterMock.js
+++ appcenter/test/AppCenterMock.js
@@ -2,12 +2,13 @@
 // Licensed under the MIT License.
 
 jest.mock('appcenter', () => ({
-    getLogLevel: jest.fn(),
-    setLogLevel: jest.fn(),
-    getInstallId: jest.fn(),
-    setUserId: jest.fn(),
-    isEnabled: jest.fn(),
-    setEnabled: jest.fn(),
-    setCustomProperties: jest.fn(),
-    getSdkVersion: jest.fn()
+  getLogLevel: jest.fn(),
+  setLogLevel: jest.fn(),
+  getInstallId: jest.fn(),
+  setUserId: jest.fn(),
+  isEnabled: jest.fn(),
+  setEnabled: jest.fn(),
+  setCustomProperties: jest.fn(),
+  getSdkVersion: jest.fn(),
+  setAuthTokenListener: jest.fn(),
 }));
