2192409be936ec3df0aa136e82cd0ebe9cf3fb41
Richa Deshmukh <rideshm@microsoft.com>
2019-09-30 15:48:21 -0700

    sdk + uwp app changes for user id support

--- SDK/AppCenter/Microsoft.AppCenter.WindowsDesktop/AppCenterPart.cs
+++ SDK/AppCenter/Microsoft.AppCenter.WindowsDesktop/AppCenterPart.cs
@@ -6,10 +6,5 @@ namespace Microsoft.AppCenter
     public partial class AppCenter
     {
         private const string PlatformIdentifier = "windowsdesktop";
-
-        static void PlatformSetUserId(string userId)
-        {
-            AppCenterLog.Error(AppCenterLog.LogTag, "AppCenter.SetUserId is not supported on Windows.");
-        }
     }
 }
