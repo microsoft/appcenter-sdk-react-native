4ce21ca924a930ec29f2053b4ce7e3e3614b325c
Michael Carlson <micarls@microsoft.com>
2019-10-01 12:27:52 -0700

    Fix build break

--- Apps/Contoso.Forms.Demo/Contoso.Forms.Demo/App.xaml.cs
+++ Apps/Contoso.Forms.Demo/Contoso.Forms.Demo/App.xaml.cs
@@ -13,6 +13,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using XamarinDevice = Xamarin.Forms.Device;
 
 namespace Contoso.Forms.Demo
 {
