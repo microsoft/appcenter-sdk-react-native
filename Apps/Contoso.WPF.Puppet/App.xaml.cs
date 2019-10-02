165b05c6df18d2832cdc2ebf26f54b93d03e7526
Guillaume Perrot <guperrot@microsoft.com>
2019-10-02 15:44:26 -0700

    Fix userId UI

--- Apps/Contoso.WPF.Puppet/App.xaml.cs
+++ Apps/Contoso.WPF.Puppet/App.xaml.cs
@@ -70,9 +70,10 @@ namespace Contoso.WPF.Puppet
             // Start AppCenter.
             AppCenter.Start("d967daf9-28ed-4899-84e8-17a00c064987", typeof(Analytics), typeof(Crashes));
 
-            if (Settings.Default.UserId is string id)
+            var userId = Settings.Default.UserId;
+            if (!string.IsNullOrEmpty(userId))
             {
-                AppCenter.SetUserId(id);
+                AppCenter.SetUserId(userId);
             }
 
             Crashes.HasCrashedInLastSessionAsync().ContinueWith(hasCrashed =>
