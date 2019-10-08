49064b4e08d14867c0fa84b3bfcc8ac4e6585f44
Alexander Chocron <alchocro@microsoft.com>
2019-10-08 09:33:33 -0700

    Add handled error log update

--- SDK/AppCenterCrashes/Microsoft.AppCenter.Crashes.Windows.Shared/Ingestion/Models/HandledErrorLog.cs
+++ SDK/AppCenterCrashes/Microsoft.AppCenter.Crashes.Windows.Shared/Ingestion/Models/HandledErrorLog.cs
@@ -1,20 +1,28 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+
+using System.Collections.Generic;
+using Microsoft.AppCenter.Ingestion.Models;
+using Newtonsoft.Json;
 
 namespace Microsoft.AppCenter.Crashes.Ingestion.Models
 {
-    using Microsoft.AppCenter.Ingestion.Models;
-    using Newtonsoft.Json;
-    using System.Collections.Generic;
 
     /// <summary>
     /// Handled Error log for managed platforms (such as Xamarin, Unity,
     /// Android Dalvik/ART)
     /// </summary>
-    [Newtonsoft.Json.JsonObject(JsonIdentifier)]
+    [JsonObject(JsonIdentifier)]
     public partial class HandledErrorLog : LogWithProperties
     {
         internal const string JsonIdentifier = "handledError";
+
+        /// <summary>
+        /// Initializes a new instance of the HandledErrorLog class.
+        /// </summary>
+        public HandledErrorLog()
+        {
+            CustomInit();
+        }
 
         /// <summary>
         /// Initializes a new instance of the HandledErrorLog class.
@@ -37,16 +45,18 @@ namespace Microsoft.AppCenter.Crashes.Ingestion.Models
         /// </param>
         /// <param name="id">Unique identifier for this Error.
         /// </param>
-        public HandledErrorLog(Device device, Exception exception, System.DateTime? timestamp = default(System.DateTime?), System.Guid? sid = default(System.Guid?), string userId = default(string), IDictionary<string, string> properties = default(IDictionary<string, string>), System.Guid? id = default(System.Guid?))
+        public HandledErrorLog(Microsoft.AppCenter.Ingestion.Models.Device device, Exception exception, System.DateTime? timestamp = default(System.DateTime?), System.Guid? sid = default(System.Guid?), string userId = default(string), IDictionary<string, string> properties = default(IDictionary<string, string>), System.Guid? id = default(System.Guid?), IList<Binary> binaries = default(IList<Binary>))
             : base(device, timestamp, sid, userId, properties)
         {
             Id = id;
+            Binaries = binaries;
             Exception = exception;
             CustomInit();
         }
 
         /// <summary>
         /// An initialization method that performs custom operations like setting defaults
+        /// </summary>
         partial void CustomInit();
 
         /// <summary>
@@ -55,6 +65,11 @@ namespace Microsoft.AppCenter.Crashes.Ingestion.Models
         /// </summary>
         [JsonProperty(PropertyName = "id")]
         public System.Guid? Id { get; set; }
+
+        /// <summary>
+        /// </summary>
+        [JsonProperty(PropertyName = "binaries")]
+        public IList<Binary> Binaries { get; set; }
 
         /// <summary>
         /// </summary>
@@ -73,6 +88,16 @@ namespace Microsoft.AppCenter.Crashes.Ingestion.Models
             if (Exception == null)
             {
                 throw new ValidationException(ValidationException.Rule.CannotBeNull, "Exception");
+            }
+            if (Binaries != null)
+            {
+                foreach (var element in Binaries)
+                {
+                    if (element != null)
+                    {
+                        element.Validate();
+                    }
+                }
             }
             if (Exception != null)
             {
