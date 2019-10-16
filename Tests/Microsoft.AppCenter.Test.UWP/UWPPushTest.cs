844e09e24b87b457b155022a766a01decc538fe7
dorothy <daxsun@microsoft.com>
2019-10-16 14:55:00 -0700

    unit test name change

--- Tests/Microsoft.AppCenter.Test.UWP/UWPPushTest.cs
+++ Tests/Microsoft.AppCenter.Test.UWP/UWPPushTest.cs
@@ -188,7 +188,7 @@ namespace Microsoft.AppCenter.Test.UWP
         /// Verify OnUserIdUpdated works with userId changes. 
         /// </summary>
         [TestMethod]
-        public void OnUserIdUpdated()
+        public void VerifyEnqueueAsyncIsCalledOnceOnUserIdUpdatedInvocation()
         {
             Push.Push.Instance.OnChannelGroupReady(_mockChannelGroup.Object, string.Empty);
             Push.Push.Instance.LatestPushToken = "token";
