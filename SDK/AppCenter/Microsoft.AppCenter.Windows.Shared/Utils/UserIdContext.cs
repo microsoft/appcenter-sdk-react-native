5e73fd72504575ce44803b847abfb673f41fd923
dorothy <daxsun@microsoft.com>
2019-10-16 17:12:12 -0700

    fix failing test

--- SDK/AppCenter/Microsoft.AppCenter.Windows.Shared/Utils/UserIdContext.cs
+++ SDK/AppCenter/Microsoft.AppCenter.Windows.Shared/Utils/UserIdContext.cs
@@ -70,7 +70,7 @@ namespace Microsoft.AppCenter.Windows.Shared.Utils
             {
                 lock (UserIdContextLock)
                 {
-                    if (!_userId.Equals(value))
+                    if (_userId == null || !_userId.Equals(value))
                     {
                         _userId = value;
                         UserIdUpdated?.Invoke(this, new UserIdUpdatedEventArgs { userId = value });
