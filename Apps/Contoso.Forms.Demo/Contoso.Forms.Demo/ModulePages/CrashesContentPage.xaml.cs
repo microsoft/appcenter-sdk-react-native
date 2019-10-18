2b91305b264b2a6d9e09deb8f624635412d20b9e
Anastasia Kubova <anastasia.kubova@akvelon.com>
2019-10-18 17:00:50 +0300

    added attachments for TrackError in demo

--- Apps/Contoso.Forms.Demo/Contoso.Forms.Demo/ModulePages/CrashesContentPage.xaml.cs
+++ Apps/Contoso.Forms.Demo/Contoso.Forms.Demo/ModulePages/CrashesContentPage.xaml.cs
@@ -242,7 +242,9 @@ namespace Contoso.Forms.Demo
             }
             Properties.Clear();
             RefreshPropCount();
-            Crashes.TrackError(e, properties);
+            // TODO: uncomment this when API will be added.
+            Crashes.TrackError(e, properties /*, App.GetErrorAttachments() */);
+
         }
 
         void ClearCrashUserConfirmation(object sender, EventArgs e)
