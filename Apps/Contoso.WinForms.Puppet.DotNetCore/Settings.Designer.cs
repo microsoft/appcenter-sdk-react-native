237c8e6ea6571e7d6c388fbc3acc4a3c7469c9de
Guillaume Perrot <guperrot@microsoft.com>
2019-10-23 15:29:10 -0700

    Fix settings in WinForms .NET Core apps

--- Apps/Contoso.WinForms.Puppet.DotNetCore/Settings.Designer.cs
+++ Apps/Contoso.WinForms.Puppet.DotNetCore/Settings.Designer.cs
@@ -8,51 +8,41 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Contoso.WinForms.Puppet.DotNetCore
-{
-
-
+namespace Contoso.WinForms.Puppet.DotNetCore {
+    
+    
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.3.0.0")]
-    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase
-    {
-
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
+        
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
-
-        public static Settings Default
-        {
-            get
-            {
+        
+        public static Settings Default {
+            get {
                 return defaultInstance;
             }
         }
-
+        
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.DefaultSettingValueAttribute("")]
-        public string FileErrorAttachments
-        {
-            get
-            {
+        public string FileErrorAttachments {
+            get {
                 return ((string)(this["FileErrorAttachments"]));
             }
-            set
-            {
+            set {
                 this["FileErrorAttachments"] = value;
             }
         }
-
+        
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.DefaultSettingValueAttribute("")]
-        public string TextErrorAttachments
-        {
-            get
-            {
+        public string TextErrorAttachments {
+            get {
                 return ((string)(this["TextErrorAttachments"]));
             }
-            set
-            {
+            set {
                 this["TextErrorAttachments"] = value;
             }
         }
