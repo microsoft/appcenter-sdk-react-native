7e086a837b25032a8f68850d4728e99280885fb4
Cloud Wu <v-ruiywu@microsoft.com>
2019-11-14 16:27:09 +0800

    Remove LogLevel UI

--- Apps/Contoso.Forms.Demo/Contoso.Forms.Demo/Constants.cs
+++ Apps/Contoso.Forms.Demo/Contoso.Forms.Demo/Constants.cs
@@ -6,11 +6,6 @@ namespace Contoso.Forms.Demo
     [Android.Runtime.Preserve(AllMembers = true)]
     public static class Constants
     {
-        public const string Verbose = "Verbose";
-        public const string Debug = "Debug";
-        public const string Info = "Info";
-        public const string Warning = "Warning";
-        public const string Error = "Error";
         public const string BingMapsAuthKey = "paste-key-here";
         public const string UserId = "userId";
     }
--- Apps/Contoso.Forms.Puppet/Contoso.Forms.Puppet/ModulePages/AppCenterContentPage.xaml.cs
+++ Apps/Contoso.Forms.Puppet/Contoso.Forms.Puppet/ModulePages/AppCenterContentPage.xaml.cs
@@ -1,10 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.Windows.Input;
 using Microsoft.AppCenter;
 using Xamarin.Forms;
 
@@ -14,45 +11,37 @@ namespace Contoso.Forms.Puppet
     public partial class AppCenterContentPage : ContentPage
     {
 
-        // E.g., calling LogFunctions["Verbose"](tag, msg) will be
-        // equivalent to calling Verbose(tag, msg)
-        Dictionary<LogLevel, Action<string, string>> LogFunctions;
-        Dictionary<LogLevel, string> LogLevelNames;
-        LogLevel LogWriteLevel;
-
         public AppCenterContentPage()
         {
             InitializeComponent();
-
-            LogFunctions = new Dictionary<LogLevel, Action<string, string>>();
-            LogFunctions.Add(LogLevel.Verbose, AppCenterLog.Verbose);
-            LogFunctions.Add(LogLevel.Debug, AppCenterLog.Debug);
-            LogFunctions.Add(LogLevel.Info, AppCenterLog.Info);
-            LogFunctions.Add(LogLevel.Warn, AppCenterLog.Warn);
-            LogFunctions.Add(LogLevel.Error, AppCenterLog.Error);
-
-            LogLevelNames = new Dictionary<LogLevel, string>();
-            LogLevelNames.Add(LogLevel.Verbose, Constants.Verbose);
-            LogLevelNames.Add(LogLevel.Debug, Constants.Debug);
-            LogLevelNames.Add(LogLevel.Info, Constants.Info);
-            LogLevelNames.Add(LogLevel.Warn, Constants.Warning);
-            LogLevelNames.Add(LogLevel.Error, Constants.Error);
-
-            LogWriteLevel = LogLevel.Verbose;
-            UpdateLogWriteLevelLabel();
-
-
             if (Xamarin.Forms.Device.RuntimePlatform == Xamarin.Forms.Device.iOS)
             {
                 Icon = "bolt.png";
             }
 
-            // Setup auth type dropdown choices
+            // Setup start type dropdown choices
             foreach (var startType in StartTypeUtils.GetStartTypeChoiceStrings())
             {
                 this.StartTypePicker.Items.Add(startType);
             }
             this.StartTypePicker.SelectedIndex = (int)(StartTypeUtils.GetPersistedStartType());
+        }
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+            AppCenterEnabledSwitchCell.On = await AppCenter.IsEnabledAsync();
+            if (Application.Current.Properties.ContainsKey(Constants.UserId) && Application.Current.Properties[Constants.UserId] is string id)
+            {
+                UserIdEntry.Text = id;
+            }
+            UserIdEntry.Unfocused += (sender, args) =>
+            {
+                var inputText = UserIdEntry.Text;
+                var text = string.IsNullOrEmpty(inputText) ? null : inputText;
+                AppCenter.SetUserId(text);
+                Application.Current.Properties[Constants.UserId] = text;
+            };
         }
 
         async void ChangeStartType(object sender, PropertyChangedEventArgs e)
@@ -72,63 +61,10 @@ namespace Contoso.Forms.Puppet
             }
         }
 
-        protected override async void OnAppearing()
-        {
-            base.OnAppearing();
-            LogLevelLabel.Text = LogLevelNames[AppCenter.LogLevel];
-            AppCenterEnabledSwitchCell.On = await AppCenter.IsEnabledAsync();
-            if (Application.Current.Properties.ContainsKey(Constants.UserId) && Application.Current.Properties[Constants.UserId] is string id)
-            {
-                UserIdEntry.Text = id;
-            }
-            UserIdEntry.Unfocused += (sender, args) =>
-            {
-                var inputText = UserIdEntry.Text;
-                var text = string.IsNullOrEmpty(inputText) ? null : inputText;
-                AppCenter.SetUserId(text);
-                Application.Current.Properties[Constants.UserId] = text;
-            };
-        }
-
-        void LogLevelCellTapped(object sender, EventArgs e)
-        {
-            var page = new LogLevelPage();
-            page.LevelSelected += (LogLevel level) =>
-            {
-                AppCenter.LogLevel = level;
-            };
-            ((NavigationPage)Application.Current.MainPage).PushAsync(page);
-        }
-
-        void LogWriteLevelCellTapped(object sender, EventArgs e)
-        {
-            var page = new LogLevelPage();
-            page.LevelSelected += (LogLevel level) =>
-            {
-                LogWriteLevel = level;
-                UpdateLogWriteLevelLabel();
-            };
-            ((NavigationPage)Application.Current.MainPage).PushAsync(page);
-        }
-
-        void WriteLog(object sender, EventArgs e)
-        {
-            string message = LogMessageEntryCell.Text;
-            string tag = LogTagEntryCell.Text;
-            if (tag != null && message != null)
-            {
-                LogFunctions[LogWriteLevel](tag, message);
-            }
-        }
-
         async void UpdateEnabled(object sender, ToggledEventArgs e)
         {
             await AppCenter.SetEnabledAsync(e.Value);
         }
 
-        void UpdateLogWriteLevelLabel()
-        {
-            LogWriteLevelLabel.Text = LogLevelNames[LogWriteLevel];
-        }
     }
 }
