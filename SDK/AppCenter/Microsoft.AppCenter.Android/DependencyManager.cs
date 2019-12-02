156e72d9dc3203a34d0430d9af22671e9372e081
Jae Lim <jaelim@microsoft.com>
2019-12-02 11:55:00 -0800

    Revert "Try bump TargetFramework from netstandard1.0 to netstandard1.1"
    This reverts commit 9dd7e3b3aa66d180089808a54f260ae6661b1cae.

--- SDK/AppCenter/Microsoft.AppCenter.Android/DependencyManager.cs
+++ SDK/AppCenter/Microsoft.AppCenter.Android/DependencyManager.cs
@@ -9,7 +9,7 @@ namespace Microsoft.AppCenter
     {
         static void PlatformSetDependencies(System.Net.Http.HttpClient httpClient)
         {
-            AndroidDependencyManager.SetDependencies((Com.Microsoft.Appcenter.Http.IHttpClient)httpClient);
+            AndroidDependencyManager.PlatformSetDependencies(httpClient);
         }
     }
 }
