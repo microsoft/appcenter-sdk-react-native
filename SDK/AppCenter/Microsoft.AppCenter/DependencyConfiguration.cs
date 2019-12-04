bb773b350aa539d62ab587d306959abdc2034d78
Jae Lim <jaelim@microsoft.com>
2019-12-04 15:28:04 -0800

    Fix portable project for functional test

---
+++ SDK/AppCenter/Microsoft.AppCenter/DependencyConfiguration.cs
@@ -0,0 +1,10 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+namespace Microsoft.AppCenter
+{
+    static partial class DependencyConfiguration
+    {
+        private static IHttpNetworkAdapter PlatformHttpNetworkAdapter { get; set; }
+    }
+}
