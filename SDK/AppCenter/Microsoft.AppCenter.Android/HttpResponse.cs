c3b9b76acb7729114a194247e11aca54478b58a4
Guillaume Perrot <guperrot@microsoft.com>
2019-12-04 12:11:54 -0800

    Address feedback

--- SDK/AppCenter/Microsoft.AppCenter.Android/HttpResponse.cs
+++ SDK/AppCenter/Microsoft.AppCenter.Android/HttpResponse.cs
@@ -1,14 +1,15 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+using AndroidHttpResponse = Com.Microsoft.Appcenter.Http.AndroidHttpResponse;
+
 namespace Microsoft.AppCenter
 {
-    using AndroidHttpResponse = Com.Microsoft.Appcenter.Http.AndroidHttpResponse;
-
     internal partial class HttpResponse
     {
         private int PlatformStatusCode { get; set; }
-        private string PlatformContent { get; set;}
+
+        private string PlatformContent { get; set; }
 
         internal HttpResponse(AndroidHttpResponse httpResponse)
         {
