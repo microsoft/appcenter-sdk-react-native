c172e2948c73efcd33cbd6effe8587f74bfbe0e3
Jae Lim <jaelim@microsoft.com>
2019-12-04 16:37:09 -0800

    Implement trackEvent functional test

---
+++ Tests/Microsoft.AppCenter.Test.Functional/Analytics/TrackEventTest.cs
@@ -0,0 +1,37 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Microsoft.AppCenter.Test.Functional.Analytics
+{
+    using Analytics = Microsoft.AppCenter.Analytics.Analytics;
+
+    public class TrackEventTest
+    {
+        private readonly string _appSecret = Guid.NewGuid().ToString();
+
+        [Fact]
+        public async Task TrackEventWithoutPropertiesAsync()
+        {
+            // Set up HttpNetworkAdapter.
+            var httpNetworkAdapter = new HttpNetworkAdapter();
+            DependencyConfiguration.HttpNetworkAdapter = httpNetworkAdapter;
+
+            // Start App Center.
+            AppCenter.Start(_appSecret, typeof(Analytics));
+
+            // Test TrackEvent.
+            Analytics.TrackEvent("Hello World");
+
+            // Wait for processing event.
+            await httpNetworkAdapter.HttpResponseTask;
+
+            // Verify.
+            // TODO: Complete this verification for the test with JSON content.
+            Assert.Equal("POST", httpNetworkAdapter.Method);
+        }
+    }
+}
---
+++ Tests/Microsoft.AppCenter.Test.Functional/HttpNetworkAdapter.cs
@@ -0,0 +1,56 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Microsoft.AppCenter.Test.Functional
+{
+    internal class HttpNetworkAdapter : IHttpNetworkAdapter
+    {
+        private readonly int _expectedStatusCode;
+        private readonly string _expectedContent;
+
+        private readonly TaskCompletionSource<HttpResponse> _taskCompletionSource = new TaskCompletionSource<HttpResponse>();
+
+        internal Task<HttpResponse> HttpResponseTask { get; private set; }
+
+        internal string Uri { get; private set; }
+        internal string Method { get; private set; }
+        internal IDictionary<string, string> Headers { get; private set; }
+        internal string JsonContent { get; private set; }
+
+        internal HttpNetworkAdapter(int expectedStatusCode = 200, string expectedContent = "")
+        {
+            _expectedStatusCode = expectedStatusCode;
+            _expectedContent = expectedContent;
+            HttpResponseTask = _taskCompletionSource.Task;
+        }
+
+        public Task<HttpResponse> SendAsync(string uri, string method, IDictionary<string, string> headers, string jsonContent, CancellationToken cancellationToken)
+        {
+            var response = new HttpResponse
+            {
+                StatusCode = _expectedStatusCode,
+                Content = _expectedContent
+            };
+
+            if (jsonContent.Contains("event"))
+            {
+                Uri = uri;
+                Method = method;
+                Headers = headers;
+                JsonContent = jsonContent;
+
+                _taskCompletionSource.SetResult(response);
+                return HttpResponseTask;
+            }
+            return Task.FromResult(response);
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
