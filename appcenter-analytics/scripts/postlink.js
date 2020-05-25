d681b6ece688499f3b5a2502b3e8e8305d021734
Charmian Cheng <v-chmin@microsoft.com>
2020-05-25 13:55:47 +0800

    Bump SDK versions

--- appcenter-analytics/scripts/postlink.js
+++ appcenter-analytics/scripts/postlink.js
@@ -34,8 +34,8 @@ if (rnpmlink.ios.checkIfAppDelegateExists()) {
             console.log(`Added code to initialize iOS Analytics SDK in ${file}`);
             return rnpmlink.ios.addPodDeps(
                 [
-                    { pod: 'AppCenter/Analytics', version: '3.1.1' },
-                    { pod: 'AppCenterReactNativeShared', version: '3.0.2' } // in case people don't link appcenter (core)
+                    { pod: 'AppCenter/Analytics', version: '3.2.0' },
+                    { pod: 'AppCenterReactNativeShared', version: '3.0.3' } // in case people don't link appcenter (core)
                 ],
                 { platform: 'ios', version: '9.0' }
             );
