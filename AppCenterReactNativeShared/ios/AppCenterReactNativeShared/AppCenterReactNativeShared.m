1ba32adc76f2d29ea72295d04a4e92e7eced5ab6
Charmian Cheng <v-chmin@microsoft.com>
2020-07-24 10:29:02 +0800

    bump SDK versions

--- AppCenterReactNativeShared/ios/AppCenterReactNativeShared/AppCenterReactNativeShared.m
+++ AppCenterReactNativeShared/ios/AppCenterReactNativeShared/AppCenterReactNativeShared.m
@@ -39,7 +39,7 @@ static NSDictionary *configuration;
 
 + (void)configureAppCenter {
   if (!wrapperSdk) {
-    MSWrapperSdk *wrapperSdk = [[MSWrapperSdk alloc] initWithWrapperSdkVersion:@"3.1.0"
+    MSWrapperSdk *wrapperSdk = [[MSWrapperSdk alloc] initWithWrapperSdkVersion:@"3.1.1"
                                                                 wrapperSdkName:@"appcenter.react-native"
                                                          wrapperRuntimeVersion:nil
                                                         liveUpdateReleaseLabel:nil
