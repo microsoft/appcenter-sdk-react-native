0acd78ab1251ff51c4ead9b04b66b266b9c9ea52
Ruslan Urmeev <v-ruurme@microsoft.com>
2020-11-18 18:47:17 +0400

    Fix jest errors

--- TestApp/__tests__/index.android.js
+++ TestApp/__tests__/index.android.js
@@ -8,7 +8,18 @@ import Index from '../index.js';
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
-it('renders correctly', () => {
+// Jest fake timers are used to fix the issue: https://github.com/facebook/jest/issues/6434
+beforeEach(() => {
+  jest.useFakeTimers()
+});
+
+// Running all pending timers and switching to real timers using Jest
+afterEach(() => {
+  jest.runOnlyPendingTimers()
+  jest.useRealTimers()
+});
+
+it('renders correctly', async () => {
   const tree = renderer.create(
     <Index />
   );
