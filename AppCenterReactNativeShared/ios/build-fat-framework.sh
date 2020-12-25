ec31776ba9073e93d98fef04df189c97244c8844
Ruslan Urmeev <v-ruurme@microsoft.com>
2020-12-25 12:57:34 +0400

    Fix AppCenterReactNativeShared script, update iphoneos deployment target

--- AppCenterReactNativeShared/ios/build-fat-framework.sh
+++ AppCenterReactNativeShared/ios/build-fat-framework.sh
@@ -9,16 +9,17 @@ TEMP_DIR=${PRODUCTS_DIR}/${ZIP_FOLDER}
 INSTALL_DIR=${TEMP_DIR}/${FMK_NAME}.framework
 
 # Working dir will be deleted after the framework creation.
-WRK_DIR=build
+DERIVED_DATA_PATH=build
+WRK_DIR=${DERIVED_DATA_PATH}/Build/Products
 DEVICE_DIR=${WRK_DIR}/Release-iphoneos
 SIMULATOR_DIR=${WRK_DIR}/Release-iphonesimulator
 
 # Cleaning previous build
-xcodebuild -project "${FMK_NAME}.xcodeproj" -configuration "Release" -target "${FMK_NAME}" clean
+xcodebuild -workspace "${FMK_NAME}.xcworkspace" -configuration "Release" -scheme "${FMK_NAME}" clean
 
 # Building both architectures.
-xcodebuild -project "${FMK_NAME}.xcodeproj" -configuration "Release" -target "${FMK_NAME}" -sdk iphoneos
-xcodebuild -project "${FMK_NAME}.xcodeproj" -configuration "Release" -target "${FMK_NAME}" -sdk iphonesimulator
+xcodebuild -workspace "${FMK_NAME}.xcworkspace" -configuration "Release" -scheme "${FMK_NAME}" -sdk iphoneos -derivedDataPath "$DERIVED_DATA_PATH"
+xcodebuild -workspace "${FMK_NAME}.xcworkspace" -configuration "Release" -scheme "${FMK_NAME}" -sdk iphonesimulator -derivedDataPath "$DERIVED_DATA_PATH"
 
 # Cleaning the oldest.
 if [ -d "${TEMP_DIR}" ]
@@ -35,7 +36,7 @@ mkdir -p "${INSTALL_DIR}/Modules"
 cp -f "${SRCROOT}/${FMK_NAME}/Support/module.modulemap" "${INSTALL_DIR}/Modules/"
 
 # Copies the headers and resources files to the final product folder.
-cp -R "${SRCROOT}/${WRK_DIR}/Release-iphoneos/include/${FMK_NAME}/" "${INSTALL_DIR}/Headers/"
+cp -R "${WRK_DIR}/Release-iphoneos/include/${FMK_NAME}/" "${INSTALL_DIR}/Headers/"
 
 # Copies the license file to the products directory (required for cocoapods)
 cp -f "../LICENSE" "${TEMP_DIR}"
@@ -43,4 +44,4 @@ cp -f "../LICENSE" "${TEMP_DIR}"
 # Uses the Lipo Tool to merge both binary files (i386 + armv6/armv7/armv7s/arm64) into one Universal final product.
 lipo -create "${DEVICE_DIR}/lib${FMK_NAME}.a" "${SIMULATOR_DIR}/lib${FMK_NAME}.a" -output "${INSTALL_DIR}/${FMK_NAME}"
 
-rm -r "${WRK_DIR}"
+rm -r "${DERIVED_DATA_PATH}"
