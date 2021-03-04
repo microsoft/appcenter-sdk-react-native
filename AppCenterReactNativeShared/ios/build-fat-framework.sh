6b648ebe19bb615dbe670f0d01864f6c09d1ce32
Dmitriy Kirakosyan <v-dmkira@microsoft.com>
2021-03-04 03:28:49 +0300

    [apple] Support Mac Catalyst

--- AppCenterReactNativeShared/ios/build-fat-framework.sh
+++ AppCenterReactNativeShared/ios/build-fat-framework.sh
@@ -30,54 +30,72 @@ then
 rm -rf "${TEMP_DIR}"
 fi
 
-# Cleaning previous build
-xcodebuild -workspace "${FMK_NAME}.xcworkspace" -configuration "Release" -scheme "${FMK_NAME}" clean
+create_versions_dir_for_mac_framework() {
+    local framework_dir=$1
+    local headers_dir=$2
+    mkdir -p "${framework_dir}/Versions"
 
-# Building Release-iphoneos temporary framework.
-xcodebuild -workspace "${FMK_NAME}.xcworkspace" -configuration "Release" -scheme "${FMK_NAME}" -sdk iphoneos -derivedDataPath "$DERIVED_DATA_PATH"
+    # Create the path to the real Headers
+    mkdir -p "${framework_dir}/Versions/A/Headers"
 
-# Creates and renews Release-iphoneos temporary framework folder.
-mkdir -p "${INSTALL_DIR_IPHONEOS}"
-mkdir -p "${FMK_DIR_IPHONEOS}"
-mkdir -p "${FMK_DIR_IPHONEOS}/Headers"
-mkdir -p "${FMK_DIR_IPHONEOS}/Modules"
+    # Create the required symlinks
+    /bin/ln -sfh A "${framework_dir}/Versions/Current"
+    /bin/ln -sfh Versions/Current/Headers "${framework_dir}/Headers"
+    /bin/ln -sfh "Versions/Current/${FMK_NAME}" "${framework_dir}/${FMK_NAME}"
 
-# Copy the swift import file to the temporary framework folder.
-cp -f "${SRCROOT}/${FMK_NAME}/Support/module.modulemap" "${FMK_DIR_IPHONEOS}/Modules/"
+    # Copy the public headers into the framework
+    /bin/cp -a ${headers_dir} "${framework_dir}/Versions/A/Headers"
+}
 
-# Copies the headers files to the temporary framework folder.
-cp -R "${WRK_DIR}/Release-iphoneos/include/${FMK_NAME}/" "${FMK_DIR_IPHONEOS}/Headers/"
 
-# Copies the static library files to the temporary framework folder.
-cp -R "${DEVICE_DIR}/lib${FMK_NAME}.a" "${FMK_DIR_IPHONEOS}/${FMK_NAME}"
+for sdk in iphoneos iphonesimulator maccatalyst; do
+    # Cleaning previous build
+    xcodebuild -workspace "${FMK_NAME}.xcworkspace" -configuration "Release" -scheme "${FMK_NAME}" clean
 
-# Building Release-iphonesimulator temporary framework.
-xcodebuild -workspace "${FMK_NAME}.xcworkspace" -configuration "Release" -scheme "${FMK_NAME}" -sdk iphonesimulator -derivedDataPath "$DERIVED_DATA_PATH"
+    # Building framework.
+    if [ ${sdk} == "maccatalyst" ]; then
+        xcodebuild -workspace "${FMK_NAME}.xcworkspace" -configuration "Release" -scheme "${FMK_NAME}" -destination 'platform=macOS,variant=Mac Catalyst' -derivedDataPath "$DERIVED_DATA_PATH"
+    else
+        xcodebuild -workspace "${FMK_NAME}.xcworkspace" -configuration "Release" -scheme "${FMK_NAME}" -sdk "${sdk}" -derivedDataPath "$DERIVED_DATA_PATH"
+    fi
 
-# Creates and renews Release-iphonesimulator temporary framework folder.
-mkdir -p "${INSTALL_DIR_IPHONESIMULATOR}"
-mkdir -p "${FMK_DIR_IPHONESIMULATOR}"
-mkdir -p "${FMK_DIR_IPHONESIMULATOR}/Headers"
-mkdir -p "${FMK_DIR_IPHONESIMULATOR}/Modules"
+    # Creates and renews Release-iphoneos temporary framework folder.
+    INSTALL_DIR=${TEMP_DIR}/Release-${sdk}
+    FMK_DIR=${INSTALL_DIR}/${FMK_NAME}.framework
+    HEADERS_DIR="${WRK_DIR}/Release-${sdk}/include/${FMK_NAME}/"
 
-# Copy the swift import file to the temporary framework folder.
-cp -f "${SRCROOT}/${FMK_NAME}/Support/module.modulemap" "${FMK_DIR_IPHONESIMULATOR}/Modules/"
+    mkdir -p "${INSTALL_DIR}"
+    mkdir -p "${FMK_DIR}"
+    mkdir -p "${FMK_DIR}/Headers"
+    mkdir -p "${FMK_DIR}/Modules"
 
-# Copies the headers files to the temporary framework folder.
-cp -R "${WRK_DIR}/Release-iphonesimulator/include/${FMK_NAME}/" "${FMK_DIR_IPHONESIMULATOR}/Headers/"
+    # Copy the swift import file to the temporary framework folder.
+    cp -f "${SRCROOT}/${FMK_NAME}/Support/module.modulemap" "${FMK_DIR}/Modules/"
 
-# Copies the static library files to the temporary framework folder.
-cp -R "${SIMULATOR_DIR}/lib${FMK_NAME}.a" "${FMK_DIR_IPHONESIMULATOR}/${FMK_NAME}"
+    # Copies the headers files to the temporary framework folder.
+    cp -R "${WRK_DIR}/Release-${sdk}/include/${FMK_NAME}/" "${FMK_DIR}/Headers/"
+
+    if [ ${sdk} == "maccatalyst" ]; then
+        create_versions_dir_for_mac_framework ${FMK_DIR} ${HEADERS_DIR}
+    fi
+
+    # Copies the static library files to the temporary framework folder.
+    SDK_DIR=${WRK_DIR}/Release-${sdk}
+    cp -R "${SDK_DIR}/lib${FMK_NAME}.a" "${FMK_DIR}/${FMK_NAME}"
+
+    frameworks+=( -framework "${FMK_DIR}")
+done
 
 # Building fina xcframework
-xcodebuild -create-xcframework -framework "${FMK_DIR_IPHONEOS}" -framework "${FMK_DIR_IPHONESIMULATOR}" -output "${INSTALL_DIR_XCFRAMEWORK}"
+xcodebuild -create-xcframework "${frameworks[@]}" -output "${INSTALL_DIR_XCFRAMEWORK}"
 
 # Copies the license file to the products directory (required for cocoapods)
 cp -f "../LICENSE" "${TEMP_DIR}"
 
-# Remove temporary install folders
-rm -r "${INSTALL_DIR_IPHONEOS}"
-rm -r "${INSTALL_DIR_IPHONESIMULATOR}"
+for sdk in iphoneos iphonesimulator maccatalyst; do
+    # Remove temporary install folders
+    rm -r ${TEMP_DIR}/Release-${sdk}
+done
 
 # Remove build folder
 rm -r "${DERIVED_DATA_PATH}"
