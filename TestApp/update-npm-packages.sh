2917dd23d0148e75d205d2ed413e5b4c9722eec7
Ruslan Urmeev <v-ruurme@microsoft.com>
2021-06-24 14:47:28 +0400

    Add comment about Xcode 12.5 fix

--- TestApp/update-npm-packages.sh
+++ TestApp/update-npm-packages.sh
@@ -22,6 +22,8 @@ rm appcenter*.tgz
 echo "Installing other packages..."
 npm install
 
+# Issue: https://github.com/react-native-community/cli/issues/1403
+# Fixed in RN 0.65: https://github.com/facebook/react-native/commit/8721ee0a6b10e5bc8a5a95809aaa7b25dd5a6043
 echo "Applying patch to react-native package that fixes build on Xcode 12.5..."
 npx patch-package
 
