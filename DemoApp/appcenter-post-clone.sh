89f7ab80b025bb95fa5b49048c5bef69f8510d42
Dmitriy Kirakosyan <v-dmkira@microsoft.com>
2021-09-21 02:28:31 +0300

    print variables

--- DemoApp/appcenter-post-clone.sh
+++ DemoApp/appcenter-post-clone.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 echo "Executing post clone script in `pwd`"
+echo 'APPCENTER_SOURCE_DIRECTORY' $APPCENTER_SOURCE_DIRECTORY
+echo 'REPO_NAME' $REPO_NAME
 echo $NPM_RC | base64 --decode > $APPCENTER_SOURCE_DIRECTORY/DemoApp/.npmrc
 # Delete everything except DemoApp folder
 rm -rf ../appcenter* ../AppCenterReactNativeShared ../TestApp34 ../BrownfieldTestApp ../TestApp ../TestAppTypescript
