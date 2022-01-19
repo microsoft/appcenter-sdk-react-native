7bb1a1f7c15938f0e4b31c872acd6cba1e2850ea
AnatolyPristensky <v-anatpr@microsoft.com>
2022-01-19 15:49:23 +0300

    Update Versions

--- AppCenterReactNativeShared/ios/AppCenterReactNativeShared/AppCenterReactNativeShared.m
+++ AppCenterReactNativeShared/ios/AppCenterReactNativeShared/AppCenterReactNativeShared.m
@@ -39,7 +39,7 @@ static NSDictionary *configuration;
 
 + (void)configureAppCenter {
   if (!wrapperSdk) {
-    MSACWrapperSdk *wrapperSdk = [[MSACWrapperSdk alloc] initWithWrapperSdkVersion:@"4.4.1"
+    MSACWrapperSdk *wrapperSdk = [[MSACWrapperSdk alloc] initWithWrapperSdkVersion:@"4.4.3"
                                                                 wrapperSdkName:@"appcenter.react-native"
                                                          wrapperRuntimeVersion:nil
                                                         liveUpdateReleaseLabel:nil
