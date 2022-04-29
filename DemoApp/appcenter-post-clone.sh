0fc8acc35d644ee9e69147dc46f6a25bed8df669
AnatolyPristensky <v-anatpr@microsoft.com>
2022-04-29 11:53:09 +0200

    Update appcenter-post-clone.sh

--- DemoApp/appcenter-post-clone.sh
+++ DemoApp/appcenter-post-clone.sh
@@ -8,6 +8,12 @@ echo 'Create local.properties file if it does not exist'
 touch -a android/local.properties
 ./put-azure-credentials.zh $USER_ACCOUNT $ACCESS_TOKEN
 
+echo 'Setting AppSecrets'
+cd ..
+./scripts/update-app-secrets.sh PROD
+echo 'AppSecret set to:'
+echo $RN_ANDROID_PROD
+
 echo 'Add private cocoapods repository'
 pod repo add $REPO_NAME https://$USER_ACCOUNT:$ACCESS_TOKEN@$PRIVATE_REPO_BASE_URL/$REPO_NAME
 pod repo update
@@ -24,8 +30,3 @@ shopt -s extglob
 echo 'NDKs after clean:'
 ls $NDK_PATH
 
-echo 'Setting AppSecrets'
-cd ..
-./scripts/update-app-secrets.sh PROD
-echo 'AppSecret set to:'
-echo $RN_ANDROID_PROD
