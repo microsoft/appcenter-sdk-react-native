89a88630f2c8ea19822075072fe582e5b994a058
Jesse Katsumata <jesse.katsumata@gmail.com>
2022-06-24 15:02:35 +0900

    fix: pod install for react-native/cli v8

--- appcenter-analytics/react-native.config.js
+++ appcenter-analytics/react-native.config.js
@@ -3,9 +3,7 @@ const path = require('path');
 module.exports = {
   dependency: {
     platforms: {
-      ios: {
-        podspecPath: path.join(__dirname, 'ios', 'appcenter-analytics.podspec')
-      },
+      ios: {},
       android: {
         packageInstance: 'new AppCenterReactNativeAnalyticsPackage(getApplication(), getResources().getString(R.string.appCenterAnalytics_whenToEnableAnalytics))'
       }
