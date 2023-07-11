d523c776d39082586f2daadc13084d577eeb683a
AnatolyPristensky <v-anatpr@microsoft.com>
2023-07-11 12:59:32 +0200

    React Native Release 5.0.0

--- appcenter/scripts/postlink.js
+++ appcenter/scripts/postlink.js
@@ -32,7 +32,7 @@ if (rnpmlink.ios.checkIfAppDelegateExists()) {
         .then((file) => {
             console.log(`Added code to initialize iOS AppCenter SDK in ${file}`);
             return rnpmlink.ios.addPodDeps(
-                [{ pod: 'AppCenterReactNativeShared', version: '4.4.5' }],
+                [{ pod: 'AppCenterReactNativeShared', version: '5.0.0' }],
                 { platform: 'ios', version: '9.0' }
             );
         })
