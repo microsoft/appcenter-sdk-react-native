945ee40250e5c1cc74cb3815ad566e5ceea885ad
Dmitriy Kirakosyan <v-dmkira@microsoft.com>
2024-01-05 16:28:31 +0700

    Migrate AppCenterReactNativeShared to MavenPublication

--- scripts/put-maven-secrets-gradle.sh
+++ scripts/put-maven-secrets-gradle.sh
@@ -2,7 +2,6 @@
 cat >> AppCenterReactNativeShared/android/local.properties << EOL
 maven.user=${1:-$MAVEN_USER}
 maven.key=${2:-$MAVEN_KEY}
-maven.repoUrl=${3:-$MAVEN_REPO}
 maven.signingKeyId=${4:-$GDP_SIGNING_KEY_ID}
 maven.secretKeyPath=${5:-$GDP_KEY_SECRET_PATH}
 maven.publicKeyPassword=${6:-$GDP_KEY_PASSWORD}
