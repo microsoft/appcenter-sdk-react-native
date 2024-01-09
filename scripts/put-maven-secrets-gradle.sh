c7aca5db8c8cedbe65ad940e55e39ce40b534a5d
AnatolyPristensky <v-anatpr@microsoft.com>
2024-01-09 10:50:15 +0100

    Correct numeration

--- scripts/put-maven-secrets-gradle.sh
+++ scripts/put-maven-secrets-gradle.sh
@@ -2,7 +2,7 @@
 cat >> AppCenterReactNativeShared/android/local.properties << EOL
 maven.user=${1:-$MAVEN_USER}
 maven.key=${2:-$MAVEN_KEY}
-maven.signingKeyId=${4:-$GDP_SIGNING_KEY_ID}
-maven.secretKeyPath=${5:-$GDP_KEY_SECRET_PATH}
-maven.publicKeyPassword=${6:-$GDP_KEY_PASSWORD}
+maven.signingKeyId=${3:-$GDP_SIGNING_KEY_ID}
+maven.secretKeyPath=${4:-$GDP_KEY_SECRET_PATH}
+maven.publicKeyPassword=${5:-$GDP_KEY_PASSWORD}
 EOL
