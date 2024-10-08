97a5607283a0b79d9f1f4b0cedad183e9f3206f9
App Center <v-msuendukov@microsoft.com>
2024-10-08 18:04:58 +0200

    temp comment pod install (revert before merge)

--- TestApp/update-npm-packages.sh
+++ TestApp/update-npm-packages.sh
@@ -50,8 +50,8 @@ echo "Install shared framework pods..."
 (cd ../AppCenterReactNativeShared/ios && pod install)
 
 # for testing with not released apple and android sdks, you will need to provide the storage access key
-echo "Running pod install and building shared framework..."
+# echo "Running pod install and building shared framework..."
 # (cd ios && pod install --repo-update)
 
 # workaround for macs on arm64 (uncomment when running on arm64 mac)
-(cd ios && pod install)
+# (cd ios && pod install)
