a2c7a12bcb85c6d95337bf8a3b13dc7b2af57f02
App Center <v-msuendukov@microsoft.com>
2024-10-09 16:39:48 +0200

    improve install-ndk-version script

--- TestApp/install-ndk-version.sh
+++ TestApp/install-ndk-version.sh
@@ -1,16 +1,26 @@
 #!/bin/bash
 
 if [ -z "$1" ]; then
-    echo "There is no NDK_VERSION variable provided"
+    echo "There is no NDK_MAJOR_VERSION variable provided"
     return 1
 fi
 
-NDK_VERSION="$1"
-echo "NDK_VERSION is $NDK_VERSION"
+if [ -z "$ANDROID_HOME" ]; then
+    echo "ANDROID_HOME is not defined"
+    return 1
+fi
 
-echo "Download Android NDK version $NDK_VERSION"
-curl -o android-ndk-r$NDK_VERSION-darwin-x86_64.zip https://dl.google.com/android/repository/android-ndk-r$NDK_VERSION-darwin-x86_64.zip
-unzip android-ndk-r$NDK_VERSION-darwin-x86_64.zip -d $HOME
-export ANDROID_NDK_HOME=$HOME/android-ndk-r$NDK_VERSION
-echo "ANDROID_NDK_HOME set as $ANDROID_NDK_HOME"
-echo "##vso[task.setvariable variable=ANDROID_NDK_HOME]$HOME/android-ndk-r$NDK_VERSION"
+NDK_MAJOR_VERSION="$1"
+echo "NDK_MAJOR_VERSION is $NDK_MAJOR_VERSION"
+
+echo "Download Android NDK version $NDK_MAJOR_VERSION"
+curl -o android-ndk-r$NDK_MAJOR_VERSION-darwin-x86_64.zip https://dl.google.com/android/repository/android-ndk-r$NDK_MAJOR_VERSION-darwin-x86_64.zip
+unzip android-ndk-r$NDK_MAJOR_VERSION-darwin-x86_64.zip -d android-ndk-r$NDK_MAJOR_VERSION
+
+echo "Copy NDK to $ANDROID_HOME/ndk-bundle"
+rm -rf $ANDROID_HOME/ndk-bundle
+cp -r ./android-ndk-r$NDK_MAJOR_VERSION/android-ndk-r$NDK_MAJOR_VERSION $ANDROID_HOME/ndk-bundle
+
+rm -rf android-ndk-r$NDK_MAJOR_VERSION android-ndk-r$NDK_MAJOR_VERSION-darwin-x86_64.zip
+
+echo "NDK $NDK_MAJOR_VERSION is installed to $ANDROID_HOME/ndk-bundle"
