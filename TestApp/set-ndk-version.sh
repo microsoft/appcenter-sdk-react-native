67d1dd36e301605bfd931f0e7e1d85c646058cf9
App Center <v-msuendukov@microsoft.com>
2024-10-09 13:43:59 +0200

    use use-ndk-version script

---
+++ TestApp/set-ndk-version.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+
+PROPERTY_FILE="$ANDROID_HOME/ndk-bundle/source.properties"
+
+echo "Reading ndk version from source.properties file..."
+VERSION=$(cat $PROPERTY_FILE | sed 's/ //g' | grep "Pkg.Revision" | cut -d'=' -f2)
+echo $VERSION
+GRADLE_FILE="$PROJECT_DIR/android/app/build.gradle"
+
+if [ -z $VERSION ]; then
+  echo "No NDK found in the default location. Proceeding..."
+else
+
+  # Insert ndkVersion = 'x.x.x' in the android section.
+  NDK_VERSION_LINE="ndkVersion = '$VERSION'"
+  echo "$(sed "s/android {/android {\\`echo -e '\n\r    '`$NDK_VERSION_LINE/g" "$GRADLE_FILE")" > $GRADLE_FILE
+fi
