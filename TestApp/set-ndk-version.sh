a0c3229d7c3fc22576920736c34f0c15ccfb4572
App Center <v-msuendukov@microsoft.com>
2024-10-10 14:18:30 +0200

    try to set ndk without install

--- TestApp/set-ndk-version.sh
+++ TestApp/set-ndk-version.sh
@@ -3,11 +3,62 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 
-PROPERTY_FILE="$ANDROID_HOME/ndk-bundle/source.properties"
+check_ndk_version() {
+    local ndk_path=$1
+    if [ -f "$ndk_path/source.properties" ]; then
+        version=$(grep "Pkg.Revision" "$ndk_path/source.properties" | cut -d' ' -f3)
+        echo "$version"
+    else
+        echo ""
+    fi
+}
 
-echo "Reading ndk version from source.properties file..."
-VERSION=$(cat $PROPERTY_FILE | sed 's/ //g' | grep "Pkg.Revision" | cut -d'=' -f2)
-echo $VERSION
+found_version=""
+
+if [ -n "$ANDROID_NDK_HOME" ]; then
+    echo "Checking NDK version in ANDROID_NDK_HOME..."
+    found_version=$(check_ndk_version "$ANDROID_NDK_HOME")
+    if [ -n "$found_version" ]; then
+        echo "NDK version: $found_version (Path: $ANDROID_NDK_HOME)"
+    else
+        echo "NDK not found in $ANDROID_NDK_HOME"
+    fi
+else
+    echo "ANDROID_NDK_HOME is not set."
+    echo "Searching for NDK in default SDK locations..."
+
+    if [ -n "$ANDROID_HOME" ]; then
+        ndk_default_path="$ANDROID_HOME/ndk"
+    else
+        ndk_default_path="$HOME/Library/Android/sdk/ndk"
+    fi
+
+    if [ -d "$ndk_default_path" ]; then
+        for ndk_dir in "$ndk_default_path"/*; do
+            if [ -d "$ndk_dir" ]; then
+                found_version=$(check_ndk_version "$ndk_dir")
+                if [ -n "$found_version" ]; then
+                    echo "NDK version: $found_version (Path: $ndk_dir)"
+                    break
+                fi
+            fi
+        done
+    else
+        echo "NDK not found in default SDK path: $ndk_default_path"
+    fi
+fi
+
+if [ -z "$found_version" ]; then
+    echo "No NDK version found."
+fi
+
+# PROPERTY_FILE="$ANDROID_HOME/ndk-bundle/source.properties"
+
+# echo "Reading ndk version from source.properties file..."
+# VERSION=$(cat $PROPERTY_FILE | sed 's/ //g' | grep "Pkg.Revision" | cut -d'=' -f2)
+# echo $VERSION
+
+$VERSION = $found_version
 GRADLE_FILE="./android/app/build.gradle"
 
 if [ -z $VERSION ]; then
