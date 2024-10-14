8edc6c88b8f5970276e62f145d3a02e7b3f44fc3
App Center <v-msuendukov@microsoft.com>
2024-10-14 13:16:33 +0200

    refactor update-npm-packages script

--- TestApp/update-npm-packages.sh
+++ TestApp/update-npm-packages.sh
@@ -5,6 +5,29 @@
 
 # Update AppCenter npm packages for the TestApp
 
+if [ -z "$1" ]; then
+    echo "Running for both Android and iOS"
+    is_android=true
+    is_ios=true
+else 
+    case "$1" in
+        android)
+            echo "Running for Android"
+            is_android=true
+            is_ios=false
+            ;;
+        ios)
+            echo "Running for iOS"
+            is_android=false
+            is_ios=true
+            ;;
+        *)
+            echo "Invalid argument: $1"
+            exit 1
+            ;;
+    esac
+fi
+
 set -e
 
 echo 'Removing existing appcenter* packages...'
@@ -19,51 +42,50 @@ npm pack ../appcenter-link-scripts
 echo "Installing appcenter* packages..."
 npm install appcenter*.tgz
 
-echo "workaround for macs on arm64"
-npm install react-native-fs --save --legacy-peer-deps
-
 echo "Cleanup appcenter*.tgz"
 rm appcenter*.tgz
 
 echo "Installing other packages..."
 npm install
 
-# Issue: https://github.com/react-native-community/cli/issues/1403
-# Fixed in RN 0.65: https://github.com/facebook/react-native/commit/8721ee0a6b10e5bc8a5a95809aaa7b25dd5a6043
-echo "Applying patch to react-native package that fixes build on Xcode 12.5..."
-npx patch-package
+if [ "$is_ios" == "true" ]; then
+    echo "Workaround for macs on arm64"
+    npm install react-native-fs --save --legacy-peer-deps
 
-echo "Running jetify to resolve AndroidX compatibility issues..."
-npx jetify
+    echo "Applying patch to react-native package that fixes build on Xcode 12.5..."
+    npx patch-package
 
-OS_TYPE=$(uname)
-if [[ "$OS_TYPE" == "Darwin" ]]; then
-    # macOS
-    SED_INPLACE=("sed" "-i" "")
-else
-    # Linux and others
-    SED_INPLACE=("sed" "-i")
+    echo "Updating CocoaPods repos..."
+    pod repo update
+
+    echo "Install shared framework pods..."
+    (cd ../AppCenterReactNativeShared/ios && pod install)
+
+    echo "Running pod install and building shared framework..."
+    (cd ios && pod install --repo-update)
 fi
 
-# Remove versionName and versionCode from build.gradle for AppCenterReactNativeShared
-# to avoid build errors like:
-# > Could not find method namespace() for arguments [com.microsoft.appcenter.reactnative.appcenter] on extension 'android' of type com.android.build.gradle.LibraryExtension.
-echo "Remove versionName and versionCode from build.gradle for AppCenterReactNativeShared"
-"${SED_INPLACE[@]}" "/buildConfigField .*VERSION_NAME/d" "../AppCenterReactNativeShared/android/build.gradle"
-"${SED_INPLACE[@]}" "/from components.release/d" "../AppCenterReactNativeShared/android/build.gradle"
 
-# Move android namespaces for the AppCenter modules from build.gradle to AndroidManifest.xml
-bash ./use-android-manifest-namespaces.sh
+if [ "$is_android" == "true" ]; then
+    echo "Running jetify to resolve AndroidX compatibility issues..."
+    npx jetify
 
-echo "Updating CocoaPods repos..."
-pod repo update
+    OS_TYPE=$(uname)
+    if [[ "$OS_TYPE" == "Darwin" ]]; then
+        # macOS
+        SED_INPLACE=("sed" "-i" "")
+    else
+        # Linux and others
+        SED_INPLACE=("sed" "-i")
+    fi
 
-echo "Install shared framework pods..."
-(cd ../AppCenterReactNativeShared/ios && pod install)
+    # Remove versionName and versionCode from build.gradle for AppCenterReactNativeShared
+    # to avoid build errors like:
+    # > Could not find method namespace() for arguments [com.microsoft.appcenter.reactnative.appcenter] on extension 'android' of type com.android.build.gradle.LibraryExtension.
+    echo "Remove versionName and versionCode from build.gradle for AppCenterReactNativeShared"
+    "${SED_INPLACE[@]}" "/buildConfigField .*VERSION_NAME/d" "../AppCenterReactNativeShared/android/build.gradle"
+    "${SED_INPLACE[@]}" "/from components.release/d" "../AppCenterReactNativeShared/android/build.gradle"
 
-# for testing with not released apple and android sdks, you will need to provide the storage access key
-echo "Running pod install and building shared framework..."
-(cd ios && pod install --repo-update)
-
-# workaround for macs on arm64 (uncomment when running on arm64 mac)
-# (cd ios && pod install)
+    # Move android namespaces for the AppCenter modules from build.gradle to AndroidManifest.xml
+    bash ./use-android-manifest-namespaces.sh
+fi
