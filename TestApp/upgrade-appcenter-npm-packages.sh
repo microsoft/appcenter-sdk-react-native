93c54f81627514f1a73287bfef39e203959aae90
Dmitriy Kirakosyan <v-dmkira@microsoft.com>
2024-11-22 15:50:23 +0700

    Add scripts for using AppCenter from remote package manager

---
+++ TestApp/upgrade-appcenter-npm-packages.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+
+npm uninstall --save appcenter appcenter-analytics appcenter-crashes appcenter-link-scripts
+
+npm install --save appcenter appcenter-analytics appcenter-crashes
+
+pod repo update
+
+(cd ios && pod install)
+
+npx react-native link
