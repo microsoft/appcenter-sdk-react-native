752ac06fb3f73bd8cde2ea9e50efa66689a2a9ea
Dmitriy Kirakosyan <v-dmkira@microsoft.com>
2024-11-26 15:32:13 +0700

    Remove TestApp62

--- TestApp62/index.js
+++
@@ -1,9 +0,0 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
-
-import { AppRegistry } from 'react-native';
-import App from './app/App';
-
-AppRegistry.registerComponent('TestApp', () => App);
-
-export default App;
